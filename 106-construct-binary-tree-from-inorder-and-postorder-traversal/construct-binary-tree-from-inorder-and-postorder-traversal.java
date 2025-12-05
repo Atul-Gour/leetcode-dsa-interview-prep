6710b7574cfaa5a70dd0cc3ae1c6b5d5e3acaaf1
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2025-12-05 17:55:08 +0530

    Time: 2 ms (55.99%) | Memory: 46.3 MB (25.82%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,41 +15,30 @@
  */
 class Solution {
 
-    TreeNode make(HashMap<Integer, Integer> map, int[] inorder, Stack<Integer> st, int left, int right){
-        
-        if(st.isEmpty() || left > right || right> inorder.length || left < 0)return null;
+int postIndex;
 
-        int parentIndex =map.get(st.peek());
-        TreeNode curr = new TreeNode(st.pop());
-        if(st.isEmpty() || left==right)return curr;
-        
-        int childIndex = map.get(st.peek());
+public TreeNode build(HashMap<Integer,Integer> map,int start,int end, int[] postorder){
+if(start > end) return null;
+int rootNode= postorder[postIndex--];
+TreeNode root=new TreeNode(rootNode);
 
-        if(childIndex>parentIndex && childIndex<=right)
-        curr.right= make(map, inorder, st, parentIndex+1 , right);
+int index=map.get(rootNode);
+root.right=build(map,index+1,end,postorder);
+root.left=build(map,start,index-1,postorder);
 
-        if(st.isEmpty() || left==right)return curr;
-        childIndex = map.get(st.peek());
+return root;
+}
 
-        if(childIndex<parentIndex && childIndex>=left){
-            curr.left= make(map, inorder, st, left , parentIndex-1);
-        }
-        return curr;
-    }
 
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-        int n=inorder.length;
-        Boolean [] visited = new Boolean[n];
-        HashMap<Integer, Integer> map = new HashMap<>();
-        Stack<Integer> st= new Stack<>();
-        for(int i=0;i<n; i++){
+    public TreeNode buildTree(int[] inorder, int[] postorder){
+             postIndex= postorder.length-1;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i =0;i<inorder.length;i++){
             map.put(inorder[i],i);
         }
-        for(int i: postorder){
-            st.push(i);
-        }
-        TreeNode root = make(map, inorder, st,0 ,n-1);
-        return root;
 
+        return build(map,0,inorder.length-1,postorder);
+        
+        
     }
 }
