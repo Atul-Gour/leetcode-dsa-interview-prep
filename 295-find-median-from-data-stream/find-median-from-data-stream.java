a8c74c0e16c4fb7cf4cf0ff78361c4b1b9764bbb
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2025-12-06 21:33:33 +0530

    Time: 1163 ms (5.12%) | Memory: 112.8 MB (5.80%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -0,0 +1,92 @@
+class Node {
+    int val;
+    Node next;
+    Node prev;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+class MedianFinder {
+    Node head;
+    int n=0;
+    Node middle;
+
+    public MedianFinder() {
+        head= null;
+        middle = head;
+    }
+
+    void add(Node newNode){
+        Node temp = head;
+        Boolean isMiddle = false;
+        if(newNode.val >= middle.val){
+            temp=middle;
+            isMiddle = true;
+        }
+
+        while(temp.next != null && newNode.val>temp.next.val){
+            temp=temp.next;
+        }
+        newNode.next= temp.next;
+        newNode.prev= temp;
+        temp.next = newNode;
+        if(newNode.next!=null)newNode.next.prev = newNode;
+        if(isMiddle){
+            if(n%2==0)middle= middle.next;
+        }else{
+            if(n%2==0)return;
+            middle= middle.prev;
+        }
+
+    }
+    
+    public void addNum(int num) {
+        Node newNode = new Node(num);
+
+        if (head == null) {
+            newNode.next = head;
+            head = newNode;
+            middle=head;
+            n++;
+            return;
+        }else if(num< head.val){
+            newNode.next = head;
+            head.prev = newNode;
+            head= newNode;
+            if(n%2!=0)middle = middle.prev;
+            n++;
+            return;
+        }
+        else {
+            add(newNode);
+            n++;
+        }
+        // Node temp = head;
+        // while(temp!=null){
+        //     System.out.print(temp.val+" ");
+        //     temp=temp.next;
+        // }
+        // System.out.println(" end ");
+    }
+    
+    public double findMedian() {
+        double median =0;
+        if(n%2!=0){
+            median = middle.val;
+        }else{
+            
+            int a= middle.val;
+            int b= middle.next.val;
+            median = (b+a)/2.0;
+        }
+        return median;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
