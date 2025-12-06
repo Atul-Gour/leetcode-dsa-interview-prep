5209cbb5e6cf7b496ed0bd40ba45dbae0f2a4de6
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2025-12-06 21:51:26 +0530

    Time: 131 ms (58.91%) | Memory: 111.4 MB (29.69%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,86 +1,33 @@
-class Node {
-    int val;
-    Node next;
-    Node prev;
-
-    Node(int val) {
-        this.val = val;
-    }
-}
 class MedianFinder {
-    Node head;
-    int n=0;
-    Node middle;
+
+    private PriorityQueue<Integer> maxHeap; // left side
+    private PriorityQueue<Integer> minHeap; // right side
 
     public MedianFinder() {
-        head= null;
-        middle = head;
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder()); // max-heap
+        minHeap = new PriorityQueue<>(); // min-heap
     }
 
-    void add(Node newNode){
-        Node temp = head;
-        Boolean isMiddle = false;
-        if(newNode.val >= middle.val){
-            temp=middle;
-            isMiddle = true;
-        }
-
-        while(temp.next != null && newNode.val>temp.next.val){
-            temp=temp.next;
-        }
-        newNode.next= temp.next;
-        newNode.prev= temp;
-        temp.next = newNode;
-        if(newNode.next!=null)newNode.next.prev = newNode;
-        if(isMiddle){
-            if(n%2==0)middle= middle.next;
-        }else{
-            if(n%2==0)return;
-            middle= middle.prev;
-        }
-
-    }
-    
     public void addNum(int num) {
-        Node newNode = new Node(num);
-
-        if (head == null) {
-            newNode.next = head;
-            head = newNode;
-            middle=head;
-            n++;
-            return;
-        }else if(num< head.val){
-            newNode.next = head;
-            head.prev = newNode;
-            head= newNode;
-            if(n%2!=0)middle = middle.prev;
-            n++;
-            return;
+        // Step 1: Add to maxHeap
+        if (maxHeap.isEmpty() || num <= maxHeap.peek()) {
+            maxHeap.offer(num);
+        } else {
+            minHeap.offer(num);
         }
-        else {
-            add(newNode);
-            n++;
+
+        // Step 2: Balance heaps
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.offer(maxHeap.poll());
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.offer(minHeap.poll());
         }
     }
-    
+
     public double findMedian() {
-        double median =0;
-        if(n%2!=0){
-            median = middle.val;
-        }else{
-            
-            int a= middle.val;
-            int b= middle.next.val;
-            median = (b+a)/2.0;
-        }
-        return median;
+        if (maxHeap.size() > minHeap.size())
+            return maxHeap.peek();
+
+        return (maxHeap.peek() + minHeap.peek()) / 2.0;
     }
 }
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder obj = new MedianFinder();
- * obj.addNum(num);
- * double param_2 = obj.findMedian();
- */
