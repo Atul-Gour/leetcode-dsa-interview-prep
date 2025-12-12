6d64a755a3072e0890ee198fcab7333cb860d7d4
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2025-12-12 12:34:17 +0530

    Time: 210 ms (58.65%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,5 +1,5 @@
 -- Write your PostgreSQL query statement below
-select p.product_name , sum(o.unit) as unit
+select p.product_name , sum(o.unit)  as unit
 from Products p
 inner join Orders o
 on p.product_id = o.product_id
