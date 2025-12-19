79d162913d129c51bc6cb8c16642a63024ed80b6
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2025-12-19 22:02:37 +0530

    Time: 1393 ms (5.14%) | Memory: 55.5 MB (5.96%) - LeetSync

---
+++ 638-shopping-offers/shopping-offers.java
@@ -0,0 +1,56 @@
+class Solution {
+    static int shopping(List<Integer> price, List<List<Integer>> special, List<Integer> needs , Map<List<Integer>, Integer> memo){
+
+        if (memo.containsKey(needs)) return memo.get(needs);
+
+
+        int n = needs.size();
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += needs.get(i) * price.get(i);
+        }
+
+        for(List<Integer> curr : special){
+
+            boolean canUse = true;
+            for(int i = 0 ; i < n ; i++){
+                if(curr.get(i) > needs.get(i)){
+                    canUse = false;
+                    break;
+                }
+            }
+            if(!canUse) continue;
+
+            for(int i = 0 ; i < n ; i++){
+                needs.set(i, needs.get(i) - curr.get(i));
+            }
+
+            int currAns = curr.get(n) + shopping(price , special , needs ,memo);
+            ans = Math.min(ans , currAns);
+
+            for(int i = 0 ; i < n ; i++){
+                needs.set(i, needs.get(i) + curr.get(i));
+            }
+        }
+        memo.put(needs, ans);
+        return ans;
+    }
+    public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+        int n = price.size();
+        for( int i = 0 ;i < n ; i++){
+            List<Integer> curr = new ArrayList<>();
+            for(int j = 0; j< n ; j++){
+                if(j==i){
+                    curr.add(1);
+                    continue;
+                }
+                curr.add(0);
+            }
+            curr.add(price.get(i));
+            special.add(curr);
+        }
+
+        return shopping(price , special , needs , new HashMap<>());
+    }
+}
