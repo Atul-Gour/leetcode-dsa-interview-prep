241dbc30eba6a634a6170224debb8b54152f0624
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2025-12-19 18:58:31 +0530

    Time: 51 ms (46.26%) | Memory: 47.7 MB (48.25%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -2,22 +2,22 @@ class Solution {
     public boolean canPartition(int[] arr) {
         int n = arr.length;
         int sum = 0;
-        for(int i: arr) sum+=i;
-        if(sum % 2 != 0)return false;
+        for(int i : arr) sum += i;
+
+        if(sum % 2 != 0) return false;
 
         sum /= 2;
-        if(arr[0]>sum)return false;
-        boolean dp[][] = new boolean[n][ sum + 1 ];
+        boolean[][] dp = new boolean[n][sum + 1];
 
         dp[0][0] = true;
-        dp[0][arr[0]] = true;
+        if(arr[0] <= sum) dp[0][arr[0]] = true;
 
-        for(int i = 1 ; i < n ;i++){
-            for(int j = 0 ; j <= sum ; j++){
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= sum; j++){
                 boolean notTake = dp[i-1][j];
                 boolean take = false;
 
-                if(arr[i]<=j){
+                if(arr[i] <= j){
                     take = dp[i-1][j - arr[i]];
                 }
                 dp[i][j] = take || notTake;
