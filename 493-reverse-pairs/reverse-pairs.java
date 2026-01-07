514b8285c44c75a9d55aa705c0203c55c19dbe2a
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-07 16:50:05 +0530

    Time: 474 ms (5.04%) | Memory: 65.9 MB (6.21%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -13,7 +13,6 @@ class Solution {
         for(int i = n - 1 ; i > 0 ; i--){
             long curr = nums[i];
             
-            // Binary search to find curr (faster than linear search)
             int index = Collections.binarySearch(list, curr, Collections.reverseOrder());
             
             list.remove(index);
@@ -28,7 +27,6 @@ class Solution {
                 continue;
             }
             
-            // Binary search for first element <= toFind
             int left = 0, right = list.size() - 1;
             int pos = -1;
             while(left <= right){
