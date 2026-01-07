7a3ff36cb65279d92f6fc17954ce73c7b0e8cd3b
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-07 17:27:45 +0530

    Time: 43 ms (71.19%) | Memory: 64.5 MB (29.67%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -1,46 +1,49 @@
 class Solution {
-    public int reversePairs(int[] nums){
-        int n = nums.length;
-        if(n <= 1) return 0;
-        
-        ArrayList<Long> list = new ArrayList<>();
-        for(int i : nums){
-            list.add((long)i);
+
+    public int reversePairs(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    private int mergeSort(int[] nums, int left, int right) {
+        if (left >= right) return 0;
+
+        int mid = left + (right - left) / 2;
+        int count = 0;
+
+        count += mergeSort(nums, left, mid);
+        count += mergeSort(nums, mid + 1, right);
+
+        int j = mid + 1;
+        for (int i = left; i <= mid; i++) {
+            while (j <= right && (long) nums[i] > 2L * nums[j]) {
+                j++;
+            }
+            count += j - (mid + 1);
         }
-        Collections.sort(list, Collections.reverseOrder());
-        int ans = 0;
-        
-        for(int i = n - 1 ; i > 0 ; i--){
-            long curr = nums[i];
-            
-            int index = Collections.binarySearch(list, curr, Collections.reverseOrder());
-            
-            list.remove(index);
-            
-            if(list.isEmpty()){
-                continue;
+
+        merge(nums, left, mid, right);
+        return count;
+    }
+
+    private void merge(int[] nums, int left, int mid, int right) {
+
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j]) {
+                temp[k++] = nums[i++];
+            } else {
+                temp[k++] = nums[j++];
             }
-            
-            long toFind = curr * 2;
-            
-            if(list.get(0) <= toFind){
-                continue;
-            }
-            
-            int left = 0, right = list.size() - 1;
-            int pos = -1;
-            while(left <= right){
-                int mid = left + (right - left) / 2;
-                if(list.get(mid) > toFind){
-                    pos = mid;
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
-            
-            if(pos >= 0) ans += pos + 1;
         }
-        return ans;
+
+        while (i <= mid) temp[k++] = nums[i++];
+        while (j <= right) temp[k++] = nums[j++];
+
+        for (int p = 0; p < temp.length; p++) {
+            nums[left + p] = temp[p];
+        }
     }
 }
