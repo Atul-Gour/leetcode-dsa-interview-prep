b290543c2236f3ce0d34749d4e500524e518ce8c
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-09 17:41:45 +0530

    Time: 19 ms (63.12%) | Memory: 47 MB (19.52%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,17 +1,50 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-
-        long ans = 0;
         int n = nums.length;
-        
-        for(int i = 0 ;i < n-1 ; i++){
-            int small = nums[i], big = nums[i];
-            for(int j = i + 1 ; j < n ; j++){
-                small = Math.min(small , nums[j]);
-                big = Math.max( big , nums[j] );
-                ans += ( big - small);
-            }
+        long maxSum = 0, minSum = 0;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+        java.util.Stack<Integer> st = new java.util.Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && nums[st.peek()] < nums[i]) st.pop();
+            left[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
         }
-        return ans;
+
+        st.clear();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[st.peek()] <= nums[i]) st.pop();
+            right[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            maxSum += (long)(i - left[i]) * (right[i] - i) * nums[i];
+        }
+
+        st.clear();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && nums[st.peek()] > nums[i]) st.pop();
+            left[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[st.peek()] >= nums[i]) st.pop();
+            right[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            minSum += (long)(i - left[i]) * (right[i] - i) * nums[i];
+        }
+
+        return maxSum - minSum;
     }
 }
