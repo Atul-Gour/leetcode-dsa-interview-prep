7e271772c9641c9fd54c24d70d52cdceab71c3b7
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-09 22:27:25 +0530

    Time: 9 ms (93.13%) | Memory: 47.2 MB (37.35%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
@@ -4,9 +4,6 @@ class Solution {
             return a[1] - b[1];
         });
         int ans = 0;
-        // for(int arr[]: pairs){
-        //     System.out.println(arr[0] +" , "+ arr[1]);
-        // }
         int last = Integer.MIN_VALUE;
         for(int arr[] : pairs){
             if(arr[0] <= last)continue;
