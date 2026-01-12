4e3654460f4c2c92778ddb5bd99d511a9a507dbc
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-12 17:09:56 +0530

    Time: 1 ms (64.76%) | Memory: 43.3 MB (36.04%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,23 +1,53 @@
+class TrieNode {
+    TrieNode[] child = new TrieNode[26];
+    boolean isEnd;
+}
+
 class Solution {
-    public String longestCommonPrefix(String[] arr) {
 
-        Arrays.sort(arr , (a , b) -> (
-            Integer.compare(a.length() , b.length())
-        ));
-        StringBuilder sb = new StringBuilder(arr[0]);
+    void insert(String word, TrieNode root) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if (cur.child[idx] == null)
+                cur.child[idx] = new TrieNode();
+            cur = cur.child[idx];
+        }
+        cur.isEnd = true;
+    }
 
-        for(int i = 1 ; i < arr.length ; i++){
-            String curr = arr[i];
-            int len = 0;
+    String getLCP(TrieNode root) {
+        StringBuilder sb = new StringBuilder();
+        TrieNode cur = root;
 
-            while(len < curr.length() && len < sb.length() && sb.charAt(len) == curr.charAt(len)){
-                len++;
+        while (true) {
+            int count = 0;
+            int next = -1;
+
+            for (int i = 0; i < 26; i++) {
+                if (cur.child[i] != null) {
+                    count++;
+                    next = i;
+                }
             }
 
-            if ( len == 0 ) return "";
-            sb.setLength(len);
-            
+            if (count != 1 || cur.isEnd)
+                break;
+
+            sb.append((char) (next + 'a'));
+            cur = cur.child[next];
         }
+
         return sb.toString();
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        TrieNode root = new TrieNode();
+
+        for (String s : strs) {
+            insert(s, root);
+        }
+
+        return getLCP(root);
     }
 }
