f0f2b2d292fb911dc63ed008f539b56e0258bf5e
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-12 13:48:04 +0530

    Time: 37 ms (8.41%) | Memory: 75.5 MB (6.87%) - LeetSync

--- 1062-partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.java
+++ 1062-partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.java
@@ -5,18 +5,36 @@ class Solution {
 
         int pre[] = new int[n];
 
+        HashMap<Integer , ArrayList<Integer>> map1 = new HashMap<>(); 
+        HashMap<Integer , ArrayList<Integer>> map2 = new HashMap<>();
+
         pre[0] = arr[0];
+        map1.computeIfAbsent(pre[0] , f -> new ArrayList<>()).add(0);
 
         for(int i = 1 ; i < n ; i++){
             pre[i] = pre[i-1] + arr[i];
+            map1.computeIfAbsent(pre[i] , f -> new ArrayList<>()).add(i);
         }
 
-        for(int i = 1 ; i < n-1 ; i++){
-            for(int j = i ; j < n-1 ; j++){
-                long first = pre[i-1];
-                long second = pre[j] - pre[i-1];
-                long third = pre[n-1] - pre[j];
-                if(first == second && second == third) return true;
+        map2.computeIfAbsent(arr[n-1] , f -> new ArrayList<>()).add(n-1);
+
+        int sum = arr[n-1];
+
+        for(int i = n-2 ; i>=0 ; i--){
+            sum += arr[i];
+            map2.computeIfAbsent(sum , f -> new ArrayList<>()).add(i);
+        }
+
+        for(int key : map1.keySet()){
+            if(!map2.containsKey(key)) continue;
+            ArrayList<Integer> iList = map1.get(key);
+            ArrayList<Integer> jList = map2.get(key);
+
+            for(int i : iList){
+                for(int j : jList){
+                    if(i >= j-1 || j < 2 || i >= n-2)continue;
+                    if(pre[j-1] - pre[i] == key)return true;
+                }
             }
         }
         return false;
