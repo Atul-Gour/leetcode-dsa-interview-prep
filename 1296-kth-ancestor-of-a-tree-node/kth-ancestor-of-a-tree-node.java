fb32f49616487d8e877cfdf4fa8a16ca07289f9a
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-13 17:22:11 +0530

    Time: 70 ms (39.21%) | Memory: 107.9 MB (53.49%) - LeetSync

--- 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.java
+++ 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.java
@@ -1,33 +1,45 @@
 class TreeAncestor {
 
-    int[][] parent;
-    int LOG = 17;
+    final int max = 17;
+    static int table[][];
 
-    public TreeAncestor(int n, int[] p) {
-        parent = new int[n][LOG];
+    void build( int[] parent ){
+        int n = parent.length;
 
-        for (int i = 0; i < n; i++) {
-            parent[i][0] = p[i];
+        for(int i = 0 ; i < n ; i++ ){
+            table[i][0] = parent[i];
         }
 
-        for (int j = 1; j < LOG; j++) {
-            for (int i = 0; i < n; i++) {
-                int mid = parent[i][j - 1];
-                if (mid == -1)
-                    parent[i][j] = -1;
-                else
-                    parent[i][j] = parent[mid][j - 1];
+        for(int j = 1 ; j < max ; j++ ){
+            for(int i = 0 ; i < n ; i++ ){
+                int mid = table[i][j-1];
+
+                if(mid == -1)
+                    table[i][j] = -1;
+                else 
+                    table[i][j] = table[mid][j-1];
             }
         }
     }
 
+    public TreeAncestor(int n, int[] parent) {
+        table = new int[n][max];
+        build(parent);
+    }
+    
     public int getKthAncestor(int node, int k) {
-        for (int j = 0; j < LOG; j++) {
-            if ((k & (1 << j)) != 0) {
-                node = parent[node][j];
-                if (node == -1) return -1;
+        for(int i = 0 ; i < max ; i++ ){
+            if( ( k & (1 << i)) != 0){
+                node = table[node][i];
             }
+            if(node == -1)return -1;
         }
         return node;
     }
 }
+
+/**
+ * Your TreeAncestor object will be instantiated and called as such:
+ * TreeAncestor obj = new TreeAncestor(n, parent);
+ * int param_1 = obj.getKthAncestor(node,k);
+ */
