089e4692e8098612591bc50b8bde1a062b62fe85
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-15 17:02:08 +0530

    Time: 15 ms (26.36%) | Memory: 43.4 MB (57.62%) - LeetSync

--- 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.java
+++ 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.java
@@ -1,42 +1,70 @@
 class Solution {
-    public int minMovesToMakePalindrome(String s) {
-        char[] arr = s.toCharArray();
-        int i = 0, j = arr.length - 1;
-        int moves = 0;
 
-        while (i < j) {
-            if (arr[i] == arr[j]) {
-                i++;
-                j--;
+    public int minMovesToMakePalindrome(String s) {
+        char[] a = s.toCharArray();
+        int ans = 0;
+
+        int l = 0, r = a.length - 1;
+
+        while (l < r) {
+
+            // already matched
+            if (a[l] == a[r]) {
+                l++;
+                r--;
                 continue;
             }
 
-            int k = j;
-            while (k > i && arr[k] != arr[i]) {
-                k--;
+            // find partner for left
+            int k1 = r;
+            while (k1 > l && a[k1] != a[l]) k1--;
+
+            // find partner for right
+            int k2 = l;
+            while (k2 < r && a[k2] != a[r]) k2++;
+
+            // left has no partner → odd char
+            if (k1 == l) {
+                swap(a, l, l + 1);
+                ans++;
+                continue;
             }
 
-            if (k == i) {
-                swap(arr, i, i + 1);
-                moves++;
-            } 
-            else {
-                while (k < j) {
-                    swap(arr, k, k + 1);
-                    moves++;
-                    k++;
+            // right has no partner → odd char
+            if (k2 == r) {
+                swap(a, r, r - 1);
+                ans++;
+                continue;
+            }
+
+            int costLeft  = r - k1; // cost to fix left
+            int costRight = k2 - l; // cost to fix right
+
+            if (costLeft <= costRight) {
+                while (k1 < r) {
+                    swap(a, k1, k1 + 1);
+                    ans++;
+                    k1++;
                 }
-                i++;
-                j--;
+                l++;
+                r--;
+            } else {
+                while (k2 > l) {
+                    swap(a, k2, k2 - 1);
+                    ans++;
+                    k2--;
+                }
+                l++;
+                r--;
             }
         }
 
-        return moves;
+        return ans;
     }
 
-    private void swap(char[] arr, int i, int j) {
-        char temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    private void swap(char[] a, int i, int j) {
+        char t = a[i];
+        a[i] = a[j];
+        a[j] = t;
     }
 }
