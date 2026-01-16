8ef4e2f69c9486fee2abbc42a951a7ce24653bec
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-16 17:22:11 +0530

    Time: 2 ms (94.93%) | Memory: 43 MB (68.42%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.java
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.java
@@ -1,34 +1,21 @@
 class Solution {
     public int lastStoneWeightII(int[] stones) {
-        int n = stones.length ;
-        if(n == 1)return stones[0];
         int total = 0;
+        for (int s : stones) total += s;
 
-        for(int ele : stones) total += ele;
+        int target = total / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
 
-        int target = total/2;
-
-        boolean[][] dp = new boolean[n][target + 1];
-
-        dp[0][0] = true;
-        dp[0][stones[0]] = true;
-
-        for(int i = 1 ; i < n ; i++){
-            for(int j = 0 ; j <= target ; j++){
-
-                if(dp[i-1][j])
-                    dp[i][j] = true;
-                else {
-                    if(j >= stones[i])
-                        dp[i][j] = dp[i-1][j - stones[i]];
-
-                    else dp[i][j] = false;
-                }
+        for (int stone : stones) {
+            for (int j = target; j >= stone; j--) {
+                dp[j] = dp[j] || dp[j - stone];
             }
         }
-        for(int j = target ; j >= 0 ; j--){
-            if(dp[n-1][j]){
-                return Math.abs(total - j - j);
+
+        for (int j = target; j >= 0; j--) {
+            if (dp[j]) {
+                return total - 2 * j;
             }
         }
         return 0;
