18ab4fcfc4fff2ad1baa47c9a5a936edfbccce58
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-17 15:51:43 +0530

    Time: 3 ms (82.37%) | Memory: 45.3 MB (79.36%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,38 +1,15 @@
 class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        
+        int[] dp = new int[n + 1];
 
-    public int minimumTotal(List<List<Integer>> list) {
-        int ans = 0;
-        int n = list.size();
-        int dp[][] = new int [n][n];
-
-        for(int arr[] :dp){
-            Arrays.fill( arr , Integer.MAX_VALUE);
-        }
-
-        for(int i = 0 ; i < n ; i++){
-            dp[n-1][i] = list.get(n-1).get(i);
-        }
-
-        int newI ;
-        int newJ ;
-
-        for(int i = n - 1 ; i > 0 ; i-- ){
-
-            newI = i-1;
-            for(int j = 0 ; j <= i ; j++){
-
-                newJ = j-1;
-                if(newJ <= newI && newJ >= 0)
-                    dp[newI][newJ] = Math.min( dp[newI][newJ] , list.get(newI).get(newJ) + dp[i][j]);
-
-                newJ = j;
-                if(newJ <= newI && newJ >= 0)
-                    dp[newI][newJ] = Math.min( dp[newI][newJ] , list.get(newI).get(newJ) + dp[i][j]);
-                
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
             }
-            
         }
 
-        return dp[0][0];
+        return dp[0];
     }
 }
