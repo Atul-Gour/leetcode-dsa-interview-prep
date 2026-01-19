07b7db056d43f9a26ab298e4f9de6d28c16789b5
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-19 17:46:37 +0530

    Time: 9 ms (96.59%) | Memory: 45.8 MB (98.56%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -3,15 +3,14 @@ class Solution {
         int n =  s.length();
         int arr[] = new int[3];
         int ans = 0;
+        int j = 0;
+        int unique = 0;
         
         for(int i = 0 ; i < n ; i++ ){
-            arr = new int[3];
             // System.out.println( "chechking " + i );
-            int unique = 0;
-            int j = i;
+            
             while(unique != 3 && j < n){
                 int curr = s.charAt(j) - 'a';
-
                 if( arr[curr] == 0 )unique++;
                 arr[curr]++;
                 j++;
@@ -20,6 +19,10 @@ class Solution {
                 // System.out.println(i + " , "+ j);
                 ans += ( n - j + 1);
             }
+
+            int curr = s.charAt(i) - 'a';
+            arr[curr]--;
+            if( arr[curr] == 0 )unique--;
         }
 
         return ans;
