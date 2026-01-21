6e0cae7ff14c065e5d7084180732ccb7ea9ea486
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-21 19:36:44 +0530

    Time: 31 ms (95.12%) | Memory: 48.9 MB (95.93%) - LeetSync

---
+++ 2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countDistinct(int[] nums, int k, int p) {
+        Set<Long> set = new HashSet<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            long hash = 0;
+            long base = 200001;  
+
+            for (int j = i; j < n; j++) {
+                if (nums[j] % p == 0) cnt++;
+                if (cnt > k) break;
+
+                hash = hash * base + nums[j];
+                set.add(hash);
+            }
+        }
+        return set.size();
+    }
+}
