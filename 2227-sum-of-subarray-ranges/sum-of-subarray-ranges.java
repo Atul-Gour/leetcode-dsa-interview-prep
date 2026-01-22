dbf092b33c46a081a6ba425980de9a458e90bdf1
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-22 18:14:07 +0530

    Time: 15 ms (95.13%) | Memory: 46.3 MB (83.74%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,78 +1,19 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
         int n = nums.length;
-        Stack<Integer> st = new Stack<>();
-        int[] left = new int[n];
-        int[] right = new int[n];
-        long ans = 0;
+        long totalSum = 0; 
 
-        for(int i = 0 ; i < n ; i++ ){
-            while(!st.isEmpty() && nums[st.peek()] <= nums[i] ){ 
-                st.pop();
+        for (int i = 0; i < n; i++) {
+            int minVal = nums[i];
+            int maxVal = nums[i];
+            for (int j = i; j < n; j++) {
+                minVal = Math.min(minVal, nums[j]);
+                maxVal = Math.max(maxVal, nums[j]);
+                
+              
+                totalSum += (maxVal - minVal);
             }
-
-            if(st.isEmpty())
-                left[i] = -1;
-            else
-                left[i] = st.peek();
-            
-            st.push(i);
         }
-        st.clear();
-        for(int i = n-1 ; i >= 0 ; i-- ){
-            while(!st.isEmpty() && nums[st.peek()] < nums[i] ){
-                st.pop();
-            }
-
-            if(st.isEmpty())
-                right[i] = n;
-            else
-                right[i] = st.peek();
-            
-            st.push(i);
-        }
-
-
-
-        for(int i = 0 ; i < n ; i++){
-            long contri = (long)( i - left[i] )*( right[i] - i ) * nums[i];
-            ans += contri;
-        }
-
-        st.clear();
-
-        for(int i = 0 ; i < n ; i++ ){
-            while(!st.isEmpty() && nums[st.peek()] >= nums[i] ){ 
-                st.pop();
-            }
-
-            if(st.isEmpty())
-                left[i] = -1;
-            else
-                left[i] = st.peek();
-            
-            st.push(i);
-        }
-        st.clear();
-        for(int i = n-1 ; i >= 0 ; i-- ){
-            while(!st.isEmpty() && nums[st.peek()] > nums[i] ){
-                st.pop();
-            }
-
-            if(st.isEmpty())
-                right[i] = n;
-            else
-                right[i] = st.peek();
-            
-            st.push(i);
-        }
-
-        for(int i = 0 ; i < n ; i++){
-            long contri = (long)( i - left[i] )*( right[i] - i ) * nums[i];
-            ans -= contri;
-        }
-        
-        return ans;
-
+        return totalSum;
     }
 }
