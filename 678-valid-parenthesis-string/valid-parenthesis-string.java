5efcd37a247bb2707ca50684534afe5a2272e460
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-23 18:23:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (40.56%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,33 +1,24 @@
 class Solution {
-    Boolean[][] memo;
-    
     public boolean checkValidString(String s) {
-        memo = new Boolean[s.length()][s.length() + 1];
-        return solve(s, 0, 0);
-    }
-    
-    private boolean solve(String s, int index, int openCount) {
-        if (openCount < 0) return false;
-        if (index == s.length()) return openCount == 0;
+        int minOpen = 0;
+        int maxOpen = 0;
         
-        if (memo[index][openCount] != null) {
-            return memo[index][openCount];
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                minOpen++;
+                maxOpen++;
+            } else if (c == ')') {
+                minOpen--;
+                maxOpen--;
+            } else {
+                minOpen--;
+                maxOpen++;
+            }
+            
+            if (maxOpen < 0) return false;
+            minOpen = Math.max(minOpen, 0);
         }
         
-        boolean result = false;
-        char c = s.charAt(index);
-        
-        if (c == '(') {
-            result = solve(s, index + 1, openCount + 1);
-        } else if (c == ')') {
-            result = solve(s, index + 1, openCount - 1);
-        } else {
-            result = solve(s, index + 1, openCount) ||
-                     solve(s, index + 1, openCount + 1) ||
-                     solve(s, index + 1, openCount - 1);
-        }
-        
-        memo[index][openCount] = result;
-        return result;
+        return minOpen == 0;
     }
 }
