9a6f93a7281da4060cdd8ace276f8f51e59b861f
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-23 11:50:57 +0530

    Time: 15 ms (61.67%) | Memory: 51.5 MB (65.48%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,18 +1,32 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        Arrays.sort(s);
-        Arrays.sort(g);
-        int j = 0;
-        for(int i = 0 ; i < g.length ; i++){
-            while( j < s.length && s[j] < g[i] ) j++;
 
-            if(j == s.length)return i;
-            else {
+        // Sort greed factors of children
+        Arrays.sort(g);
+
+        // Sort cookie sizes
+        Arrays.sort(s);
+
+        int n = g.length; // number of children
+        int m = s.length; // number of cookies
+
+        int i = 0; // pointer for children
+        int j = 0; // pointer for cookies
+
+        // Try to assign cookies to children
+        while (i < n && j < m) {
+
+            // If current cookie can satisfy current child
+            if (s[j] >= g[i]) {
+                i++; // child is satisfied
+                j++; // cookie is used
+            } else {
+                // Cookie too small, try next bigger cookie
                 j++;
             }
-
         }
 
-        return g.length;
+        // Number of satisfied children
+        return i;
     }
 }
