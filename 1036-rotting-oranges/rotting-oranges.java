0b839cc4407f96735dc9d9bba23b0d2d88acabc6
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-27 19:49:26 +0530

    Time: 4 ms (6.41%) | Memory: 46.3 MB (5.43%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,45 +1,81 @@
 class Solution {
-    public int orangesRotting(int[][] grid) {
-        int n = grid.length, m = grid[0].length;
+
+    private void bfs(int arr[], int minutes[][], int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        boolean visited[][] = new boolean[n][m];
+
         Queue<int[]> q = new LinkedList<>();
-        int fresh = 0;
+        q.offer(arr);
+
+        int time = 0;
+        int[][] dirs = new int[][] { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
+
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                int curr[] = q.poll();
+                minutes[curr[0]][curr[1]] = Math.min( minutes[curr[0]][curr[1]] , time );
+                visited[curr[0]][curr[1]] = true;
+
+                for (int[] d : dirs) {
+                    int newI = curr[0] + d[0];
+                    int newJ = curr[1] + d[1];
+                    if (newI >= 0 && newI < n && newJ >= 0 && newJ < m && !visited[newI][newJ] && grid[newI][newJ] == 1) {
+                        q.offer(new int[] { newI, newJ });
+                    }
+                }
+            }
+
+            time++;
+        }
+
+    }
+
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        ArrayList<int[]> rottenList = new ArrayList<>();
+        int minutes[][] = new int[n][m];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
-                    q.offer(new int[]{i, j});
-                } else if (grid[i][j] == 1) {
-                    fresh++;
+                    rottenList.add(new int[] { i, j });
+                }
+                if( grid[i][j] != 0 )
+                    minutes[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+
+        for (int[] arr : rottenList) {
+            bfs(arr, minutes , grid);
+        }
+
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < m; j++) {
+        //         System.out.print( minutes[i][j] + " , ");
+        //     }
+        //     System.out.println();
+        // }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if( grid[i][j] == 1 ){
+                    if( minutes[i][j] == Integer.MAX_VALUE ) return -1;
+
+                    ans = Math.max( ans , minutes[i][j] );
                 }
             }
         }
 
-        if (fresh == 0) return 0;
-
-        int time = 0;
-        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-            boolean rotted = false;
-
-            for (int k = 0; k < size; k++) {
-                int[] cur = q.poll();
-                for (int[] d : dir) {
-                    int ni = cur[0] + d[0];
-                    int nj = cur[1] + d[1];
-
-                    if (ni >= 0 && nj >= 0 && ni < n && nj < m && grid[ni][nj] == 1) {
-                        grid[ni][nj] = 2;
-                        fresh--;
-                        q.offer(new int[]{ni, nj});
-                        rotted = true;
-                    }
-                }
-            }
-            if (rotted) time++;
-        }
-
-        return fresh == 0 ? time : -1;
+        return ans;
     }
 }
