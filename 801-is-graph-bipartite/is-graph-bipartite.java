a710d4e02253942cb976279c04bafd1e894e34cd
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-28 19:25:05 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (29.67%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,37 +1,30 @@
 class Solution {
-    private boolean check( int node , int[][] graph , int[] color){
-        int n = graph.length;
+    private boolean check(int curr, int[][] graph, int[] color) {
 
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(node);
-        color[node] = 0;
+        for (int neigh : graph[curr]) {
 
-        while(!q.isEmpty()){
-
-            int curr = q.poll();
-            
-            for( int neigh : graph[curr]){
-
-                if( color[neigh] == color[curr] ){
-                    return false;
-                }
-                else if( color[neigh] == -1 ){
-                    color[neigh] = 1 - color[curr];
-                    q.offer( neigh );
-                }
+            if (color[neigh] == color[curr]) {
+                return false;
+            } else if (color[neigh] == -1) {
+                color[neigh] = 1 - color[curr];
+                if( check( neigh , graph , color ) == false )return false;
             }
+
         }
         return true;
+
     }
+
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
         int color[] = new int[n];
 
-        Arrays.fill(color , -1);
+        Arrays.fill(color, -1);
 
-        for(int i = 0 ; i < n ; i++){
-            if(color[i] == -1){
-                if(check( i , graph , color ) == false )
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                color[i] = 0;
+                if (check(i, graph, color) == false)
                     return false;
             }
         }
