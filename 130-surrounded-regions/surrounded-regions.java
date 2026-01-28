9af34363a81e5209ee488fd08fa94402a9586f1c
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-28 17:22:59 +0530

    Time: 2 ms (82.55%) | Memory: 47.8 MB (48.18%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,55 +1,39 @@
 class Solution {
-    private void bfs(int x, int y, boolean visited[][], char[][] board) {
-
-        int n = board.length;
-        int m = board[0].length;
-        int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-        Queue<int[]> q = new LinkedList<>();
-        ArrayList<int[]> list = new ArrayList<>();
-
-        q.add(new int[] { x, y });
-        visited[x][y] = true;
-        boolean surrounded = true;
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int[] curr = q.poll();
-                list.add(new int[] { curr[0], curr[1] });
-
-                for (int d[] : dirs) {
-                    int newI = curr[0] + d[0];
-                    int newJ = curr[1] + d[1];
-                    if (newI < n && newI >= 0 && newJ < m && newJ >= 0 && board[newI][newJ] == 'O'
-                            && !visited[newI][newJ]) {
-                        if (newI == n - 1 || newJ == m - 1 || newI == 0 || newJ == 0)
-                            surrounded = false;
-
-                        visited[newI][newJ] = true;
-                        q.add(new int[] { newI, newJ });
-                    }
-                }
-            }
-        }
-
-        if (surrounded) {
-            for (int[] cell : list) {
-                board[cell[0]][cell[1]] = 'X';
-            }
-        }
-
-    }
+    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
 
     public void solve(char[][] board) {
-        int n = board.length;
-        int m = board[0].length;
+        int n = board.length, m = board[0].length;
 
-        boolean[][] visited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            if (board[i][0] == 'O') bfs(i, 0, board);
+            if (board[i][m-1] == 'O') bfs(i, m-1, board);
+        }
+
+        for (int j = 0; j < m; j++) {
+            if (board[0][j] == 'O') bfs(0, j, board);
+            if (board[n-1][j] == 'O') bfs(n-1, j, board);
+        }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (!visited[i][j] && board[i][j] == 'O' && i != n - 1 && j != m - 1 && i != 0 && j != 0) {
-                    bfs(i, j, visited, board);
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                else if (board[i][j] == '#') board[i][j] = 'O';
+            }
+        }
+    }
+
+    private void bfs(int x, int y, char[][] board) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        board[x][y] = '#';
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int[] d : dirs) {
+                int ni = cur[0] + d[0], nj = cur[1] + d[1];
+                if (ni >= 0 && nj >= 0 && ni < board.length && nj < board[0].length && board[ni][nj] == 'O') {
+                    board[ni][nj] = '#';
+                    q.add(new int[]{ni, nj});
                 }
             }
         }
