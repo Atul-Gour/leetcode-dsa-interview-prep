812a9d2dd57bf2f75d1aa961b90c0c84bf0fb4d1
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-28 19:03:21 +0530

    Time: 1 ms (61.39%) | Memory: 47.5 MB (14.14%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -0,0 +1,40 @@
+class Solution {
+    private boolean check( int node , int[][] graph , int[] color){
+        int n = graph.length;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(node);
+        color[node] = 0;
+
+        while(!q.isEmpty()){
+
+            int curr = q.poll();
+            
+            for( int neigh : graph[curr]){
+
+                if( color[neigh] == color[curr] ){
+                    return false;
+                }
+                else if( color[neigh] == -1 ){
+                    color[neigh] = 1 - color[curr];
+                    q.offer( neigh );
+                }
+            }
+        }
+        return true;
+    }
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int color[] = new int[n];
+
+        Arrays.fill(color , -1);
+
+        for(int i = 0 ; i < n ; i++){
+            if(color[i] == -1){
+                if(check( i , graph , color ) == false )
+                    return false;
+            }
+        }
+        return true;
+    }
+}
