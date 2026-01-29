659e143f812eb0463f6ae0ccb8df4b54e2f4bc86
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-29 18:20:42 +0530

    Time: 6 ms (75.44%) | Memory: 47.3 MB (44.45%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,48 +1,35 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+
         ArrayList<Integer>[] adj = new ArrayList[numCourses];
-        Queue<Integer> q = new LinkedList<>();
+        int[] in = new int[numCourses];
 
-        int in[] = new int[numCourses];
-
-        for(int i = 0 ; i < numCourses ; i++ ){
+        for (int i = 0; i < numCourses; i++) {
             adj[i] = new ArrayList<>();
         }
-        for( int e[] : prerequisites ){
+
+        for (int[] e : prerequisites) {
             adj[e[1]].add(e[0]);
-        }
-        
-        for(int i = 0 ; i < numCourses ; i++){
-            for(int neigh : adj[i]){
-                in[neigh]++;
-            }
-        }
-        
-        for(int i = 0 ; i < numCourses ; i++){
-            if( in[i] == 0 ){
-                q.offer(i);
-            }
+            in[e[0]]++;
         }
 
-        ArrayList<Integer> ans = new ArrayList<>();
-        
-        boolean visited[] = new boolean[numCourses];
-        
-        while( !q.isEmpty() ){
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (in[i] == 0) q.offer(i);
+        }
+
+        int count = 0;
+
+        while (!q.isEmpty()) {
             int curr = q.poll();
-            
-            for(int neigh : adj[curr]){
-                in[neigh]--;
-                if( in[neigh] == 0 ){
-                    q.offer(neigh);
-                }
-            }
-            
-            ans.add(curr);
-        }
-        
-        
-        return ans.size() == numCourses ;
-    }
+            count++;
 
+            for (int neigh : adj[curr]) {
+                in[neigh]--;
+                if (in[neigh] == 0) q.offer(neigh);
+            }
+        }
+
+        return count == numCourses;
+    }
 }
