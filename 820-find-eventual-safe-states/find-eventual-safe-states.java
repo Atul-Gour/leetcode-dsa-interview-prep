cd7ddafb709f4c65e1ec41f0bb43c5a389aab0e0
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-29 12:38:11 +0530

    Time: 4 ms (99.65%) | Memory: 63.6 MB (27.60%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,52 +1,49 @@
 class Solution {
 
-    private boolean dfsCheck(
+    private boolean dfs(
             int node,
             int[][] graph,
-            int[] vis,
-            int[] pathVis,
-            int[] check
+            boolean[] vis,
+            boolean[] pathVis,
+            boolean[] safe
     ) {
 
-        vis[node] = 1;
-        pathVis[node] = 1;
-        check[node] = 0;
+        vis[node] = true;
+        pathVis[node] = true;
+        safe[node] = false; // assume unsafe
 
         for (int neigh : graph[node]) {
-
-            if (vis[neigh] == 0) {
-                if (dfsCheck(neigh, graph, vis, pathVis, check)) {
+            if (!vis[neigh]) {
+                if (dfs(neigh, graph, vis, pathVis, safe)) {
                     return true;
                 }
-            }
-            else if (pathVis[neigh] == 1) {
-                return true;
+            } else if (pathVis[neigh]) {
+                return true; // cycle
             }
         }
 
-        check[node] = 1;
-        pathVis[node] = 0;
+        safe[node] = true;   // proven safe
+        pathVis[node] = false;
         return false;
     }
 
     public List<Integer> eventualSafeNodes(int[][] graph) {
 
         int n = graph.length;
-        int[] vis = new int[n];
-        int[] pathVis = new int[n];
-        int[] check = new int[n];
+        boolean[] vis = new boolean[n];
+        boolean[] pathVis = new boolean[n];
+        boolean[] safe = new boolean[n];
 
         for (int i = 0; i < n; i++) {
-            if (vis[i] == 0) {
-                dfsCheck(i, graph, vis, pathVis, check);
+            if (!vis[i]) {
+                dfs(i, graph, vis, pathVis, safe);
             }
         }
 
         List<Integer> ans = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            if (check[i] == 1) ans.add(i);
+            if (safe[i]) ans.add(i);
         }
-
         return ans;
     }
 }
