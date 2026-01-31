d621c6f79d0f64ac059c1a635595b1b51353d9f8
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-01-31 17:49:50 +0530

    Time: 190 ms (21.90%) | Memory: 48.3 MB (56.09%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,78 +1,46 @@
 class Solution {
 
-    private static HashMap<String, ArrayList<String>> buildOneDiffMap(List<String> words) {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
 
-        HashMap<String, ArrayList<String>> patternMap = new HashMap<>();
-        HashMap<String, ArrayList<String>> result = new HashMap<>();
-
-        // initialize result map
-        for (String w : words) {
-            result.put(w, new ArrayList<>());
+        Queue<String> q = new LinkedList<>();
+        int level = 2;
+        HashSet<String> set = new HashSet<>();
+        
+        for( String s : wordList ){
+            set.add(s);
         }
 
-        // build pattern map
-        for (String word : words) {
-            for (int i = 0; i < word.length(); i++) {
-
-                String pattern =
-                        word.substring(0, i) + "*" + word.substring(i + 1);
-
-                patternMap
-                        .computeIfAbsent(pattern, k -> new ArrayList<>())
-                        .add(word);
-            }
+        if( !set.contains(endWord)){
+            return 0;
         }
-
-        // build final adjacency list
-        for (ArrayList<String> group : patternMap.values()) {
-            if (group.size() > 1) {
-                for (String a : group) {
-                    for (String b : group) {
-                        if (!a.equals(b)) {
-                            result.get(a).add(b);
+        
+        q.offer(beginWord);
+        set.remove(beginWord);
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            
+            while(size-- > 0){
+                String curr = q.poll();
+                
+                for(int i = 0 ; i < curr.length() ; i++){
+                    for( char ch = 'a' ; ch <= 'z'  ; ch++ ){
+                        String newCurr = curr.substring(0 , i) + ch + curr.substring(i + 1 , curr.length());
+                        if(set.contains(newCurr)){
+                            
+                            if(newCurr.equals(endWord)) return level;
+                            
+                            q.offer(newCurr);
+                            set.remove( newCurr );
                         }
                     }
                 }
+                
             }
+            level++;
         }
-
-        return result;
+        
+        return 0;
     }
-
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-
-    if (!wordList.contains(endWord)) return 0;
-
-    wordList.add(beginWord);
-    HashMap<String, ArrayList<String>> map = buildOneDiffMap(wordList);
-
-    Queue<String> q = new LinkedList<>();
-    HashSet<String> visited = new HashSet<>();
-
-    q.offer(beginWord);
-    visited.add(beginWord);
-
-    int cost = 1;
-
-    while (!q.isEmpty()) {
-        int size = q.size();
-
-        for (int i = 0; i < size; i++) {
-            String curr = q.poll();
-
-            if (curr.equals(endWord)) return cost;
-
-            for (String neigh : map.get(curr)) {
-                if (!visited.contains(neigh)) {
-                    visited.add(neigh);   
-                    q.offer(neigh);
-                }
-            }
-        }
-        cost++;
-    }
-
-    return 0;
-}
 
 }
