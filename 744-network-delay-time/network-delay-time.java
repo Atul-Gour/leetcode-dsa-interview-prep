f1d72f53ac28f8cf2ce628f69867acaa877586ab
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-01 22:16:54 +0530

    Time: 12 ms (59.07%) | Memory: 49.2 MB (60.93%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,46 +1,50 @@
 class Solution {
-    private long key(int a , int b){
-        return ((long) a << 32) | b;
-    }
-    public int networkDelayTime(int[][] edges, int V, int src) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a , b) -> Integer.compare(a[1],b[1]));
-        
-        ArrayList<Integer>[] adj = new ArrayList[V + 1];
-        HashMap<Long , Integer> distCost = new HashMap<>();
-        for(int  i = 1 ; i <= V ; i++ ){
-            adj[i] = new ArrayList<>();
+    public int networkDelayTime(int[][] times, int n, int k) {
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        int[] totalTime = new int[n + 1];
+        Arrays.fill(totalTime, Integer.MAX_VALUE);
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
         }
-        for( int[] e : edges){
-            adj[ e[0] ].add( e[1] );            
-            distCost.put( key(e[0] ,e[1]) , e[2] ); 
+
+        for (int[] time : times) {
+            int from = time[0];
+            int to = time[1];
+            int cost = time[2];
+
+            adj.get(from).add(new int[] { to, cost });
         }
-        
-        int[] ans = new int[V + 1];
-        Arrays.fill( ans , Integer.MAX_VALUE);
-        
-        ans[src] = 0;
-        
-        pq.add(new int[]{src , 0});
-        
-        while( !pq.isEmpty() ){
-            int curr[] = pq.poll();
-            int currNode = curr[0];
-            int currDist = curr[1];
-            if (currDist > ans[currNode]) continue;   
-            for( int neigh : adj[currNode] ){
-                int currToNeigh = distCost.get( key(currNode ,neigh) );
-                if( ans[neigh] > currDist + currToNeigh){
-                    ans[neigh] = currDist + currToNeigh;
-                    pq.offer( new int[]{ neigh , ans[neigh] } );
+
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+
+        queue.offer(new int[] { k, 0 });
+        totalTime[k] = 0;
+
+        while (!queue.isEmpty()) {
+            int[] pair = queue.poll();
+            int node = pair[0];
+            int currTotalTime = pair[1];
+
+            for (int[] neighbour : adj.get(node)) {
+                int time = neighbour[1];
+                int dest = neighbour[0];
+
+                if (currTotalTime + time < totalTime[dest]) {
+                    totalTime[dest] = currTotalTime + time;
+                    queue.offer(new int[] { dest, totalTime[dest] });
                 }
             }
         }
-        int max = -1;
-        ans[0] = 0;
-        for(int i : ans){
-            max = Math.max(max , i);
+
+        int ans = Integer.MIN_VALUE;
+
+        for (int i = 1; i <= n; i++) {
+            if (totalTime[i] == Integer.MAX_VALUE) {
+                return -1;
+            }
+            ans = Math.max(ans, totalTime[i]);
         }
-        
-        return max == Integer.MAX_VALUE ? -1 : max;
+
+        return ans;
     }
 }
