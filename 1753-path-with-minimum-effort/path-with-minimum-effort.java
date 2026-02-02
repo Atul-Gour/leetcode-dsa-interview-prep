678bef9748d1478f5ba635403ff905a9213debb2
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-02 19:10:02 +0530

    Time: 59 ms (25.92%) | Memory: 48 MB (52.87%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+
+        int n = heights.length;
+        int m = heights[0].length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>( (a,b) -> Integer.compare(a[2] ,b[2]));
+        int arr[][] = new int[n][m];
+        int dirs[][] = {{-1 , 0},{1 , 0},{0 , -1},{0 , 1}};
+
+        for(int[] row : arr){
+            Arrays.fill( row , Integer.MAX_VALUE );
+        }
+
+        arr[0][0] = 0;
+
+        pq.offer( new int[]{ 0 , 0 , heights[0][0] });
+
+        while( !pq.isEmpty() ){
+            int currI = pq.peek()[0];
+            int currJ = pq.peek()[1];
+            pq.poll();
+            for(int d[] : dirs){
+                int newI = currI + d[0];
+                int newJ = currJ + d[1];
+                if( newI < n && newI >= 0 && newJ < m && newJ >= 0 ){
+
+                    int maxDif = Math.max(arr[currI][currJ] , Math.abs(heights[newI][newJ] - heights[currI][currJ]) );
+                    if( maxDif < arr[newI][newJ] ){
+                        arr[newI][newJ] = maxDif;
+                        pq.offer( new int[]{ newI , newJ , maxDif } );
+                    }
+                }
+            }
+        }
+        return arr[n-1][m-1];
+    }
+}
