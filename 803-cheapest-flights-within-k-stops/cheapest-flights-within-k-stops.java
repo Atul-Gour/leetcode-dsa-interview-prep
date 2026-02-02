1e92f3c0e124b27f59802460ddad5c792c6f69ee
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-02 19:57:45 +0530

    Time: 1701 ms (5.11%) | Memory: 166.9 MB (5.05%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,30 +1,51 @@
 class Solution {
-
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        
-        int cost[] = new int[n];
+        ArrayList<int[]>[] adj = new ArrayList[n];
+        int[][] arr = new int[n][k + 1];
 
-        Arrays.fill(cost , Integer.MAX_VALUE);
-
-        cost[src] = 0;
-
-        for(int i = 0 ; i <= k ; i++){
-
-            int[] temp = cost.clone();
-
-            for(int[] f: flights){
-                int u = f[0] , v = f[1] , w = f[2];
-
-                if (cost[u] != Integer.MAX_VALUE && cost[u] + w < temp[v]) {
-                    temp[v] = cost[u] + w;
-                }
-            }
-
-            cost = temp;
+        for(int a[] : arr){
+            Arrays.fill(a , Integer.MAX_VALUE);
         }
 
-        
-        return cost[dst] == Integer.MAX_VALUE ? -1 : cost[dst] ;
-        
+        for(int i = 0 ; i < n ; i++){
+            adj[i] = new ArrayList<>();
+        }
+
+        for(int[] flight : flights){
+            adj[flight[0]].add(new int[]{ flight[1] , flight[2] });
+        }
+
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{src , 0});
+        arr[src][0] = 0;
+
+        int stop = 0;
+        int ans = Integer.MAX_VALUE;
+
+        while(!q.isEmpty() && stop <= k){
+            int size = q.size();
+
+            while(size-- > 0){
+                int curr[] = q.poll();
+                int currCity = curr[0];
+                int currCost = curr[1];
+
+                for(int neigh[] : adj[currCity]){
+                    int totalCost = currCost + neigh[1];
+
+                    if( arr[neigh[0]][stop] <= totalCost )continue;
+                    arr[neigh[0]][stop] = totalCost;
+
+                    if(neigh[0] == dst){
+                        ans = Math.min( ans , totalCost );
+                    }else{
+                        q.offer(new int[]{ neigh[0] , totalCost });
+                    }
+                }
+            }
+            
+            stop++;            
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
