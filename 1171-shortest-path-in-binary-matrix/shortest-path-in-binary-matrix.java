b25b92f9b1b81056b6e2aad8a613c60d75a14231
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-02 18:35:39 +0530

    Time: 15 ms (68.53%) | Memory: 47.4 MB (82.04%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,38 +1,38 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
+
         int n = grid.length;
-        int m = grid[0].length;
+        if (grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
 
-        if(grid[0][0] == 1 || grid[n-1][m-1] == 1)return -1;
+        int[][] dist = new int[n][n];
+        for (int[] row : dist) Arrays.fill(row, -1);
 
-        int[][] arr = new int[n][m];
-        Queue<int[]> pq = new LinkedList<int[]>();
-        pq.offer(new int[]{ 0 , 0 });
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0});
+        dist[0][0] = 1;
 
-        for(int a[] : arr){
-            Arrays.fill(a , -1);
-        }
-        arr[0][0] = 1;
+        int[] dx = {-1,-1,-1,0,0,1,1,1};
+        int[] dy = {-1,0,1,-1,1,-1,0,1};
 
-        while( !pq.isEmpty() ){
-            int currI = pq.peek()[0];
-            int currJ = pq.peek()[1];
-            int currCost = arr[currI][currJ];
-            pq.poll();
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0], y = cur[1];
 
-            for( int i = -1 ; i <= 1 ; i++){
-                for(int j = -1 ; j <= 1 ; j++){
-                    if (i == 0 && j == 0) continue;
-                    int newI = currI + i;
-                    int newJ = currJ + j;
-                    if( newI < n && newI >= 0 && newJ < m && newJ >= 0 && grid[newI][newJ] == 0 && arr[newI][newJ] == -1 ){
-                        arr[newI][newJ] = currCost + 1;
-                        if( newI == n-1 && newJ == m-1 )return arr[newI][newJ];
-                        pq.offer(new int[]{ newI , newJ});
-                    }
+            if (x == n-1 && y == n-1) return dist[x][y];
+
+            for (int d = 0; d < 8; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n
+                        && grid[nx][ny] == 0
+                        && dist[nx][ny] == -1) {
+
+                    dist[nx][ny] = dist[x][y] + 1;
+                    q.offer(new int[]{nx, ny});
                 }
             }
         }
-        return arr[n-1][m-1];
+        return -1;
     }
 }
