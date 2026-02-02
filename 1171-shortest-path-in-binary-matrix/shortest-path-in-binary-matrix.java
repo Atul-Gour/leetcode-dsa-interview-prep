f437811c22ef9f4234e6c0ac6ccc573579a570a7
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-02 18:31:26 +0530

    Time: 33 ms (10.15%) | Memory: 48.3 MB (8.63%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        if(grid[0][0] == 1 || grid[n-1][m-1] == 1)return -1;
+
+        int[][] arr = new int[n][m];
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a , b) -> Integer.compare(a[2] , b[2]));
+        pq.offer(new int[]{ 0 , 0 , 1 });
+
+        for(int a[] : arr){
+            Arrays.fill(a , -1);
+        }
+        arr[0][0] = 1;
+
+        while( !pq.isEmpty() ){
+            int currI = pq.peek()[0];
+            int currJ = pq.peek()[1];
+            int currCost = pq.peek()[2];
+            pq.poll();
+
+            for( int i = -1 ; i <= 1 ; i++){
+                for(int j = -1 ; j <= 1 ; j++){
+                    int newI = currI + i;
+                    int newJ = currJ + j;
+                    if( newI < n && newI >= 0 && newJ < m && newJ >= 0 && grid[newI][newJ] == 0 && arr[newI][newJ] == -1 ){
+                        arr[newI][newJ] = currCost + 1;
+                        if( newI == n-1 && newJ == m-1 )return arr[newI][newJ];
+                        pq.offer(new int[]{ newI , newJ , arr[newI][newJ] });
+                    }
+                }
+            }
+        }
+        return arr[n-1][m-1];
+    }
+}
