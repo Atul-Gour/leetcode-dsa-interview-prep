8e1c9b50c4efdb6c9b5f2b3a0b2babd068ff3aab
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-04 00:25:24 +0530

    Time: 32 ms (21.65%) | Memory: 47 MB (20.63%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,51 +1,57 @@
 class Solution {
-    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+    public int findTheCity(int n, int[][] edges, int threshold) {
+
         ArrayList<int[]>[] adj = new ArrayList[n];
+        for(int i=0;i<n;i++) adj[i] = new ArrayList<>();
 
-        for(int i = 0 ; i < n ; i++) adj[i] = new ArrayList<>();
-
-        for( int[] edge : edges ){
-            if( edge[2] > distanceThreshold )continue;
-            adj[ edge[0] ].add( new int[]{ edge[1] , edge[2] } );
-            adj[ edge[1] ].add( new int[]{ edge[0] , edge[2] } );
+        for(int[] e : edges){
+            adj[e[0]].add(new int[]{e[1], e[2]});
+            adj[e[1]].add(new int[]{e[0], e[2]});
         }
-        Queue<int[]> q = new ArrayDeque<>();
-        int cost[] = new int[n];
 
-        int ans = 0;
-        int ansScore = Integer.MAX_VALUE;
+        int answer = 0;
+        int minReachable = Integer.MAX_VALUE;
 
-        for(int i = 0 ; i < n ; i++){
-            int currScore = 0;
-            q.offer( new int[]{ i , 0 } );
+        for(int src = 0; src < n; src++){
 
-            Arrays.fill( cost , Integer.MAX_VALUE );
-            cost[i] = 0;
-            
-            while( !q.isEmpty() ){
-                int curr[] = q.poll();
-                int currNode = curr[0];
-                int currCost = curr[1];
+            int[] dist = new int[n];
+            Arrays.fill(dist, Integer.MAX_VALUE);
+            dist[src] = 0;
 
-                if( cost[ currNode ] < currCost )continue;
+            PriorityQueue<int[]> pq =
+                new PriorityQueue<>((a,b)->a[1]-b[1]);
 
-                for( int neigh[] : adj[currNode] ){
-                    int totalCost = currCost + neigh[1] ;
-                    if( cost[neigh[0]] > totalCost && totalCost <= distanceThreshold ){
-                        if( cost[neigh[0]] == Integer.MAX_VALUE )currScore++;
-                        cost[neigh[0]] = totalCost;
-                        if( totalCost < distanceThreshold ) q.offer( new int[]{ neigh[0] , totalCost } );
+            pq.offer(new int[]{src,0});
+
+            while(!pq.isEmpty()){
+
+                int[] curr = pq.poll();
+                int node = curr[0];
+                int d = curr[1];
+
+                if(d > dist[node]) continue;
+
+                for(int[] neigh : adj[node]){
+                    int newDist = d + neigh[1];
+
+                    if(newDist < dist[neigh[0]]){
+                        dist[neigh[0]] = newDist;
+                        pq.offer(new int[]{neigh[0], newDist});
                     }
                 }
             }
 
-            if( currScore < ansScore ){
-                ansScore = currScore;
-                ans = i;
-            }else if(currScore == ansScore ){
-                ans = Math.max ( ans , i );
+            int reachable = 0;
+            for(int i=0;i<n;i++){
+                if(i != src && dist[i] <= threshold) reachable++;
+            }
+
+            if(reachable <= minReachable){
+                minReachable = reachable;
+                answer = src;
             }
         }
-        return ans;
+
+        return answer;
     }
 }
