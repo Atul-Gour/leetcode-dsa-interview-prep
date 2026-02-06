6bf21d1ae8657da05bc9e2d658798aefe832b59a
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-06 21:48:22 +0530

    Time: 2553 ms (3.49%) | Memory: 42.5 MB (81.79%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -1,7 +1,42 @@
 class Solution {
-    public int divide(int dividend, int divisor) {
-        long ans = (long)dividend / divisor;
+    public int divide(int divid, int divis) {
+        long dividend = divid;
+        long divisor = divis;
+
+        if( dividend < 0 && divisor < 0 ){
+            dividend *= -1;
+            divisor *= -1;
+        }
+
+        int isNegative = 1;
+
+        if( dividend < 0 || divisor < 0 ){
+            isNegative *= -1;
+
+            if( dividend < 0 ) dividend *= -1;
+            if( divisor < 0 ) divisor *= -1;
+        }
+
+        if(divisor > dividend)return 0;
+        if( dividend == divisor ) return 1 * isNegative;
+        if( divisor == 1 ){
+            if( isNegative == 1 ){
+                if( dividend >= 2_147_483_647L ) return 2147483647 * isNegative;
+            }
+            else if( isNegative == -1 ){
+                if( dividend > 2_147_483_647L ) return (int)(2_147_483_648L * isNegative);
+            }
+            else return (int)dividend * isNegative;
+        }
+
+        long ans = 0;
+
+        while( dividend >= divisor ){
+            ans++;
+            dividend -= divisor;
+        }
+
         if( ans >= 2_147_483_648L ) ans = 2147483647;
-        return (int) ans;
+        return (int) ans * isNegative;
     }
 }
