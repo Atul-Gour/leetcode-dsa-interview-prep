593ab000e44a8c9d2e872ef2d5cc54422bbe93a9
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-08 10:37:42 +0530

    Time: 132 ms (86.42%) | Memory: 47.2 MB (92.09%) - LeetSync

--- 4202-maximum-score-using-exactly-k-pairs/maximum-score-using-exactly-k-pairs.java
+++ 4202-maximum-score-using-exactly-k-pairs/maximum-score-using-exactly-k-pairs.java
@@ -5,43 +5,47 @@ class Solution {
         int n = nums1.length;
         int m = nums2.length;
 
-        long[][] curr = new long[m + 1][pairs + 1];
-        long[][] next = new long[m + 1][pairs + 1];
+        long NEG = Long.MIN_VALUE / 2;
 
-        for (int j = 0; j <= m; j++) {
-            for (int k = 0; k <= pairs; k++) {
-                if (k == pairs) {
-                    next[j][k] = 0;
-                } else {
-                    next[j][k] = Long.MIN_VALUE / 2;
-                }
+        long[][] next = new long[m+1][pairs+1];
+        long[][] curr = new long[m+1][pairs+1];
+
+        // Base case → i = n
+        for(int j=0;j<=m;j++){
+            for(int k=0;k<=pairs;k++){
+                if(k == pairs) next[j][k] = 0;
+                else next[j][k] = NEG;
             }
-
         }
-        for( int i = n - 1 ; i >= 0 ; i-- ){
 
+        for(int i=n-1;i>=0;i--){
+
+            // reset curr
             for(int j=0;j<=m;j++){
                 for(int k=0;k<=pairs;k++){
                     if(k == pairs) curr[j][k] = 0;
-                    else curr[j][k] = Long.MIN_VALUE / 2;
+                    else curr[j][k] = NEG;
                 }
             }
 
-            for( int j = m - 1 ; j >= 0 ; j-- ){
-                for( int k = pairs - 1 ; k >= 0 ; k-- ){
-                    
-                    long skip1 = next[j][k];
+            for(int j=m-1;j>=0;j--){
+                for(int k=pairs-1;k>=0;k--){
 
+                    long skip1 = next[j][k];
                     long skip2 = curr[j+1][k];
 
-                    long take = (long) nums1[i] * nums2[j] + next[j+1][k+1];
+                    long take =
+                        (long)nums1[i]*nums2[j]
+                        + next[j+1][k+1];
 
-                    curr[j][k] = Math.max(take, Math.max(skip1, skip2));
-                    
+                    curr[j][k] =
+                        Math.max(take, Math.max(skip1, skip2));
                 }
             }
+
+            long[][] temp = next;
             next = curr;
-            curr = new long[m + 1][pairs + 1];
+            curr = temp;
         }
 
         return next[0][0];
