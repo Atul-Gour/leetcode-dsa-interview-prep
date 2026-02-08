bfb24eff07ab259d857061e72b9b62d60bfd1afe
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-08 10:37:07 +0530

    Time: 166 ms (73.69%) | Memory: 47.3 MB (83.42%) - LeetSync

--- 4202-maximum-score-using-exactly-k-pairs/maximum-score-using-exactly-k-pairs.java
+++ 4202-maximum-score-using-exactly-k-pairs/maximum-score-using-exactly-k-pairs.java
@@ -19,11 +19,16 @@ class Solution {
 
         }
         for( int i = n - 1 ; i >= 0 ; i-- ){
+
+            for(int j=0;j<=m;j++){
+                for(int k=0;k<=pairs;k++){
+                    if(k == pairs) curr[j][k] = 0;
+                    else curr[j][k] = Long.MIN_VALUE / 2;
+                }
+            }
+
             for( int j = m - 1 ; j >= 0 ; j-- ){
                 for( int k = pairs - 1 ; k >= 0 ; k-- ){
-                    if( j == m - 1 ){
-                        curr[j+1][k] = Long.MIN_VALUE / 2;
-                    }
                     
                     long skip1 = next[j][k];
 
