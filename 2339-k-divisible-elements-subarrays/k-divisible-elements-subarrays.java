aa1902a020c2c0a9f585f2c5f7dd88d7568758c8
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-12 20:39:24 +0530

    Time: 195 ms (54.86%) | Memory: 116.7 MB (30.29%) - LeetSync

--- 2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.java
+++ 2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.java
@@ -1,26 +1,24 @@
 class Solution {
     public int countDistinct(int[] nums, int k, int p) {
-        int n = nums.length;
+
         HashSet<ArrayList<Integer>> set = new HashSet<>();
-        int l = 0;
-        int divisibleCount = 0;
-        ArrayDeque<Integer> list = new ArrayDeque<>();
 
-        for( int i = 0 ; i < n ; i++ ){
-            divisibleCount = 0;
-            list = new ArrayDeque<>();
+        for(int i = 0; i < nums.length; i++){
 
-            for(int j = i ; j < n ; j++){
+            int divisibleCount = 0;
+            ArrayList<Integer> list = new ArrayList<>();
 
-                if( nums[j] % p == 0 ) divisibleCount++;
-                list.addLast(nums[j]);
+            for(int j = i; j < nums.length; j++){
 
-                if( divisibleCount > k )break;
+                if(nums[j] % p == 0) divisibleCount++;
 
-                set.add( new ArrayList<>(list) );
+                if(divisibleCount > k) break;
 
+                list.add(nums[j]);
+                set.add(new ArrayList<>(list));
             }
         }
+
         return set.size();
     }
 }
