4f3b6f43dcfcf69553150946d46a080ae807eb2f
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-13 19:29:43 +0530

    Time: 7 ms (98.32%) | Memory: 65.4 MB (56.97%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,40 +1,42 @@
 class Solution {
 
-    void solve(int index, String s, List<String> currList, List<List<String>> ans) {
+    public List<List<String>> partition(String s) {
+
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+
+        for (int end = 0; end < n; end++) {
+            for (int start = 0; start <= end; start++) {
+
+                if (s.charAt(start) == s.charAt(end)) {
+
+                    if (end - start <= 2 || dp[start + 1][end - 1]) {
+                        dp[start][end] = true;
+                    }
+                }
+            }
+        }
+
+        List<List<String>> ans = new ArrayList<>();
+        backtrack(0, s, dp, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    void backtrack(int index, String s, boolean[][] dp,
+                   List<String> curr, List<List<String>> ans) {
 
         if (index == s.length()) {
-            ans.add(new ArrayList<>(currList));
+            ans.add(new ArrayList<>(curr));
             return;
         }
 
         for (int i = index; i < s.length(); i++) {
 
-            if (isPalindrome(s, index, i)) {
-
-                currList.add(s.substring(index, i + 1));
-                solve(i + 1, s, currList, ans);
-                currList.remove(currList.size() - 1);
+            if (dp[index][i]) {
+                curr.add(s.substring(index, i + 1));
+                backtrack(i + 1, s, dp, curr, ans);
+                curr.remove(curr.size() - 1);
             }
         }
-    }
-
-    boolean isPalindrome(String s, int left, int right) {
-
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right))
-                return false;
-
-            left++;
-            right--;
-        }
-
-        return true;
-    }
-
-    public List<List<String>> partition(String s) {
-
-        List<List<String>> ans = new ArrayList<>();
-        solve(0, s, new ArrayList<>(), ans);
-        return ans;
     }
 }
