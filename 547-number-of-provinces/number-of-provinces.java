cb06ac1d9c7aabe18afd9d1e235b62f0d82f2c07
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-18 17:25:19 +0530

    Time: 2 ms (48.13%) | Memory: 47.3 MB (75.34%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,25 +1,43 @@
 class Solution {
 
-    private void dfs(int node, boolean[] visited, int[][] isConnected){
-        visited[node] = true;
+    static private int findParent( int node , int[] parent ){
+        if(parent[node] == node)
+            return node;
 
-        for(int j = 0; j < isConnected.length; j++){
-            if(isConnected[node][j] == 1 && !visited[j]){
-                dfs(j, visited, isConnected);
-            }
-        }
+        return parent[node] = findParent( parent[node] , parent );
     }
 
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        boolean[] visited = new boolean[n];
+
+        int[] parent = new int[n];
+        int[] rank = new int[n];
+
+        for( int i = 0 ; i < n ; i++ ){
+            parent[i] = i;
+        }
+
         int count = 0;
 
         for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                count++;
-                dfs(i, visited, isConnected);
+            for( int j = 0 ; j < n ; j++ ){
+                if( i != j && isConnected[i][j] != 0  ){
+                    int parentA = findParent( i , parent );
+                    int parentB = findParent( j , parent );
+                    if( parentA == parentB )continue;
+
+                    if( rank[parentA] <= rank[parentB] ){
+                        if( rank[parentA] == rank[parentB] ) rank[parentB]++;
+                        parent[parentA] = parentB;
+                    }else{
+                        parent[parentB] = parentA;
+                    }
+                }
             }
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(parent[i] == i) count++;
         }
 
         return count;
