963a5a6ecfbc40354b412fe0b054198e2c3c74d8
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-18 16:17:00 +0530

    Time: 8 ms (56.55%) | Memory: 46.7 MB (32.29%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -1,43 +1,32 @@
-class Node {
-    String s;
-    int freq;
-
-    Node( String s , int freq ){
-        this.s = s;
-        this.freq = freq;
-    }
-}
-
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        HashMap<String , Integer> map = new HashMap<>();
-        PriorityQueue<Node> pq = new PriorityQueue<>((a , b) -> {
-            if( a.freq != b.freq )
-                return Integer.compare( a.freq , b.freq );
-            else
-                return (b.s).compareTo(a.s);
-        });
 
-        List<String> ans = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<>();
 
-        for( String word : words ){
-            map.put( word , map.getOrDefault( word , 0 ) + 1 );
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
 
-        for( String word : map.keySet() ){
-            pq.offer( new Node( word , map.get(word) ) );
+        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> {
+            if (!map.get(a).equals(map.get(b))) {
+                return map.get(a) - map.get(b); 
+            }
+            return b.compareTo(a); 
+        });
 
-            if( pq.size() > k ){
+        for (String word : map.keySet()) {
+            pq.offer(word);
+            if (pq.size() > k) {
                 pq.poll();
             }
         }
 
-        for( int i = 0 ; i < k ; i++ ){
-            ans.add( pq.poll().s );
+        List<String> ans = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            ans.add(pq.poll());
         }
 
         Collections.reverse(ans);
         return ans;
-
     }
 }
