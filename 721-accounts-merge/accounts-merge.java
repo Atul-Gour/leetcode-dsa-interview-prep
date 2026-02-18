922ca98aaca37cfe2c3bba7dd0a3eaf3ed75d9d4
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-18 19:47:28 +0530

    Time: 42 ms (27.91%) | Memory: 49.4 MB (87.46%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -1,72 +1,79 @@
 class Solution {
-    static private String findParent( String curr , HashMap<String , String> parent ){
-        if( curr == parent.get(curr) ){
+    static private String findParent(String curr, HashMap<String, String> parent) {
+        if (curr.equals(parent.get(curr))) {
             return curr;
         }
-        String newParent = findParent( parent.get(curr) , parent );
-        parent.put(curr , newParent );
+        String newParent = findParent(parent.get(curr), parent);
+        parent.put(curr, newParent);
         return newParent;
     }
+
+    static void union(String a, String b,
+            HashMap<String, String> parent,
+            HashMap<String, Integer> rank) {
+
+        String root1 = findParent(a, parent);
+        String root2 = findParent(b, parent);
+
+        if (root1.equals(root2))
+            return;
+
+        int r1 = rank.get(root1);
+        int r2 = rank.get(root2);
+
+        if (r1 < r2) {
+            parent.put(root1, root2);
+        } else if (r1 > r2) {
+            parent.put(root2, root1);
+        } else {
+            parent.put(root2, root1);
+            rank.put(root1, r1 + 1);
+        }
+    }
+
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        HashMap<String , String> parent = new HashMap<>();
-        HashMap<String , String> name = new HashMap<>();
-        HashMap<String , Integer> rank = new HashMap<>();
-        HashMap<String , List<String> > emails = new HashMap<>();
+        HashMap<String, String> parent = new HashMap<>();
+        HashMap<String, String> name = new HashMap<>();
+        HashMap<String, Integer> rank = new HashMap<>();
+        HashMap<String, List<String>> emails = new HashMap<>();
         List<List<String>> ans = new ArrayList<>();
 
-        for( List<String> account : accounts ){
-            int maxRank = 0;
-            String maxRankEmail = "";
-            int m = account.size();
+        for (List<String> account : accounts) {
+            String accName = account.get(0);
 
-            for( int i = 1 ; i < m ; i++ ){
-                int currRank = 0;
-                String currEmail ;
+            for (int i = 1; i < account.size(); i++) {
+                String email = account.get(i);
 
-                if( !rank.containsKey( account.get(i) ) ){
-                    rank.put( account.get(i) , 0 );
-                    parent.put( account.get(i) , account.get(i) );
-                    currEmail = account.get(i);
-                }
-                else{
-                    currEmail = findParent( account.get(i) , parent );
-                    currRank = rank.get( currEmail );
-                    
-                }
-
-                if( currRank >= maxRank ){
-                    maxRank = currRank;
-                    maxRankEmail = currEmail;
-                }
+                parent.putIfAbsent(email, email);
+                rank.putIfAbsent(email, 0);
+                name.put(email, accName);
             }
+        }
 
-            for( int  i = 1 ; i < m ; i++){
-                parent.put( findParent(account.get(i), parent) , maxRankEmail );
+        for (List<String> account : accounts) {
+            String firstEmail = account.get(1);
+
+            for (int i = 2; i < account.size(); i++) {
+                union(firstEmail, account.get(i), parent, rank);
             }
-
-            rank.put( maxRankEmail , maxRank + 1 );
-            name.put( maxRankEmail , account.get(0) );
         }
 
-        for( Map.Entry<String , String> entry : parent.entrySet() ){
-            String main = findParent(entry.getValue() , parent );
-            emails.computeIfAbsent( main , f -> new ArrayList<>() ).add(entry.getKey());
+        for (String email : parent.keySet()) {
+            String root = findParent(email, parent);
+            emails.computeIfAbsent(root, k -> new ArrayList<>()).add(email);
         }
 
-        for( Map.Entry<String , List<String> > entry : emails.entrySet() ){
-            List<String> value = entry.getValue();
-            String na = name.get(entry.getKey());
+        for (String root : emails.keySet()) {
+            List<String> list = emails.get(root);
+            Collections.sort(list);
 
-            Collections.sort( value , (a , b) -> a.compareTo(b) );
+            List<String> temp = new ArrayList<>();
+            temp.add(name.get(root));
+            temp.addAll(list);
 
-            List<String> account = new ArrayList<>();
-            account.add(na);
-            account.addAll(value);
-
-            ans.add(account);
-
+            ans.add(temp);
         }
-    
+
         return ans;
     }
 }
