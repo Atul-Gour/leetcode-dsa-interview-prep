9e6932fa90b494fee36de26c258a26cdbe53777f
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-18 16:14:11 +0530

    Time: 7 ms (85.62%) | Memory: 46.5 MB (70.54%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -11,30 +11,32 @@ class Node {
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         HashMap<String , Integer> map = new HashMap<>();
+        PriorityQueue<Node> pq = new PriorityQueue<>((a , b) -> {
+            if( a.freq != b.freq )
+                return Integer.compare( a.freq , b.freq );
+            else
+                return (b.s).compareTo(a.s);
+        });
+
         List<String> ans = new ArrayList<>();
 
         for( String word : words ){
             map.put( word , map.getOrDefault( word , 0 ) + 1 );
         }
 
-        Node[] arr = new Node[map.size()];
-
-        int i = 0;
         for( String word : map.keySet() ){
-            arr[i++] = new Node( word , map.get(word) );
+            pq.offer( new Node( word , map.get(word) ) );
+
+            if( pq.size() > k ){
+                pq.poll();
+            }
         }
 
-        Arrays.sort( arr , (a , b) -> {
-            if( a.freq != b.freq )
-                return Integer.compare( b.freq , a.freq );
-            else
-                return (a.s).compareTo(b.s);
-        } );
-
-        for( i = 0 ; i < k ; i++ ){
-            ans.add( arr[i].s );
+        for( int i = 0 ; i < k ; i++ ){
+            ans.add( pq.poll().s );
         }
 
+        Collections.reverse(ans);
         return ans;
 
     }
