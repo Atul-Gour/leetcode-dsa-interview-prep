4f0c88901c5bd4acab5823b4ec0a106ad6638a6d
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-21 16:18:06 +0530

    Time: 3 ms (72.32%) | Memory: 138 MB (98.15%) - LeetSync

--- 4062-house-robber-v/house-robber-v.java
+++ 4062-house-robber-v/house-robber-v.java
@@ -1,23 +1,19 @@
 class Solution {
-    public long rob(int[] nums, int[] colors) {
+    public long rob(int[] nums, int[] color) {
         int n = nums.length;
-        long dp[] = new long[n + 1];
+        long arr[] = new long[n+1];
 
-        for( int i = n - 1 ; i >= 0 ; i-- ){
-            long notTake = dp[i+1];
-            long take = nums[i];
+        arr[n - 1] = nums[n-1];  
 
-            if (i + 1 < n) {
-                if (colors[i + 1] != colors[i]) {
-                    take += dp[i+1];
-                } else if( i + 2 < n) {
-                    take += dp[i+2];
-                }
+        for( int i = n - 2 ; i >= 0 ; i-- ){
+            if( color[i] == color[i + 1] ){
+                arr[i] = Math.max( nums[i] + arr[ i + 2 ] , arr[i + 1] );
             }
-
-            dp[i] =  Math.max(take, notTake);
+            else{
+                arr[i] = nums[i] + arr[i + 1];
+            }
         }
 
-        return dp[0];
+        return arr[0];
     }
 }
