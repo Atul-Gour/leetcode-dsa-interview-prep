60c01eda6392d6b685ee921a953022ca866084b7
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-22 19:53:00 +0530

    Time: 12 ms (74.86%) | Memory: 47.3 MB (71.14%) - LeetSync

--- 1219-longest-well-performing-interval/longest-well-performing-interval.java
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.java
@@ -1,24 +1,23 @@
 class Solution {
     public int longestWPI(int[] hours) {
         int n = hours.length;
-        long[] prefix = new long[n + 1];
-        for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + ( hours[i] > 8 ? 1 : -1 );
+
+        int[] nums = new int[n + 1];
+        for( int i = 0 ; i < n ; i++ ){
+            nums[i+1] = nums[i] + (hours[i] > 8 ? 1 : -1) ;
         }
 
-        Deque<Integer> stack = new ArrayDeque<>();
+        Stack<Integer> st = new Stack<>();
 
-        for (int i = 0; i <= n; i++) {
-            if (stack.isEmpty() || prefix[i] < prefix[stack.peek()]) {
-                stack.push(i);
-            }
+        for( int i = 0 ; i <= n ; i++ ){
+            if( !st.isEmpty() && nums[st.peek()] <= nums[i] )continue;
+            st.push(i);
         }
 
         int maxLen = 0;
-
-        for (int j = n; j >= 0; j--) {
-            while (!stack.isEmpty() && prefix[j] > prefix[stack.peek()]) {
-                maxLen = Math.max(maxLen, j - stack.pop());
+        for( int i = n ; i >= 0 ; i-- ){
+            while( !st.isEmpty() && nums[st.peek()] < nums[i] ){
+                maxLen = Math.max( maxLen , ( i - st.pop())) ;
             }
         }
 
