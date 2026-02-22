6b39912dc9304f09c43cc3fb70b52369426c1c5d
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-22 12:01:19 +0530

    Time: 62 ms (73.06%) | Memory: 106.6 MB (65.28%) - LeetSync

--- 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.java
+++ 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.java
@@ -1,39 +1,44 @@
 class TreeAncestor {
+    int[] parent;
+    int n;
+    int[][] dp;
 
-    final int max = 17;
-    static int table[][];
-
-    void build( int[] parent ){
-        int n = parent.length;
-
-        for(int i = 0 ; i < n ; i++ ){
-            table[i][0] = parent[i];
-        }
-
-        for(int j = 1 ; j < max ; j++ ){
-            for(int i = 0 ; i < n ; i++ ){
-                int mid = table[i][j-1];
-
-                if(mid == -1)
-                    table[i][j] = -1;
-                else 
-                    table[i][j] = table[mid][j-1];
-            }
-        }
-    }
 
     public TreeAncestor(int n, int[] parent) {
-        table = new int[n][max];
-        build(parent);
+        this.parent = parent;
+        this.n = n;
+        int[][] dp = new int[20][n];
+
+        for( int j = 0 ; j < n ; j++ ){
+            dp[0][j] = parent[j];
+        }
+
+        for( int i = 1 ; i < 20 ; i++ ){
+            for( int j = 0 ; j < n ; j++ ){
+                if( dp[i-1][j] == -1 ) dp[i][j] = -1;
+                else dp[i][j] = dp[i-1][ dp[i-1][j] ];
+            }
+        }
+
+        this.dp = dp;
     }
     
     public int getKthAncestor(int node, int k) {
-        for(int i = 0 ; i < max ; i++ ){
-            if( ( k & (1 << i)) != 0){
-                node = table[node][i];
+        int i = 0;
+        while( k > 0 ){
+            
+            if( (k & 1) == 0 ){
+                k >>= 1;
+                i++;
+                continue;
             }
+            node = dp[i][node];
             if(node == -1)return -1;
+
+            k >>= 1;
+            i++;
         }
+
         return node;
     }
 }
