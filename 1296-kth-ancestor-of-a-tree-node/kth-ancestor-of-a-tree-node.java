35d9f35c554b9641587a49df32bc6070bbfdd04d
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-22 12:08:10 +0530

    Time: 64 ms (63.99%) | Memory: 109.5 MB (44.82%) - LeetSync

--- 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.java
+++ 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.java
@@ -2,18 +2,19 @@ class TreeAncestor {
     int[] parent;
     int n;
     int[][] dp;
+    final int MAX = 17;
 
 
     public TreeAncestor(int n, int[] parent) {
         this.parent = parent;
         this.n = n;
-        int[][] dp = new int[20][n];
+        int[][] dp = new int[MAX][n];
 
         for( int j = 0 ; j < n ; j++ ){
             dp[0][j] = parent[j];
         }
 
-        for( int i = 1 ; i < 20 ; i++ ){
+        for( int i = 1 ; i < MAX ; i++ ){
             for( int j = 0 ; j < n ; j++ ){
                 if( dp[i-1][j] == -1 ) dp[i][j] = -1;
                 else dp[i][j] = dp[i-1][ dp[i-1][j] ];
@@ -24,27 +25,13 @@ class TreeAncestor {
     }
     
     public int getKthAncestor(int node, int k) {
-        int i = 0;
-        while( k > 0 ){
-            
-            if( (k & 1) == 0 ){
-                k >>= 1;
-                i++;
-                continue;
+        for( int i = 0 ; i < MAX ; i++ ){
+            if(( k & ( 1 << i )) != 0 ){
+                node = dp[i][node];
             }
-            node = dp[i][node];
-            if(node == -1)return -1;
-
-            k >>= 1;
-            i++;
+            if( node == -1 )return node;
         }
 
         return node;
     }
 }
-
-/**
- * Your TreeAncestor object will be instantiated and called as such:
- * TreeAncestor obj = new TreeAncestor(n, parent);
- * int param_1 = obj.getKthAncestor(node,k);
- */
