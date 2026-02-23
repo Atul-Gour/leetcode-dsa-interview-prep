fc9676fed2d5759708d46f25732437836f3da552
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-23 18:42:27 +0530

    Time: 10 ms (94.07%) | Memory: 47.7 MB (99.01%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,20 +1,24 @@
 class Solution {
     public boolean hasAllCodes(String s, int k) {
-        int n = (int)Math.pow(2 , k);
+        int n = (int) Math.pow(2, k);
         boolean[] present = new boolean[n];
 
-        if( s.length() < k )return false;
-        int r = k;
-        int l = 0;
+        if (s.length() < k)
+            return false;
 
-        while( r < s.length() + 1 ){
-            l = r - k;
-            present[ Integer.parseInt(s.substring(l , r) , 2) ] = true;
-            r++;
+        int num = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            num = ((num << 1) & ((1 << k) - 1)) | (s.charAt(i) - '0');
+
+            if (i >= k - 1) {
+                present[num] = true;
+            }
         }
 
-        for( boolean ele : present ){
-            if( !ele )return false;
+        for (boolean ele : present) {
+            if (!ele)
+                return false;
         }
 
         return true;
