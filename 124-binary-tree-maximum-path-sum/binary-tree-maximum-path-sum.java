845833a9a248646d25c3973a6d91f14dec6ffe75
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-25 16:09:17 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (6.72%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -1,48 +1,22 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+
     int ans = Integer.MIN_VALUE;
 
-    private int find( TreeNode root ){
-        if( root == null )return -1001;
+    private int find(TreeNode root) {
+        if (root == null) return 0;
 
-        int left = find( root.left );
-        int right = find( root.right );
+        int left = Math.max(0, find(root.left));
+        int right = Math.max(0, find(root.right));
 
-        if( left == -1001 && right == -1001 ){
-            ans = Math.max( ans , root.val );
-            return root.val;
-        }
+        int currentPath = root.val + left + right;
 
-        long currMax = root.val;
-        currMax = Math.max( currMax , root.val + left );
-        currMax = Math.max( currMax , root.val + right );
-        currMax = Math.max( currMax , Integer.MIN_VALUE );
-        int returnMax = (int)currMax;
+        ans = Math.max(ans, currentPath);
 
-        currMax = Math.max( currMax , root.val + right + left );
-        currMax = Math.max( currMax , Integer.MIN_VALUE );
-        ans = (int) Math.max( ans , currMax );
-        
-        return returnMax;
+        return root.val + Math.max(left, right);
     }
 
     public int maxPathSum(TreeNode root) {
-        find( root );
-        
-        return ans == Integer.MIN_VALUE ? 0 : ans;
+        find(root);
+        return ans;
     }
 }
