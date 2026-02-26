d8f3e8d464906e1e81b0c87a752be9c5164b835e
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-02-26 18:18:02 +0530

    Time: 2 ms (64.33%) | Memory: 46.4 MB (65.15%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -1,44 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    int postIndex ;
+    HashMap<Integer , Integer> inIndex = new HashMap<>();
 
-int postIndex;
+    private TreeNode build( int[] postorder , int inStart , int inEnd ){
+        if( inStart > inEnd )return null;
 
-    public TreeNode build(HashMap<Integer,Integer> map,int start,int end, int[] postorder){
-        if(start > end) return null;
-        int rootNode= postorder[postIndex--];
-        TreeNode root=new TreeNode(rootNode);
+        int val = postorder[postIndex--];
+        TreeNode curr = new TreeNode( val );
 
-        int index=map.get(rootNode);
-        root.right=build(map,index+1,end,postorder);
-        root.left=build(map,start,index-1,postorder);
+        int currIndex = inIndex.get(val);
+        curr.right = build( postorder , currIndex + 1 , inEnd );
+        curr.left = build( postorder , inStart , currIndex - 1 );
 
-        return root;
+        return curr;
+    }
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+
+       for( int i = 0 ; i < inorder.length ; i++ )inIndex.put(inorder[i] , i);
+       postIndex = inorder.length - 1;
+       return build( postorder , 0 , inorder.length - 1 );
     }
 
 
-    public TreeNode buildTree(int[] inorder, int[] postorder){
-             postIndex= postorder.length-1;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i =0;i<inorder.length;i++){
-            map.put(inorder[i],i);
-        }
-
-        return build(map,0,inorder.length-1,postorder);
-        
-        
-    }
 }
