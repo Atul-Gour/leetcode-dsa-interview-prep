b42d1740d681ef0c0b84c4f41346ffff5896b818
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-01 21:33:52 +0530

    Time: 6 ms (97.05%) | Memory: 47.3 MB (96.51%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,20 +9,15 @@
  */
 
 class Solution {
+    private TreeNode LCA(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q )return root;
 
-    TreeNode find(TreeNode root, int p, int q){
-        if(root == null)return null;
-        int rootVal= root.val;
-        if((p==rootVal || q==rootVal) ||(p<rootVal && q>rootVal))return root;
-        else if(q<rootVal){
-            return find(root.left ,p ,q);
-        }else{
-            return find(root.right ,p ,q);
-        }
+        if( root.val > p.val && root.val < q.val )return root;
+        else if( root.val < p.val ) return LCA( root.right , p , q );
+        else return LCA( root.left , p , q );
     }
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(p.val>q.val)return find(root, q.val , p.val);
-        else return find(root, p.val , q.val);
+        if( p.val > q.val )return LCA( root , q , p );
+        else return LCA( root , p , q );
     }
 }
