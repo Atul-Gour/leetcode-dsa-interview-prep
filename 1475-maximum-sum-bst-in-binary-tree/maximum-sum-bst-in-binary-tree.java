3a22f430ec9fe1604742acfcf2cc09825770ccb6
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-07 09:50:32 +0530

    Time: 6 ms (89.57%) | Memory: 66.8 MB (20.45%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
@@ -3,43 +3,25 @@ class Solution {
 
     static class node{
         int min, max , sum;
-        boolean valid;
 
-        node( int min , int max , int sum , boolean valid ){
+        node( int min , int max , int sum ){
             this.min = min;
             this.max = max;
             this.sum = sum;
-            this.valid = valid;
         }
     }
 
     private node find( TreeNode root ){
+
+        if( root == null )return new node( Integer.MAX_VALUE , Integer.MIN_VALUE , 0 );
         
-        node right = null , left = null;
-        int currMin = root.val;
-        int currMax = root.val;
-        int currSum = root.val;
-        boolean canTake = true;
+        node left = find( root.left );
+        node right = find( root.right );
 
-        if( root.left != null ){
-            left = find( root.left );
-            if( !left.valid || root.val <= left.max  )canTake = false;
-
-            currSum += left.sum;
-            currMin = left.min;
-        }
-
-        if( root.right != null ){
-            right = find( root.right );
-            if( !right.valid || root.val >= right.min )canTake = false;
-
-            currSum += right.sum;
-            currMax = right.max;
-        }
-
-        if( !canTake  ) return new node(0 , 0 , 0 , false);
+        if( root.val <= left.max || root.val >= right.min ) return new node( Integer.MIN_VALUE , Integer.MAX_VALUE , 0 );
+        int currSum = left.sum + right.sum + root.val;
         ans = Math.max( ans , currSum );
-        return new node( currMin , currMax , currSum , true );
+        return new node( Math.min( left.min , root.val ) , Math.max( right.max , root.val ) , currSum );
     }
 
     public int maxSumBST(TreeNode root) {
