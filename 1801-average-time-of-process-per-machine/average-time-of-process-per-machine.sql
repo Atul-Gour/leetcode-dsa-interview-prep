a50acb941b4e33e56fb09b2ed6ffb7e5faee7969
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-07 20:38:35 +0530

    Time: 160 ms (98.13%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,7 +1,12 @@
 -- Write your PostgreSQL query statement below
-select machine_id ,
-Round(sum( case when activity_type ='end' then timestamp 
- else -timestamp end) :: NUMERIC(20,3) /
-(count(distinct process_id)),3) as processing_time
-from activity
-group by machine_id
+select machine_id ,  processing_time 
+from ( 
+    select machine_id ,
+        round((sum(case 
+                        when activity_type = 'start' then -timestamp 
+                        else +timestamp
+                    end) :: NUMERIC(20,3) / (count(*)/2) ) , 3) as processing_time 
+    from Activity
+    group by machine_id
+) t
+order by machine_id
