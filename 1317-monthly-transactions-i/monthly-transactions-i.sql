1d691f1b4a5501ec0a4a5efefcb4e59bd95c47ed
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-09 19:32:37 +0530

    Time: 429 ms (53.49%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,10 +1,9 @@
--- Write your PostgreSQL query statement below
 select 
-    to_char(t.trans_date , 'yyyy-mm') as month ,
-    t.country ,
-    count(8) as trans_count,
-    count(state) filter (where state ='approved') as approved_count,
-    sum(amount) as trans_total_amount,
-    coalesce(sum(amount) filter (where state = 'approved'),0) as approved_total_amount
+    to_char(t.trans_date , 'YYYY-MM' ) as month,
+    t.country,
+    count(*) as trans_count,
+    count(*) filter (where t.state = 'approved' ) as approved_count,
+    sum(t.amount) as trans_total_amount,
+    coalesce(sum(t.amount) filter (where t.state = 'approved' ) , 0) as approved_total_amount
 from Transactions t
-group by t.country , to_char(t.trans_date , 'yyyy-mm')
+group by to_char(t.trans_date , 'YYYY-MM' ) , t.country
