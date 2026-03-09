8627e7ba73795026fadcd663bc9201f610e210be
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-09 17:56:37 +0530

    Time: 3 ms (71.03%) | Memory: 42.8 MB (90.08%) - LeetSync

---
+++ 1042-minimum-cost-to-merge-stones/minimum-cost-to-merge-stones.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+
+    public int mergeStones(int[] stones, int k) {
+
+        int n = stones.length;
+
+        if ((n - 1) % (k - 1) != 0)
+            return -1;
+
+        int[][] dp = new int[n][n];
+
+        int[] prefix = new int[n + 1];
+
+        for (int i = 0; i < n; i++)
+            prefix[i + 1] = prefix[i] + stones[i];
+
+        for (int len = k; len <= n; len++) {
+
+            for (int i = 0; i + len <= n; i++) {
+
+                int j = i + len - 1;
+
+                dp[i][j] = Integer.MAX_VALUE;
+
+                for (int m = i; m < j; m += k - 1) {
+
+                    dp[i][j] = Math.min(
+                        dp[i][j],
+                        dp[i][m] + dp[m + 1][j]
+                    );
+                }
+
+                if ((len - 1) % (k - 1) == 0) {
+                    dp[i][j] += prefix[j + 1] - prefix[i];
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+}
