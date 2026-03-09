760c33a593fb5e69b29c7e3d0c7db3f79e72d42e
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-09 10:18:00 +0530

    Time: 1 ms (98.87%) | Memory: 45.4 MB (81.29%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.java
+++ 419-battleships-in-a-board/battleships-in-a-board.java
@@ -1,30 +1,29 @@
 class Solution {
-    boolean visited[][] ;
 
     void spread( char[][] board , int i , int j ){
 
         int n = board.length;
         int m = board[0].length;
 
-        if( (i - 1 >= 0 && board[i-1][j] == 'X' && !visited[i-1][j]) || (i + 1 < n && board[i+1][j] == 'X' && !visited[i+1][j]) ){
+        if( (i - 1 >= 0 && board[i-1][j] == 'X' ) || (i + 1 < n && board[i+1][j] == 'X') ){
             int upwardI = i - 1;
 
-            while( upwardI >= 0 && board[upwardI][j] == 'X' && !visited[upwardI][j] ){
-                visited[upwardI][j] = true; upwardI--;
+            while( upwardI >= 0 && board[upwardI][j] == 'X' ){
+                board[upwardI][j] = 'Y'; upwardI--;
             }
 
-            while( i < n && board[i][j] == 'X' && !visited[i][j] ){
-                visited[i][j] = true; i++;
+            while( i < n && board[i][j] == 'X' ){
+                board[i][j] = 'Y'; i++;
             }
         }else{
             int right = j + 1;
 
-            while( j >= 0 && board[i][j] == 'X' && !visited[i][j] ){
-                visited[i][j] = true; j--;
+            while( j >= 0 && board[i][j] == 'X' ){
+                board[i][j] = 'Y'; j--;
             }
 
-            while( right < m && board[i][right] == 'X' && !visited[i][right] ){
-                visited[i][right] = true; right++;
+            while( right < m && board[i][right] == 'X' ){
+                board[i][right] = 'Y'; right++;
             }
         }
     }
@@ -32,12 +31,11 @@ class Solution {
     public int countBattleships(char[][] board) {
         int n = board.length;
         int m = board[0].length;
-        visited = new boolean[n][m];
         int ans = 0;
 
         for( int i = 0 ; i < n ; i++ ){
             for( int j = 0 ; j < m ; j++ ){
-                if( board[i][j] == 'X' && !visited[i][j] ){
+                if( board[i][j] == 'X' ){
                     spread( board , i , j );
                     ans++;
                 }
