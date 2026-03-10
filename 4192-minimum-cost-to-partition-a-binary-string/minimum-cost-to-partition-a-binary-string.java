1900d5e98aa1c83d453710634522525944a5fad8
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-10 23:25:10 +0530

    Time: 45 ms (83.45%) | Memory: 48.4 MB (30.43%) - LeetSync

--- 4192-minimum-cost-to-partition-a-binary-string/minimum-cost-to-partition-a-binary-string.java
+++ 4192-minimum-cost-to-partition-a-binary-string/minimum-cost-to-partition-a-binary-string.java
@@ -1,48 +1,47 @@
 class Solution {
-    private long currCost( int[] prefixOneCount , int i , int j , int encCost, int flatCost ){
-        int sensitiveCount = prefixOneCount[j + 1] - prefixOneCount[i];
-        int cost = 0;
-        if( sensitiveCount == 0 )return flatCost;
-        else return (long)(j - i + 1) * sensitiveCount * encCost;
-    }
     public long minCost(String s, int encCost, int flatCost) {
         int n = s.length();
-        int prefixOneCount[] = new int[n+1];
+        int[] pre = new int[n + 1];
+        for (int i = 0; i < n; i++)
+            pre[i + 1] = pre[i] + (s.charAt(i) == '1' ? 1 : 0);
 
-        // count levels
-        int m = 0, tmp = n;
-        while( tmp % 2 == 0 ){ tmp /= 2; m++; }
+        // count how many times n is divisible by 2
+        int m = 0;
+        int tmp = n;
+        while (tmp % 2 == 0) { tmp /= 2; m++; }
 
-        // jagged dp: dp[len][i] instead of dp[i][j]
-        long[][] dp = new long[n + 1][];
-        dp[1] = new long[n]; // single chars, length=1
+        // numSegments[k] = how many segments at level k
+        // segLen[k]      = length of each segment at level k
+        // level 0 = whole string (1 segment of length n)
+        // level m = smallest segments (2^m segments of length n/2^m)
 
-        for( int i = 1 ; i <= n ; i++ ){
-            if( s.charAt(i-1) == '1' )
-                prefixOneCount[i] = prefixOneCount[i-1] + 1;
-            else prefixOneCount[i] = prefixOneCount[i-1];
+        long[][] dp = new long[m + 1][];
+
+        // bottom level: fill smallest segments first (they can't split, odd length)
+        int baseLen = tmp; // = n / 2^m, this is odd
+        int baseCount = (int) Math.pow(2, m); // = 2^m segments at bottom
+        dp[m] = new long[baseCount];
+        for (int i = 0; i < baseCount; i++) {
+            int start = i * baseLen;
+            int x = pre[start + baseLen] - pre[start];
+            dp[m][i] = (x == 0) ? flatCost : (long) baseLen * x * encCost;
         }
 
-        for( int i = 0 ; i < n ; i++ ){
-            if( s.charAt(i) == '1' )
-                dp[1][i] = encCost;
-            else dp[1][i] = flatCost;
-        }
-
-        for( int len = 2 ; len <= n ; len++ ){
-            if( n % len != 0 ) continue; // only valid segment lengths
-            dp[len] = new long[n / len];
-            for( int i = 0 ; i < n / len ; i++ ){
-                int l = i * len;
-                int r = l + len - 1;
-                long wholeCost = currCost( prefixOneCount , l , r , encCost, flatCost );
-                long separationCost = Long.MAX_VALUE;
-                if( len % 2 == 0 ){
-                    separationCost = dp[len/2][2*i] + dp[len/2][2*i+1];
-                }
-                dp[len][i] = Math.min( wholeCost , separationCost );
+        // build up level by level toward the full string
+        for (int k = m - 1; k >= 0; k--) {
+            int numSegments = (int) Math.pow(2, k);
+            int segLen = n / numSegments;
+            dp[k] = new long[numSegments];
+            for (int i = 0; i < numSegments; i++) {
+                int start = i * segLen;
+                int x = pre[start + segLen] - pre[start];
+                long wholeCost = (x == 0) ? flatCost : (long) segLen * x * encCost;
+                // split into left child (2*i) and right child (2*i+1) from level below
+                long splitCost = dp[k + 1][2 * i] + dp[k + 1][2 * i + 1];
+                dp[k][i] = Math.min(wholeCost, splitCost);
             }
         }
-        return dp[n][0];
+
+        return dp[0][0];
     }
 }
