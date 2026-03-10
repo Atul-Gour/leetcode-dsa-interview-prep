361687752edbd451563aca9775ed1a4df9035673
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-10 22:29:39 +0530

    Time: 42 ms (93.30%) | Memory: 48.4 MB (30.43%) - LeetSync

--- 4192-minimum-cost-to-partition-a-binary-string/minimum-cost-to-partition-a-binary-string.java
+++ 4192-minimum-cost-to-partition-a-binary-string/minimum-cost-to-partition-a-binary-string.java
@@ -1,26 +1,39 @@
 class Solution {
-    int encCost;
-    int flatCost;
-    int[] pre;
     public long minCost(String s, int encCost, int flatCost) {
-        pre = new int[s.length() + 1];
-        for (int i = 1; i < pre .length; i++) {
-            pre[i] = pre[i - 1] + (s.charAt(i - 1) - '0');
+        int n = s.length();
+        int[] pre = new int[n + 1];
+        for (int i = 0; i < n; i++)
+            pre[i + 1] = pre[i] + (s.charAt(i) == '1' ? 1 : 0);
+
+        // count how many times n is divisible by 2
+        int m = 0;
+        int tmp = n;
+        while (tmp % 2 == 0) { tmp /= 2; m++; }
+
+        long[][] dp = new long[m + 1][];
+
+        // bottom level: smallest segments (length = n >> m, which is odd or 1)
+        int baseLen = n >> m;
+        dp[m] = new long[1 << m];
+        for (int i = 0; i < (1 << m); i++) {
+            int start = i * baseLen;
+            int x = pre[start + baseLen] - pre[start];
+            dp[m][i] = (x == 0) ? flatCost : (long) baseLen * x * encCost;
         }
-        this.encCost = encCost;
-        this.flatCost = flatCost;
-        return helper(0, s.length());
-    }
-    private long helper(int l, int r) {
-        int L = r - l;
-        int X = pre[r] - pre [l];
-        if (X == 0)
-            return flatCost;
-        long cost =  (long) L * X * encCost;
-        if (L % 2 == 0) {
-            int mid = l + (r - l) / 2;
-            cost = Math.min(cost, helper(l, mid) + helper(mid, r));
+
+        // build up from bottom
+        for (int k = m - 1; k >= 0; k--) {
+            int len = n >> k;
+            dp[k] = new long[1 << k];
+            for (int i = 0; i < (1 << k); i++) {
+                int start = i * len;
+                int x = pre[start + len] - pre[start];
+                long wholeCost = (x == 0) ? flatCost : (long) len * x * encCost;
+                long splitCost = dp[k + 1][2 * i] + dp[k + 1][2 * i + 1];
+                dp[k][i] = Math.min(wholeCost, splitCost);
+            }
         }
-        return cost;
+
+        return dp[0][0];
     }
 }
