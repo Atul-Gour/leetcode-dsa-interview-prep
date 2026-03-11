75ba6cd986b0ef325daf98894e6e070d7fd44abf
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-11 16:42:45 +0530

    Time: 116 ms (24.80%) | Memory: 44.1 MB (97.70%) - LeetSync

---
+++ 312-burst-balloons/burst-balloons.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    int[][] memo;
+
+    public int maxCoins(int[] nums) {
+
+        int n = nums.length;
+
+        int[] arr = new int[n + 2];
+        arr[0] = 1;
+        arr[n + 1] = 1;
+
+        for(int i = 0; i < n; i++)
+            arr[i + 1] = nums[i];
+
+        memo = new int[n + 2][n + 2];
+
+        return solve(arr, 0, n + 1);
+    }
+
+    private int solve(int[] arr, int left, int right){
+
+        if(left + 1 == right)
+            return 0;
+
+        if(memo[left][right] != 0)
+            return memo[left][right];
+
+        int ans = 0;
+
+        for(int k = left + 1; k < right; k++){
+
+            int cost =
+                    solve(arr, left, k) +
+                    arr[left] * arr[k] * arr[right] +
+                    solve(arr, k, right);
+
+            ans = Math.max(ans, cost);
+        }
+
+        memo[left][right] = ans;
+
+        return ans;
+    }
+}
