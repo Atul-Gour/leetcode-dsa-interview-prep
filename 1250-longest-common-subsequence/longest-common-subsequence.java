e808b614421b11f09b333a453fd39407d969603b
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-12 20:23:03 +0530

    Time: 19 ms (90.08%) | Memory: 53.9 MB (78.95%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,19 +1,17 @@
 class Solution {
+
     public int longestCommonSubsequence(String text1, String text2) {
-        char arr1[] = text1.toCharArray();
-        char arr2[] = text2.toCharArray();
-        int n = arr1.length;
-        int m = arr2.length;
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n + 1][m + 1];
 
-        int dp[][] = new int[n + 1][m + 1];
+        for( int i = 0 ; i <= n ; i++ )dp[i][m] = 0;
+        Arrays.fill(dp[n] , 0);
 
-        for( int i = n - 1 ; i >= 0 ; i-- ){
-            for( int j = m - 1 ; j >= 0 ; j-- ){
-                if( arr1[i] == arr2[j] ){
-                    dp[i][j] = 1 + dp[i + 1][j + 1];
-                }else{
-                    dp[i][j] = Math.max(dp[i][j + 1] , dp[i + 1][j]);
-                }
+        for( int i = n-1 ; i >= 0 ; i-- ){
+            for( int j = m-1 ; j >= 0 ; j-- ){
+                if( text1.charAt(i) == text2.charAt(j) ) dp[i][j] = 1 + dp[i+1][j+1];
+                else dp[i][j] = Math.max( dp[i][j+1] , dp[i+1][j] );
             }
         }
 
