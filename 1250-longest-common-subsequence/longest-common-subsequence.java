e1f6eb45844072f5c990fdb5075def7febe44d88
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-12 20:23:38 +0530

    Time: 19 ms (90.08%) | Memory: 53.8 MB (78.95%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -5,9 +5,6 @@ class Solution {
         int m = text2.length();
         int[][] dp = new int[n + 1][m + 1];
 
-        for( int i = 0 ; i <= n ; i++ )dp[i][m] = 0;
-        Arrays.fill(dp[n] , 0);
-
         for( int i = n-1 ; i >= 0 ; i-- ){
             for( int j = m-1 ; j >= 0 ; j-- ){
                 if( text1.charAt(i) == text2.charAt(j) ) dp[i][j] = 1 + dp[i+1][j+1];
