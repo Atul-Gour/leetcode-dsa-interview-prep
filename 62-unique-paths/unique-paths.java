d5f6867d52e9647796c5640728a8fb47ad5d2c06
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-12 17:12:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (79.94%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,18 +1,20 @@
 class Solution {
-    long calculateUniquePaths(int m, int n, int i, int j, long[][] dp) {
-        if (i == m - 1 || j == n - 1) return 1;
+    long solve(int m, int n, int i, int j, long[][] dp) {
+        if (i >= m || j >= n) return 0;
+        if (i == m-1 && j == n-1) return 1;
+
         if (dp[i][j] != -1) return dp[i][j];
-        
-        dp[i][j] = calculateUniquePaths(m, n, i + 1, j, dp) 
-                 + calculateUniquePaths(m, n, i, j + 1, dp);
-        return dp[i][j];
-    }
+
+        return dp[i][j] =
+            solve(m,n,i+1,j,dp) +
+            solve(m,n,i,j+1,dp);
+    }   
     
     public int uniquePaths(int m, int n) {
         long[][] dp = new long[m][n];
         for (int i = 0; i < m; i++) 
             Arrays.fill(dp[i], -1);
         
-        return (int) calculateUniquePaths(m, n, 0, 0, dp);
+        return (int) solve(m, n, 0, 0, dp);
     }
 }
