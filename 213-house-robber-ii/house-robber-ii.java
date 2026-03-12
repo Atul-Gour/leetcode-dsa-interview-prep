9d7e2e41345be6ab5cc61e0733b65cbf4b36ff2c
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-12 16:34:22 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (19.49%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,21 +1,28 @@
 class Solution {
-    private int find( int[] nums ){
-        int n = nums.length;
-        int dp[][] = new int[n][2];
 
-        dp[0][0] = nums[0];
+    private int find(int[] nums, int start, int end) {
 
-        for( int i = 1 ; i < n ; i++ ){
-            dp[i][0] = dp[i-1][1] + nums[i] ;
-            dp[i][1] = Math.max( dp[i-1][0] , dp[i-1][1] );
+        int rob = 0;
+        int skip = 0;
+
+        for(int i = start; i <= end; i++){
+            int newRob = skip + nums[i];
+            skip = Math.max(skip, rob);
+            rob = newRob;
         }
 
-        return Math.max(dp[n-1][0] , dp[n-1][1]);
+        return Math.max(rob, skip);
     }
-    public int rob(int[] nums) {
-        int n = nums.length;
-        if( n == 1 )return nums[0];
 
-        return Math.max(find( Arrays.copyOfRange(nums, 0, n - 1) ) , find( Arrays.copyOfRange(nums, 1, n ) ));
+    public int rob(int[] nums) {
+
+        int n = nums.length;
+
+        if(n == 1) return nums[0];
+
+        return Math.max(
+            find(nums, 0, n - 2),
+            find(nums, 1, n - 1)
+        );
     }
 }
