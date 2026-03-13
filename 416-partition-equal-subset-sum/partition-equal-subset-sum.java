7c7a4a046935a373f64a0607d3c563eaa767f443
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-13 19:14:50 +0530

    Time: 49 ms (51.58%) | Memory: 47.5 MB (60.38%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,30 +1,5 @@
 class Solution {
 
-    private HashSet<Integer> helper( int nums[] , int left , int right ){
-        int n = right - left + 1;
-        int sum = 0;
-        for( int i = 0 ; i < n ; i++ ) sum += nums[i + left];
-
-        HashSet<Integer> set = new HashSet<>();
-        set.add(0);
-        set.add(nums[left]);
-        boolean[][] dp = new boolean[n][sum + 1];
-
-        dp[0][nums[left]] = true;
-        for( int i = 0 ; i < n ; i++ ) dp[i][0] = true;
-        
-        for( int i = 1 ; i < n ; i++ ){
-            for( int j = 0 ; j <= sum ; j++ ){
-                if( j < nums[i + left] ) dp[i][j] = dp[i-1][j];
-                else dp[i][j] = dp[i-1][ j - nums[i + left] ] || dp[i-1][j];
-
-                if( dp[i][j] ) set.add( j );
-            }
-        }
-
-        return set;
-    }
-
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         if( n <= 1 ) return false;
@@ -32,15 +7,20 @@ class Solution {
         for( int ele : nums ) sum += ele;
 
         if( sum % 2 != 0 )return false;
-        int half = n / 2;
+        sum = sum/2;
 
-        HashSet<Integer> set1 = helper( nums , 0 , half - 1 );
-        HashSet<Integer> set2 = helper( nums , half , n - 1 );
+        boolean[][] dp = new boolean[n][sum + 1];
 
-        int target = sum/2;
+        if(nums[0] <= sum) dp[0][nums[0]] = true;
+        for( int i = 0 ; i < n ; i++ ) dp[i][0] = true;
+        
+        for( int i = 1 ; i < n ; i++ ){
+            for( int j = 0 ; j <= sum ; j++ ){
+                if( j < nums[i] ) dp[i][j] = dp[i-1][j];
+                else dp[i][j] = dp[i-1][ j - nums[i] ] || dp[i-1][j];
 
-        for( int a : set1 ){
-            if( set2.contains( target - a ) ) return true;
+                if( dp[i][j] && j == sum ) return true;
+            }
         }
         return false;
     }
