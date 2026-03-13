5d7d1240f128bf16859ed02ae4e2913c9e1c0e13
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-13 19:15:42 +0530

    Time: 48 ms (55.36%) | Memory: 47.6 MB (56.14%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -15,7 +15,7 @@ class Solution {
         for( int i = 0 ; i < n ; i++ ) dp[i][0] = true;
         
         for( int i = 1 ; i < n ; i++ ){
-            for( int j = 0 ; j <= sum ; j++ ){
+            for( int j = 1 ; j <= sum ; j++ ){
                 if( j < nums[i] ) dp[i][j] = dp[i-1][j];
                 else dp[i][j] = dp[i-1][ j - nums[i] ] || dp[i-1][j];
 
