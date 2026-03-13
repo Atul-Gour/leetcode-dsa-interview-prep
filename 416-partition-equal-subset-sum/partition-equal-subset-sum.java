745717f3cbf1a96d9c666dc549fe263f3b512e95
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-13 19:11:43 +0530

    Time: 153 ms (5.55%) | Memory: 48.3 MB (42.93%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,28 +1,47 @@
 class Solution {
-    public boolean canPartition(int[] arr) {
-        int n = arr.length;
+
+    private HashSet<Integer> helper( int nums[] , int left , int right ){
+        int n = right - left + 1;
         int sum = 0;
-        for(int i : arr) sum += i;
+        for( int i = 0 ; i < n ; i++ ) sum += nums[i + left];
 
-        if(sum % 2 != 0) return false;
-
-        sum /= 2;
+        HashSet<Integer> set = new HashSet<>();
+        set.add(0);
+        set.add(nums[left]);
         boolean[][] dp = new boolean[n][sum + 1];
 
-        dp[0][0] = true;
-        if(arr[0] <= sum) dp[0][arr[0]] = true;
+        dp[0][nums[left]] = true;
+        for( int i = 0 ; i < n ; i++ ) dp[i][0] = true;
+        
+        for( int i = 1 ; i < n ; i++ ){
+            for( int j = 0 ; j <= sum ; j++ ){
+                if( j < nums[i + left] ) dp[i][j] = dp[i-1][j];
+                else dp[i][j] = dp[i-1][ j - nums[i + left] ] || dp[i-1][j];
 
-        for(int i = 1; i < n; i++){
-            for(int j = 0; j <= sum; j++){
-                boolean notTake = dp[i-1][j];
-                boolean take = false;
-
-                if(arr[i] <= j){
-                    take = dp[i-1][j - arr[i]];
-                }
-                dp[i][j] = take || notTake;
+                if( dp[i][j] ) set.add( j );
             }
         }
-        return dp[n-1][sum];
+
+        return set;
+    }
+
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        if( n <= 1 ) return false;
+        int sum = 0;
+        for( int ele : nums ) sum += ele;
+
+        if( sum % 2 != 0 )return false;
+        int half = n / 2;
+
+        HashSet<Integer> set1 = helper( nums , 0 , half - 1 );
+        HashSet<Integer> set2 = helper( nums , half , n - 1 );
+
+        int target = sum/2;
+
+        for( int a : set1 ){
+            if( set2.contains( target - a ) ) return true;
+        }
+        return false;
     }
 }
