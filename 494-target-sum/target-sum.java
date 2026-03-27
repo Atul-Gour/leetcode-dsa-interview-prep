7653978a42817d753bfde4647bfbf43f2627020c
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-27 14:27:27 +0530

    Time: 2 ms (99.93%) | Memory: 42.6 MB (99.33%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,33 +1,23 @@
 class Solution {
-    private int solve(int index, int curr, int target, int[] nums, int[][] dp, int sum) {
-
-        if (index == nums.length) {
-            return curr == target ? 1 : 0;
-        }
-
-        int key = curr + sum;
-
-        if (dp[index][key] != -1) return dp[index][key];
-
-        int total = 0;
-
-        total += solve(index + 1, curr + nums[index], target, nums, dp, sum);
-        total += solve(index + 1, curr - nums[index], target, nums, dp, sum);
-
-        return dp[index][key] = total;
-    }
-
     public int findTargetSumWays(int[] nums, int target) {
-
-        int n = nums.length;
 
         int sum = 0;
         for (int num : nums) sum += num;
 
-        int[][] dp = new int[n][2 * sum + 1];
+        if (Math.abs(target) > sum) return 0;
+        if ((target + sum) % 2 != 0) return 0;
 
-        for (int[] arr : dp) Arrays.fill(arr, -1);
+        int P = (target + sum) / 2;
 
-        return solve(0, 0, target, nums, dp, sum);
+        int[] dp = new int[P + 1];
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int j = P; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+
+        return dp[P];
     }
 }
