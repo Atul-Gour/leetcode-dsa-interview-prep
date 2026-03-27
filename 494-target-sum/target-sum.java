fea380edee96c09573b80e1d231c044c36a3c1fa
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-27 14:24:49 +0530

    Time: 7 ms (64.79%) | Memory: 46.9 MB (24.61%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,35 +1,33 @@
 class Solution {
-    private int solve( int index , int curr , int target , int nums[] , int[][] dp ){
+    private int solve(int index, int curr, int target, int[] nums, int[][] dp, int sum) {
 
-        int n = nums.length;
-
-        if ( index == n ){
-            if( curr == target ) return 1;
-            else return 0;
+        if (index == nums.length) {
+            return curr == target ? 1 : 0;
         }
 
-        int key = curr + (n * 1000);
+        int key = curr + sum;
 
-        if( dp[index][key] != -1 ) return dp[index][key];
+        if (dp[index][key] != -1) return dp[index][key];
 
         int total = 0;
 
-        total += solve( index + 1 , curr + nums[index] , target , nums , dp );
-        total += solve( index + 1 , curr - nums[index] , target , nums , dp );
+        total += solve(index + 1, curr + nums[index], target, nums, dp, sum);
+        total += solve(index + 1, curr - nums[index], target, nums, dp, sum);
 
-        return dp[index][key] = total; 
-
+        return dp[index][key] = total;
     }
 
     public int findTargetSumWays(int[] nums, int target) {
 
         int n = nums.length;
-        int MAX = n * 1000 * 2;
-        int[][] dp = new int[n][MAX + 1];
 
-        for( int[] arr : dp )Arrays.fill( arr , -1 );
+        int sum = 0;
+        for (int num : nums) sum += num;
 
-        return solve( 0 , 0 , target , nums , dp );
+        int[][] dp = new int[n][2 * sum + 1];
 
+        for (int[] arr : dp) Arrays.fill(arr, -1);
+
+        return solve(0, 0, target, nums, dp, sum);
     }
 }
