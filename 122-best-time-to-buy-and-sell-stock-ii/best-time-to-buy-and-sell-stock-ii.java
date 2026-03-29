30f4ab2b76c14fcc27d48ee95994a09581a214a8
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-29 22:00:58 +0530

    Time: 4 ms (8.36%) | Memory: 50.3 MB (5.13%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,28 +1,27 @@
 class Solution {
-    private int solve( int index , int buyIndex , int[] prices , Map<Long, Integer> map ){
-        int n = prices.length;
+    private int solve(int i, int canBuy, int[] prices, int[][] dp) {
+        if (i == prices.length) return 0;
 
-        if( index >= n )return 0;
+        if (dp[i][canBuy] != -1) return dp[i][canBuy];
 
-        long key = (long)index << 32 | buyIndex ; 
-        if( map.containsKey(key) )return map.get(key);
+        int profit;
 
-        int sell = 0;
-        if( buyIndex != -1 && prices[index] > prices[buyIndex] )
-                 sell = solve( index + 1 , index + 1 , prices , map ) + prices[index] - prices[buyIndex];
+        if (canBuy == 1) {
+            int buy = -prices[i] + solve(i + 1, 0, prices, dp);
+            int skip = solve(i + 1, 1, prices, dp);
+            profit = Math.max(buy, skip);
+        } else {
+            int sell = prices[i] + solve(i + 1, 1, prices, dp);
+            int skip = solve(i + 1, 0, prices, dp);
+            profit = Math.max(sell, skip);
+        }
 
-        if( prices[index] < prices[buyIndex] ) buyIndex = index;
-        int notSell = solve( index + 1 , buyIndex , prices , map );
-
-        map.put( key , Math.max( sell , notSell ) );
-        return map.get(key);
+        return dp[i][canBuy] = profit;
     }
 
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-
-        Map<Long, Integer> map = new HashMap<>();
-
-        return solve( 0 , 0 , prices , map );
+        int[][] dp = new int[prices.length][2];
+        for (int[] row : dp) Arrays.fill(row, -1);
+        return solve(0, 1, prices, dp);
     }
 }
