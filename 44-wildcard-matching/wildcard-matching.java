90f39954e5db08685289eaa83df1dbd753d9ae57
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-29 20:33:04 +0530

    Time: 1 ms (99.86%) | Memory: 45.1 MB (93.15%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,53 +1,26 @@
 class Solution {
-    
-    private String reducedString( String s ){
-        int n = s.length();
-        StringBuilder sb = new StringBuilder();
-        int i = 0;
-        while(  i < n ){
-
-            if( s.charAt(i) == '*' ){
-                sb.append('*');
-
-                while( i < n && s.charAt(i) == '*' )i++;
-            }
-
-            if( i < n ) sb.append( s.charAt(i) );
-            i++;
-        }
-
-        return sb.toString();
-    } 
-
-    public boolean isMatch(String s, String pp) {
-        
-        String p = reducedString(pp);
-
-        int n = s.length();
-        int m = p.length();
-
-        int dp[][] = new int[n + 1][m + 1];
-
-        dp[n][m] = 2;
-        for( int i = 0 ; i < n ; i++ ) dp[i][m] = 1;
-        for (int j = m - 1; j >= 0; j--) {
-            if (p.charAt(j) == '*')
-                dp[n][j] = dp[n][j + 1];
-            else
-                dp[n][j] = 1;
-        }
-
-
-        for( int i = n-1 ; i >= 0 ; i-- ){
-            for( int j = m-1 ; j >= 0 ; j-- ){
-
-                if( p.charAt(j) == '?' ) dp[i][j] = dp[i+1][j+1]; 
-                else if( p.charAt(j) == '*' ) dp[i][j] = (dp[i+1][j] == 2 || dp[i][j+1] == 2) ? 2 : 1;
-                else if( s.charAt(i) == p.charAt(j) ) dp[i][j] = dp[i+1][j+1];
-                                
+    public boolean isMatch(String s, String p) {
+        int si = 0, pi = 0, match = 0, star = -1;
+        int sn = s.length(), pn = p.length();
+        while (si < sn) {
+            if (pi < pn && (p.charAt(pi) == '?' || p.charAt(pi) == s.charAt(si))) {
+                si++;
+                pi++;
+            } else if (pi < pn && p.charAt(pi) == '*') {
+                star = pi;
+                match = si;
+                pi++;
+            } else if (star != -1) {
+                pi = star + 1;
+                match++;
+                si = match;
+            } else {
+                return false;
             }
         }
-
-        return dp[0][0] == 2;
+        while (pi < pn && p.charAt(pi) == '*') {
+            pi++;
+        }
+        return pi == pn;
     }
 }
