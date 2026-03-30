a53db77a053f6cf68010ad9d7adbf6b1bd88d089
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-30 19:22:45 +0530

    Time: 1 ms (99.90%) | Memory: 43.2 MB (92.43%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,31 +1,30 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int curr[][] = new int[2][k + 1];
-        int next[][] = new int[2][k + 1];
+        int curr[] = new int[2*k + 1];
+        int next[] = new int[2*k + 1];
 
-        for( int i = n - 1 ; i >= 0 ; i-- ){
-            for( int buy = 1 ; buy >= 0 ; buy-- ){
-                for( int trans = k - 1 ; trans >= 0 ; trans-- ){
+        for (int i = n - 1; i >= 0; i--) {
+            for (int trans = 2*k - 1 ; trans >= 0 ; trans--) {
+                int profit = 0;
 
-                    int profit = 0;
-                    if( buy == 1 ){
-                        int take = -prices[i] + next[0][trans];
-                        int skip = next[1][trans];
-                        profit = Math.max( take , skip );
-                    }else{
-                        int take = +prices[i] + next[1][trans + 1];
-                        int skip = next[0][trans];
-                        profit = Math.max( take , skip );
+                    if (trans % 2 == 0) {
+                        int take = -prices[i] + next[trans + 1];
+                        int skip = next[trans];
+                        profit = Math.max(take, skip);
+                    } else {
+                        int take = prices[i] + next[trans + 1];
+                        int skip = next[trans];
+                        profit = Math.max(take, skip);
                     }
-                    curr[buy][trans] = profit;
-                }
+
+                    curr[trans] = profit;
             }
-            int[][] temp = next;
+            int[] temp = next;
             next = curr;
             curr = temp;
         }
 
-        return next[1][0];
+        return next[0];
     }
 }
