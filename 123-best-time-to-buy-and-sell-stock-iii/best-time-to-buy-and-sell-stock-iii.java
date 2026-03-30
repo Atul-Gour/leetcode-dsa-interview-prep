bd77ba5e4d2a68b0c9ce05be93297227e4425210
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-30 15:21:05 +0530

    Time: 55 ms (33.77%) | Memory: 117.8 MB (31.29%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,36 +1,32 @@
 class Solution {
-    private int solve(int index, int buy, int transactions, int[] prices, int[][][] dp) {
-        int n = prices.length;
-
-        if (index == n || transactions == 2) return 0;
-
-        if (dp[index][buy][transactions] != -1)
-            return dp[index][buy][transactions];
-
-        int profit = 0;
-
-        if (buy == 1) {
-            int take = -prices[index] + solve(index + 1, 0, transactions, prices, dp);
-            int skip = solve(index + 1, 1, transactions, prices, dp);
-            profit = Math.max(take, skip);
-        } else {
-            int take = prices[index] + solve(index + 1, 1, transactions + 1, prices, dp);
-            int skip = solve(index + 1, 0, transactions, prices, dp);
-            profit = Math.max(take, skip);
-        }
-
-        return dp[index][buy][transactions] = profit;
-    }
-
+    
     public int maxProfit(int[] prices) {
         int n = prices.length;
 
-        int[][][] dp = new int[n][2][2];
+        int[][][] dp = new int[n + 1][2][3];
 
-        for (int[][] arr2D : dp)
-            for (int[] arr : arr2D)
-                Arrays.fill(arr, -1);
+        for( int i = n-1 ; i >= 0 ; i-- ){
+            for( int j = 1 ; j >= 0 ; j--){
+                for( int buy = 0 ; buy < 2 ; buy++ ){
+                    
+                    int profit = 0;
 
-        return solve(0, 1, 0, prices, dp);
+                    if (buy == 1) {
+                        int take = -prices[i] + dp[i + 1][1 - buy][j];
+                        int skip = dp[i + 1][buy][j];
+                        profit = Math.max(take, skip);
+                    } else {
+                        int take = prices[i] + dp[i + 1][1 - buy][j+1];
+                        int skip = dp[i + 1][buy][j];
+                        profit = Math.max(take, skip);
+                    }
+
+                    dp[i][buy][j] = profit;
+
+                }
+            }
+        }
+
+        return dp[0][1][0];
     }
 }
