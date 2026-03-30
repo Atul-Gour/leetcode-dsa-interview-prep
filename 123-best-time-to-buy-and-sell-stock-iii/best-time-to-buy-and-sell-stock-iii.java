1c93a0cc09ae59371c683ad1327ac1caa2bbd4d8
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-30 15:21:25 +0530

    Time: 52 ms (39.81%) | Memory: 118.1 MB (27.34%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,28 +1,26 @@
 class Solution {
-    
     public int maxProfit(int[] prices) {
         int n = prices.length;
 
         int[][][] dp = new int[n + 1][2][3];
 
-        for( int i = n-1 ; i >= 0 ; i-- ){
-            for( int j = 1 ; j >= 0 ; j--){
-                for( int buy = 0 ; buy < 2 ; buy++ ){
-                    
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int trans = 0; trans < 2; trans++) {
+
                     int profit = 0;
 
                     if (buy == 1) {
-                        int take = -prices[i] + dp[i + 1][1 - buy][j];
-                        int skip = dp[i + 1][buy][j];
+                        int take = -prices[i] + dp[i + 1][0][trans];
+                        int skip = dp[i + 1][1][trans];
                         profit = Math.max(take, skip);
                     } else {
-                        int take = prices[i] + dp[i + 1][1 - buy][j+1];
-                        int skip = dp[i + 1][buy][j];
+                        int take = prices[i] + dp[i + 1][1][trans + 1];
+                        int skip = dp[i + 1][0][trans];
                         profit = Math.max(take, skip);
                     }
 
-                    dp[i][buy][j] = profit;
-
+                    dp[i][buy][trans] = profit;
                 }
             }
         }
