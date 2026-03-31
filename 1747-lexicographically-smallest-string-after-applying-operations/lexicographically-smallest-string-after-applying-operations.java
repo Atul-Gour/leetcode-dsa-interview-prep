d7515838d87a38908d3a0bd556deff380a42f74d
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-03-31 18:02:42 +0530

    Time: 92 ms (10.49%) | Memory: 50 MB (20.76%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.java
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.java
@@ -1,57 +1,48 @@
 class Solution {
-    String smallest ;
-    private String optionA(String sb, int a) {
-        StringBuilder newSb = new StringBuilder(sb);
-
-        for (int i = 1; i < sb.length(); i += 2) {
-            int digit = (newSb.charAt(i) - '0' + a) % 10;
-            newSb.setCharAt(i, (char)(digit + '0'));
-        }
-
-        return newSb.toString();
-    }
-    private String optionB( String sb , int b ){
-        int n = sb.length();
-        StringBuilder newSb = new StringBuilder();
-
-        for( int i = n - b ; i < n ; i++ ) newSb.append( sb.charAt(i) );
-        for( int i = 0 ; i < n - b ; i++ ) newSb.append( sb.charAt(i) );
-
-        return newSb.toString();
-    }
-    private boolean compare(String a, String b) {
-        int n = a.length();
-        int m = b.length();
-        int len = Math.min(n, m);
-
-        for (int i = 0; i < len; i++) {
-            if (a.charAt(i) < b.charAt(i)) return true;
-            if (a.charAt(i) > b.charAt(i)) return false;
-        }
-
-        return n < m;
-    }
-    private void solve( String s , int a , int b , HashSet<String> set ){
-
-        if( set.contains(s) )return;
-        set.add( s );
-
-        String temp = optionA( s , a );
-        if( compare( temp , smallest ) ){
-            smallest = temp;}
-            solve( temp , a , b , set );
-        
-
-        temp = optionB( s , b );
-        if( compare( temp , smallest ) ){
-            smallest = temp;}
-            solve( temp , a , b , set );
-        
-
-    }
     public String findLexSmallestString(String s, int a, int b) {
-        smallest = s;
-        solve( s , a , b % s.length()  , new HashSet<>() );
+        Queue<String> q = new LinkedList<>();
+        HashSet<String> visited = new HashSet<>();
+
+        String smallest = s;
+
+        q.offer(s);
+        visited.add(s);
+
+        while (!q.isEmpty()) {
+            String curr = q.poll();
+
+            if (curr.compareTo(smallest) < 0) {
+                smallest = curr;
+            }
+
+            String opA = addOperation(curr, a);
+            if (!visited.contains(opA)) {
+                visited.add(opA);
+                q.offer(opA);
+            }
+
+            String opB = rotateOperation(curr, b);
+            if (!visited.contains(opB)) {
+                visited.add(opB);
+                q.offer(opB);
+            }
+        }
+
         return smallest;
+    }
+
+    private String addOperation(String s, int a) {
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 1; i < s.length(); i += 2) {
+            int digit = (sb.charAt(i) - '0' + a) % 10;
+            sb.setCharAt(i, (char)(digit + '0'));
+        }
+        return sb.toString();
+    }
+
+    private String rotateOperation(String s, int b) {
+        int n = s.length();
+        b %= n;
+        return s.substring(n - b) + s.substring(0, n - b);
     }
 }
