9e44403ee5fec4a5adf27f070462f74e20bb3d4f
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-02 17:04:04 +0530

    Time: 103 ms (61.80%) | Memory: 237.7 MB (86.52%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
@@ -1,67 +1,65 @@
 class Solution {
-    private int solve(int i, int j, int k, int[][] coins, int[][][] dp) {
-        int n = coins.length;
-        int m = coins[0].length;
-
-        if (i >= n || j >= m)
-            return Integer.MIN_VALUE;
-
-        if (i == n - 1 && j == m - 1) {
-            if (coins[i][j] >= 0)
-                return coins[i][j];
-            else if (k < 2)
-                return 0;
-            else
-                return coins[i][j];
-        }
-
-        if (dp[i][j][k] != Integer.MIN_VALUE)
-            return dp[i][j][k];
-
-        int right = Integer.MIN_VALUE;
-        if (j + 1 < m) {
-            int next = solve(i, j + 1, k, coins, dp);
-            int take = (next == Integer.MIN_VALUE) ? Integer.MIN_VALUE : next + coins[i][j];
-
-            int skip = Integer.MIN_VALUE;
-            if (coins[i][j] < 0 && k < 2)
-                skip = solve(i, j + 1, k + 1, coins, dp);
-
-            right = Math.max(take, skip);
-        }
-
-        int down = Integer.MIN_VALUE;
-        if (i + 1 < n) {
-            int next = solve(i + 1, j, k, coins, dp);
-            int take = (next == Integer.MIN_VALUE) ? Integer.MIN_VALUE : next + coins[i][j];
-
-            int skip = Integer.MIN_VALUE;
-
-            if (coins[i][j] < 0 && k < 2)
-                skip = skip = solve(i + 1, j, k + 1, coins, dp);
-
-            down = Math.max(take, skip);
-        }
-
-        return dp[i][j][k] = Math.max(right, down);
-
-    }
 
     public int maximumAmount(int[][] coins) {
         int n = coins.length;
         int m = coins[0].length;
 
-        int[][][] dp = new int[n][m][3];
+        int[][][] dp = new int[n + 1][m + 1][3];
+
+        for (int i = 0; i <= n; i++)
+            for (int k = 0; k < 3; k++)
+                dp[i][m][k] = Integer.MIN_VALUE;
+
+        for (int j = 0; j <= m; j++)
+            for (int k = 0; k < 3; k++)
+                dp[n][j][k] = Integer.MIN_VALUE;
+
+        if (coins[n - 1][m - 1] >= 0) {
+            dp[n - 1][m - 1][0] = coins[n - 1][m - 1];
+            dp[n - 1][m - 1][1] = coins[n - 1][m - 1];
+            dp[n - 1][m - 1][2] = coins[n - 1][m - 1];
+        } else {
+            dp[n - 1][m - 1][0] = 0;
+            dp[n - 1][m - 1][1] = 0;
+            dp[n - 1][m - 1][2] = coins[n - 1][m - 1];
+        }
 
         for (int i = n - 1; i >= 0; i--) {
             for (int j = m - 1; j >= 0; j--) {
                 for (int k = 2; k >= 0; k--) {
-                    dp[i][j][k] = Integer.MIN_VALUE;
+                    
+                    if( i == n-1 && j == m-1 )continue;
+                    int right = Integer.MIN_VALUE;
+                    if (j + 1 < m) {
+                        int next = dp[i][j + 1][k];
+                        int take = (next == Integer.MIN_VALUE) ? Integer.MIN_VALUE : next + coins[i][j];
+
+                        int skip = Integer.MIN_VALUE;
+                        if (coins[i][j] < 0 && k < 2)
+                            skip = dp[i][j + 1][k + 1];
+
+                        right = Math.max(take, skip);
+                    }
+
+                    int down = Integer.MIN_VALUE;
+                    if (i + 1 < n) {
+                        int next = dp[i + 1][j][k];
+                        int take = (next == Integer.MIN_VALUE) ? Integer.MIN_VALUE : next + coins[i][j];
+
+                        int skip = Integer.MIN_VALUE;
+
+                        if (coins[i][j] < 0 && k < 2)
+                            skip = skip = dp[i + 1][j][k + 1];
+
+                        down = Math.max(take, skip);
+                    }
+
+                    dp[i][j][k] = Math.max(right, down);
                 }
             }
         }
 
-        int ans = solve(0, 0, 0, coins, dp);
+        int ans = dp[0][0][0];
 
         return ans;
 
