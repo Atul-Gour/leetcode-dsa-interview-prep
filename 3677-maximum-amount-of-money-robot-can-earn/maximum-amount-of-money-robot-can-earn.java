6d9672dc6445dfa86078210e9aca5446ce85ece7
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-02 16:08:41 +0530

    Time: 215 ms (10.11%) | Memory: 238.2 MB (67.42%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
@@ -3,40 +3,43 @@ class Solution {
         int n = coins.length;
         int m = coins[0].length;
 
-        if( k > 2 )return Integer.MAX_VALUE;
-        if (i >= n || j >= m)return 0;
+        if (i >= n || j >= m) return Integer.MIN_VALUE;
 
-        if( i == n-1 && j == m-1 ){
-            if( coins[i][j] > 0 ) return coins[i][j];
-            else if( k < 2 ) return 0;
+        if (i == n - 1 && j == m - 1) {
+            if (coins[i][j] >= 0) return coins[i][j];
+            else if (k < 2) return 0;
             else return coins[i][j];
         }
 
-        if( dp[i][j][k] != Integer.MIN_VALUE ) return dp[i][j][k];
+        if (dp[i][j][k] != Integer.MIN_VALUE) return dp[i][j][k];
 
         int right = Integer.MIN_VALUE;
         if (j + 1 < m) {
-            int take = solve(i , j+1 , k , coins , dp ) + coins[i][j];
-            int skip = Integer.MIN_VALUE;
+            int next = solve(i, j + 1, k, coins, dp);
+            int take = (next == Integer.MIN_VALUE) ? Integer.MIN_VALUE : next + coins[i][j];
 
-            if (coins[i][j] < 0 && k < 2) skip = solve(i , j+1 , k+1 , coins , dp );
+            int skip = Integer.MIN_VALUE;
+            if (coins[i][j] < 0 && k < 2) {
+                skip = solve(i, j + 1, k + 1, coins, dp);
+            }
 
             right = Math.max(take, skip);
         }
 
         int down = Integer.MIN_VALUE;
         if (i + 1 < n) {
-            int take = solve(i+1 , j , k , coins , dp ) + coins[i][j];
-            int skip = Integer.MIN_VALUE;
+            int next = solve(i + 1, j, k, coins, dp);
+            int take = (next == Integer.MIN_VALUE) ? Integer.MIN_VALUE : next + coins[i][j];
 
-            if (coins[i][j] < 0 && k < 2) skip = skip = solve(i+1 , j , k+1 , coins , dp );
-            
-            down = Math.max(take, skip) ;
+            int skip = Integer.MIN_VALUE;
+            if (coins[i][j] < 0 && k < 2) {
+                skip = solve(i + 1, j, k + 1, coins, dp);
+            }
+
+            down = Math.max(take, skip);
         }
 
-
         return dp[i][j][k] = Math.max(right, down);
-
     }
 
     public int maximumAmount(int[][] coins) {
@@ -44,20 +47,10 @@ class Solution {
         int m = coins[0].length;
 
         int[][][] dp = new int[n][m][3];
+        for (int[][] layer : dp)
+            for (int[] row : layer)
+                Arrays.fill(row, Integer.MIN_VALUE);
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 0; j--) {
-                for (int k = 2; k >= 0; k--) {
-                    dp[i][j][k] = Integer.MIN_VALUE;
-                }
-            }
-        }
-
-        int ans = solve( 0 , 0 , 0 , coins , dp);
-
-        return ans;
-
-        
-
+        return solve(0, 0, 0, coins, dp);
     }
 }
