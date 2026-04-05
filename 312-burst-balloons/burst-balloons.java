3de3ea0cc06ad77f4cfc9c3dedbe73802865e565
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-05 17:22:26 +0530

    Time: 112 ms (35.56%) | Memory: 44.6 MB (21.65%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,38 +1,35 @@
 class Solution {
+    
+    private int solve( int i , int j , int[] arr , int[][] dp ){
 
-    int[][] dp;
+        if( dp[i][j] != -1 ) return dp[i][j];
+        if( i == j ) return dp[i][j] = arr[i - 1] * arr[i] * arr[i + 1] ;
+        int ans = 0;
 
-    public int maxCoins(int[] nums) {
+        for( int k = i ; k <= j ; k++ ){
+            int cost = solve( i , k - 1 , arr , dp ) +
+                       solve( k + 1 , j , arr , dp ) + 
+                       arr[i - 1] * arr[k] * arr[j + 1];      
 
-        int n = nums.length;
-
-        int[] arr = new int[n + 2];
-        arr[0] = 1;
-        arr[n + 1] = 1;
-
-        for(int i = 0; i < n; i++)
-            arr[i + 1] = nums[i];
-
-        dp = new int[n + 2][n + 2];
-
-        for( int i = 1 ; i < n + 1 ; i++ ){
-            dp[i][i] = arr[i-1] * arr[i] * arr[i+1];
+            ans = Math.max( ans , cost );  
         }
 
-        for( int len = 1 ; len < n ; len++  ){
-            for( int i = 1 ; i + len < n + 1 ; i++ ){
-                int j = i + len;
-                int max = Integer.MIN_VALUE;
-
-                for( int k = i ; k <= j ; k++ ){
-                    max = Math.max( max , dp[i][k-1] + ( arr[i-1] * arr[k] * arr[j+1] ) + dp[k+1][j] );
-                }
-
-                dp[i][j] = max;
-            }
-        }
-
-        return dp[1][n];
+        return dp[i][j] = ans;
     }
 
+    public int maxCoins(int[] nums) {
+        int arr[] = new int[nums.length + 2];
+        int n = arr.length;
+        arr[0] = 1;
+        arr[ n - 1 ] = 1;
+
+        for( int i = 1 ; i < n - 1 ; i++ ){
+            arr[i] = nums[i - 1];
+        }
+
+        int[][] dp = new int[n][n];
+        for( int[] d: dp ) Arrays.fill( d , -1 );
+
+        return solve( 1 , n - 2 , arr , dp );
+    }
 }
