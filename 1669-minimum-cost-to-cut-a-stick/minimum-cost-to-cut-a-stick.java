f22267a6f8cdaac60c877b564fba6ff5b8d0f9bb
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-05 16:16:09 +0530

    Time: 14 ms (95.83%) | Memory: 44.4 MB (65.77%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,41 +1,35 @@
 class Solution {
 
-    private int solve(int i, int j, int[] arr, int[][] dp) {
-        if (i > j) return 0;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int ans = Integer.MAX_VALUE;
-
-        for (int k = i; k <= j; k++) {
-
-            int cost = (arr[j + 1] - arr[i - 1])
-                    + solve(i, k - 1, arr, dp)
-                    + solve(k + 1, j, arr, dp);
-
-            ans = Math.min(ans, cost);
-        }
-
-        return dp[i][j] = ans;
-    }
-
     public int minCost(int n, int[] cuts) {
-
-        Arrays.sort(cuts);
-
+        
+        Arrays.sort( cuts );
         int arr[] = new int[cuts.length + 2];
-        arr[0] = 0;
-        arr[arr.length - 1] = n;
-
-        for (int i = 0; i < cuts.length; i++) {
-            arr[i + 1] = cuts[i];
-        }
-
         int nn = arr.length;
+
+        for( int i = 0 ; i < cuts.length ; i++ ){
+            arr[ i + 1 ] = cuts[i];
+        }
+        arr[ nn - 1 ] = n;
+
         int dp[][] = new int[nn][nn];
 
-        for (int[] d : dp) Arrays.fill(d, -1);
+        for( int len = 2 ; len < nn ; len++ ){
+            for( int i = 1 ; i + len - 1 < nn ; i++ ){
+                int j = i + len - 1;
 
-        return solve(1, nn - 2, arr, dp);
+                int ans = Integer.MAX_VALUE;
+                for( int k = i ; k < j ; k++ ){
+                    int a = dp[i][k];
+                    int b = dp[k + 1][j];
+                    int c = arr[j] - arr[i - 1];
+
+                    ans = Math.min( ans , a + b + c );
+                }
+                dp[i][j] = ans;
+
+            }
+        }
+
+        return dp[1][nn - 1];
     }
 }
