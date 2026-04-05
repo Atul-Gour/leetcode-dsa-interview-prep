2ecc726747565a8335ae2b05e02209db3b9f7f0e
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-05 16:19:02 +0530

    Time: 15 ms (86.65%) | Memory: 44.7 MB (34.47%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -2,35 +2,40 @@ class Solution {
 
     public int minCost(int n, int[] cuts) {
         
-        Arrays.sort( cuts );
+        Arrays.sort(cuts);
+
         int arr[] = new int[cuts.length + 2];
         int nn = arr.length;
 
-        for( int i = 0 ; i < cuts.length ; i++ ){
-            arr[ i + 1 ] = cuts[i];
+        arr[0] = 0;
+        arr[nn - 1] = n;
+
+        for (int i = 0; i < cuts.length; i++) {
+            arr[i + 1] = cuts[i];
         }
-        arr[ nn - 1 ] = n;
 
         int dp[][] = new int[nn][nn];
 
-        for( int len = 2 ; len < nn ; len++ ){
-            for( int i = 1 ; i + len - 1 < nn ; i++ ){
+        for (int len = 1; len <= nn - 2; len++) {
+
+            for (int i = 1; i + len - 1 <= nn - 2; i++) {
+
                 int j = i + len - 1;
-
                 int ans = Integer.MAX_VALUE;
-                for( int k = i ; k < j ; k++ ){
 
-                    int cost =  dp[i][k] +
-                                dp[k + 1][j] +
-                                arr[j] - arr[i - 1];
+                for (int k = i; k <= j; k++) {
 
-                    ans = Math.min( ans , cost );
+                    int cost = dp[i][k - 1] +
+                               dp[k + 1][j] +
+                               arr[j + 1] - arr[i - 1];
+
+                    ans = Math.min(ans, cost);
                 }
-                dp[i][j] = ans;
 
+                dp[i][j] = ans == Integer.MAX_VALUE ? 0 : ans;
             }
         }
 
-        return dp[1][nn - 1];
+        return dp[1][nn - 2];
     }
 }
