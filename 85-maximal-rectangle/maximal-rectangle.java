bee5399609792a8fa21e891e4d1e45a45c23bacb
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-06 16:59:12 +0530

    Time: 18 ms (80.37%) | Memory: 50.8 MB (47.39%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,65 +1,47 @@
 class Solution {
-
-    private int maxRectangle( int[] nums ){
-        int n = nums.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        Stack<Integer> st = new Stack<>();
-
-        for( int i = 0 ; i < n ; i++ ){
-            while( !st.isEmpty() && nums[st.peek()] >= nums[i] ){
-                st.pop();
-            }
-
-            if( st.isEmpty() ) left[i] = -1;
-            else left[i] = st.peek();
-            
-            st.push(i);
-        }
-
-        st.clear();
-
-        for( int i = n-1 ; i >= 0 ; i-- ){
-            while( !st.isEmpty() && nums[st.peek()] >= nums[i] ){
-                st.pop();
-            }
-
-            if( st.isEmpty() ) right[i] = n;
-            else right[i] = st.peek();
-            
-            st.push(i);
-        }
-
-        int maxRec = 0;
-
-        for( int i = 0 ; i < n ; i++ ){
-            maxRec = Math.max( maxRec , nums[i] * (right[i] - left[i] - 1) );
-        }
-
-        return maxRec;
-    }
-
     public int maximalRectangle(char[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        int[][] grid = new int[n + 1][m];
-        
-        for( int i = n - 1 ; i >= 0 ; i-- ){
-            for( int j = 0 ; j < m ; j++ ){
-                if( matrix[i][j] == '0' ) continue;
-                grid[i][j] = grid[i+1][j] + (matrix[i][j] == '1' ? 1 : 0);
-            }
-        }
-
-
+        int[] heights = new int[m];
         int ans = 0;
 
-        for( int i = 0 ; i < n ; i++ ){
-            int maxRec = maxRectangle( grid[i] );
-            ans = Math.max( ans , maxRec );
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1') {
+                    heights[j]++;
+                } else {
+                    heights[j] = 0;
+                }
+            }
+
+            ans = Math.max(ans, maxRectangle(heights));
         }
 
         return ans;
+    }
+
+    private int maxRectangle(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        int max = 0;
+
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n) ? 0 : nums[i];
+
+            while (!st.isEmpty() && nums[st.peek()] > h) {
+                int height = nums[st.pop()];
+                int right = i;
+                int left = st.isEmpty() ? -1 : st.peek();
+                int width = right - left - 1;
+
+                max = Math.max(max, height * width);
+            }
+
+            st.push(i);
+        }
+
+        return max;
     }
 }
