40e6cc2bf77ec3050d0beca96e633393ec75329d
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-06 15:35:31 +0530

    Time: 6 ms (94.94%) | Memory: 43.9 MB (72.02%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,27 +1,25 @@
 class Solution {
-    private int solve( int i , int j , int k , int[] arr , int[][] dp ){
-        if( i > j ) return 0;
-        if( dp[i][j] != -1 ) return dp[i][j];
-
-        int maxValue = 0;
-        int ans = 0;
-
-        for( int step = 0 ; step < k   ; step++ ){
-            if( i + step > j )continue;
-            maxValue = Math.max( maxValue , arr[i + step] );
-
-            int profit = ((step + 1) * maxValue) + solve( i + step + 1 , j , k , arr , dp );
-            ans = Math.max( profit , ans );
-        }
-
-        return dp[i][j] = ans;
-    }
 
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int n = arr.length;
-        int dp[][] = new int[n][n] ;
-        for( int[] d : dp ) Arrays.fill( d , -1 );
+        int dp[] = new int[n + 1];
 
-        return solve( 0 , n - 1 , k , arr , dp );
+        for( int i = n - 1 ; i >= 0 ; i-- ){
+            int maxValue = 0;
+            int ans = 0;
+
+            for( int step = 0 ; step < k   ; step++ ){
+                int mid = i + step;
+                if( mid >= n )continue;
+                maxValue = Math.max( maxValue , arr[mid] );
+
+                int profit = ((step + 1) * maxValue) + dp[mid + 1];
+                ans = Math.max( profit , ans );
+            }
+
+            dp[i] = ans;
+        }
+
+        return dp[0];
     }
 }
