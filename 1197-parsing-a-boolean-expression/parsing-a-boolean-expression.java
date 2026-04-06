d23e9999bb21db9d42671d831084db38b69f863b
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-06 12:30:57 +0530

    Time: 6 ms (84.94%) | Memory: 44.8 MB (40.81%) - LeetSync

---
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.java
@@ -0,0 +1,69 @@
+class Solution {
+    private ArrayList<String> splitExpression( String s ){
+
+        int n = s.length();
+        ArrayList<String> list = new ArrayList<>();
+
+        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for( int i = 0 ; i < n ; i++ ){
+
+            char ch = s.charAt(i);
+
+            if( ch == '(' ){
+                st.push('(');
+                sb.append(ch);
+            }
+            else if( ch == ')' ){
+                st.pop();
+                sb.append(ch);
+            }
+            else if( ch == ',' ){
+                if( st.isEmpty() ){
+                    list.add( sb.toString() );
+                    sb.setLength(0);
+                }
+                else sb.append(ch);
+            }
+            else sb.append( ch );
+        }
+
+        if( !sb.isEmpty() ) list.add(sb.toString());
+        return list;
+    }
+
+    public boolean parseBoolExpr(String expression) {
+        
+        char ch = expression.charAt(0);
+
+        if( ch == 't' ) return true;
+        else if( ch == 'f' ) return false;
+        else if( ch == '&' ){
+            String subExpression = expression.substring( 2 , expression.length() - 1 );
+            ArrayList<String> subExpressions = splitExpression( subExpression );
+
+            for( String exp : subExpressions ){
+                if( !parseBoolExpr( exp ) ) return false;
+            }
+
+            return true;
+        }
+        else if( ch == '|' ){
+            String subExpression = expression.substring( 2 , expression.length() - 1 );
+            ArrayList<String> subExpressions = splitExpression( subExpression );
+
+            for( String exp : subExpressions ){
+                if( parseBoolExpr( exp ) ) return true;
+            }
+
+            return false;
+        }
+        else if( ch == '!' ){
+            String subExpression = expression.substring( 2 , expression.length() - 1 );
+
+            return !parseBoolExpr(subExpression);
+        }
+        else return false;
+    }
+}
