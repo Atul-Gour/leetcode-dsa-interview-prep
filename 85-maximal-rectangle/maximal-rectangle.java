cdf326eb96317eced38e9ebe6a90b0ec75260fe6
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-06 16:08:13 +0530

    Time: 15 ms (85.86%) | Memory: 52.1 MB (6.95%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -3,62 +3,32 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        int arr[][] = new int[n][m];
-
-        for(int i = 0 ; i < n ; i++ ){
-            for(int j = 0 ; j < m ; j++ ){
-                arr[i][j] = matrix[i][j] - '0';
+        int[][] grid = new int[n + 1][m];
+        
+        for( int i = n - 1 ; i >= 0 ; i-- ){
+            for( int j = 0 ; j < m ; j++ ){
+                if( matrix[i][j] == '0' ) continue;
+                grid[i][j] = grid[i+1][j] + (matrix[i][j] == '1' ? 1 : 0);
             }
         }
 
-        for(int i = n-2 ; i >= 0 ; i-- ){
-            for(int j = 0 ; j < m ; j++ ){
-                if( arr[i][j] == 1 ){
-                    arr[i][j] = arr[i + 1][j] + 1;
-                }
-            }
-        }
 
-        Stack<Integer> st = new Stack<>();
-        int left[] = new int[m];
-        int right[] = new int[m];
         int ans = 0;
 
-        for(int a[] : arr){
+        for( int i = 0 ; i < n ; i++ ){
+            for( int j = 0 ; j < m ; j++ ){
 
-            for(int i = 0 ; i < m ; i++){
-                while(!st.isEmpty() && a[st.peek()] >= a[i] ){
-                    st.pop();
+                int min = Integer.MAX_VALUE;
+
+                for( int k = j ; k >= 0 ; k-- ){
+                    min = Math.min( min , grid[i][k] );
+                    if( min == 0 ) break;
+                    int cost =  min * ( j - k + 1 );
+                    ans = Math.max( ans , cost );
                 }
-
-                if( st.isEmpty() )
-                    left[i] = -1;
-                else 
-                    left[i] = st.peek();
-
-                st.push(i);
-            }
-
-            st.clear();
-
-            for( int i = m-1 ; i >= 0 ; i-- ){
-                while(!st.isEmpty() && a[st.peek()] >= a[i] ){
-                    st.pop();
-                }
-
-                if( st.isEmpty() )
-                    right[i] = m;
-                else 
-                    right[i] = st.peek();
-
-                st.push(i);
-            }
-            st.clear();
-
-            for( int i = 0 ; i < m ; i++ ){
-                ans = Math.max( ans , a[i] * ( right[i] - left[i] - 1 ) );
             }
         }
+
         return ans;
     }
 }
