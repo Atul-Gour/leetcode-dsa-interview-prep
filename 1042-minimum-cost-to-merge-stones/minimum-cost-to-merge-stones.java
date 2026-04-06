615e6edb2962874d5d6b849a600672ff217a096d
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-06 10:41:49 +0530

    Time: 3 ms (69.64%) | Memory: 42.5 MB (99.01%) - LeetSync

--- 1042-minimum-cost-to-merge-stones/minimum-cost-to-merge-stones.java
+++ 1042-minimum-cost-to-merge-stones/minimum-cost-to-merge-stones.java
@@ -1,37 +1,34 @@
+import java.util.*;
+
 class Solution {
-    private int solve( int i , int j , int k , int[] stones , int[][] dp , int[] prefix ){
-
-        if( i == j ) return 0;
-        if( dp[i][j] != -1 ) return dp[i][j];
-
-        int ans = Integer.MAX_VALUE;
-        
-        for( int m = i ; m < j ; m += (k - 1) ){
-            int left = solve( i , m , k , stones , dp , prefix );
-            int right = solve( m + 1 , j , k , stones , dp , prefix );
-
-            ans = Math.min( ans , left + right );
-        }
-
-        if( (j - i) % (k - 1) == 0 ){
-            ans += prefix[j + 1] - prefix[i];
-        }
-        
-        return dp[i][j] = ans;
-    }
-
     public int mergeStones(int[] stones, int k) {
         int n = stones.length;
+
         if ((n - 1) % (k - 1) != 0) return -1;
 
         int[] prefix = new int[n + 1];
-        for( int i = 0 ; i < n ; i++ ) prefix[i + 1] = prefix[i] + stones[i];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + stones[i];
+        }
 
-        int dp[][] = new int[n][n];
-        for( int[] d : dp )Arrays.fill( d , -1);
+        int[][] dp = new int[n][n];
 
-        int ans = solve( 0 , n - 1 , k , stones , dp , prefix );
+        for (int len = k; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                int j = i + len - 1;
+                dp[i][j] = Integer.MAX_VALUE;
 
-        return ans;
+                for (int mid = i; mid < j; mid += (k - 1)) {
+                    dp[i][j] = Math.min(dp[i][j],
+                        dp[i][mid] + dp[mid + 1][j]);
+                }
+
+                if ((len - 1) % (k - 1) == 0) {
+                    dp[i][j] += prefix[j + 1] - prefix[i];
+                }
+            }
+        }
+
+        return dp[0][n - 1];
     }
 }
