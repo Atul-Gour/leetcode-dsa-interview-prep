c3b8c52780a7131934b0bd5a15ea7e6e2ff08a36
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-08 13:47:17 +0530

    Time: 17 ms (17.20%) | Memory: 51.8 MB (77.37%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -1,53 +1,75 @@
 class Solution {
-    public int countPaths(int n, int[][] roads) {
-        long ans = 0;
-        int MOD = 1_000_000_007;
-        ArrayList<int[]>[] adj = new ArrayList[n];
-        PriorityQueue<long[]> q = new PriorityQueue<>((a, b) -> Long.compare(a[1], b[1]));
 
-        long[] arr = new long[n];
+    static final long MOD = 1_000_000_007;
+
+    static class Node{
+        int node ;
+        long time;
+
+        Node( int node , long time ){
+            this.node = node;
+            this.time = time;
+        }  
+    }
+
+    static private long dijkastra( int src , int dst , ArrayList<int[]>[] graph){
+
+        int n = graph.length;
+        
+        PriorityQueue< Node > pq = new PriorityQueue<>( (a , b) -> Long.compare( a.time , b.time ) );
+        long[] dist = new long[n];
         long[] ways = new long[n];
+        Arrays.fill( dist , Long.MAX_VALUE );
 
-        Arrays.fill(arr, Long.MAX_VALUE);
+        ways[src] = 1;
+        dist[src] = 0;
+        pq.offer( new Node( src , 0 ) );
 
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<>();
-        }
+        while( !pq.isEmpty() ){
+            Node curr = pq.poll();
+            int u = curr.node;
+            long currTime = curr.time ;
 
-        for (int[] road : roads) {
-            adj[road[0]].add(new int[] { road[1], road[2] });
-            adj[road[1]].add(new int[] { road[0], road[2] });
-        }
+            if( dist[u] < currTime ) continue;
 
-        q.offer(new long[] { 0, 0 });
-        arr[0] = 0;
-        ways[0] = 1;
+            for( int[] neigh : graph[u] ){
+                int v = neigh[0];
+                long time = neigh[1];
+                long newTime = currTime + time ;
 
-        while (!q.isEmpty()) {
-            long curr[] = q.poll();
-            int currNode = (int)curr[0];
-            long currDist = curr[1];
-            long currWays = ways[currNode];
+                if( newTime == dist[v] ) ways[v] = (ways[u] + ways[v]) % MOD ;
+                if( newTime >= dist[v] ) continue;
 
-            if (currDist > arr[currNode])
-                continue;
+                if( newTime < dist[v] ) ways[v] = ways[u];
 
-            for (int neigh[] : adj[currNode]) {
+                dist[v] = newTime;
 
-                long totalDist = currDist + neigh[1];
-
-                if (arr[neigh[0]] > totalDist) {
-                    ways[neigh[0]] = (currWays) % MOD;
-                    arr[neigh[0]] = totalDist;
-                    q.offer(new long[] { neigh[0], totalDist });
-                }
-                else if( arr[neigh[0]] == totalDist ){
-                    ways[neigh[0]] = (ways[neigh[0]] + currWays) % MOD;
-                }
+                pq.offer( new Node( v , newTime ) );
 
             }
         }
 
-        return (int) ways[n-1];
+        return  ways[dst] % MOD;
+
+    }
+
+    public int countPaths(int n, int[][] roads) {
+        boolean[] visited = new boolean[n];
+        ArrayList<int[]>[] graph = new ArrayList[n];
+
+        for( int i = 0 ; i < n ; i++ ) graph[i] = new ArrayList<>();
+
+        for( int[] road : roads ){
+            int u = road[0];
+            int v = road[1];
+            int time = road[2];
+
+            graph[u].add( new int[]{ v , time } );
+            graph[v].add( new int[]{ u , time } );
+        }
+
+        long cost = dijkastra( 0, n-1, graph );
+
+        return (int)cost;
     }
 }
