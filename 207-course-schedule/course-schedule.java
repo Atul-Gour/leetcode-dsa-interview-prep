14280bf8f35b4274fe219efb2d805c46356cd585
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-09 19:20:27 +0530

    Time: 4 ms (92.96%) | Memory: 47.3 MB (38.06%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,21 +1,27 @@
 class Solution {
-    private boolean hasCycleDfs( int node , boolean[] visited , boolean[] pathVisited , List<List<Integer>> graph ){
+    private boolean hasCycleDfs(int node, boolean[] visited, boolean[] pathVisited, List<List<Integer>> graph) {
 
         visited[node] = true;
         pathVisited[node] = true;
 
-        for( int neigh : graph.get(node) ){
-            if( visited[neigh] && pathVisited[neigh] ) return true;
+        for (int neigh : graph.get(node)) {
 
-            if( !visited[neigh] ){
-                if( hasCycleDfs( neigh , visited , pathVisited , graph ) ) return true;
+            if (!visited[neigh]) {
+                if (hasCycleDfs(neigh, visited, pathVisited, graph)) {
+                    pathVisited[node] = false;
+                    return true;
+                }
+            } 
+            else if (pathVisited[neigh]) {
+                pathVisited[node] = false;
+                return true;
             }
         }
 
-        pathVisited[node] = false;
+        pathVisited[node] = false; // normal backtrack
         return false;
-
     }
+
     public boolean canFinish(int V, int[][] edges) {
 
         List<List<Integer>> graph = new ArrayList<>();
@@ -29,17 +35,15 @@ class Solution {
         for (int[] edge : edges) {
             int u = edge[0];
             int v = edge[1];
-
             graph.get(v).add(u);
         }
 
-        for( int i = 0 ; i < V ; i++ ){
-            if( !visited[i] ){
-                if( hasCycleDfs( i , visited , pathVisited , graph ) ) return false;
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (hasCycleDfs(i, visited, pathVisited, graph)) return false;
             }
         }
 
         return true;
-
     }
 }
