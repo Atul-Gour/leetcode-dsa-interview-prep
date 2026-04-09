b26143ade821a6390c10969f4106014b56726171
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-09 18:08:41 +0530

    Time: 1 ms (40.73%) | Memory: 47.1 MB (6.54%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,39 +1,36 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
         int n = image.length;
         int m = image[0].length;
 
         int startingColor = image[sr][sc];
 
-        boolean[][] visited = new boolean[n][m];
+        if (startingColor == color) return image;
+
         ArrayDeque<int[]> q = new ArrayDeque<>();
-        int[][] dirs = new int[][] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+        int[][] dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
 
-
-
-        q.offer(new int[] { sr, sc });
+        q.offer(new int[]{sr, sc});
         image[sr][sc] = color;
-        visited[sr][sc] = true;
 
         while (!q.isEmpty()) {
-            int curr[] = q.poll();
+            int[] curr = q.poll();
             int i = curr[0];
             int j = curr[1];
 
             for (int[] dir : dirs) {
-                int newI = i + dir[0];
-                int newJ = j + dir[1];
+                int ni = i + dir[0];
+                int nj = j + dir[1];
 
-                if (newI >= n || newI < 0 || newJ >= m || newJ < 0 || image[newI][newJ] != startingColor || visited[newI][newJ] )
-                    continue;
-                
-                visited[newI][newJ] = true;
-                image[newI][newJ] = color;
-                q.offer(new int[] { newI, newJ });
+                if (ni < 0 || nj < 0 || ni >= n || nj >= m) continue;
+                if (image[ni][nj] != startingColor) continue;
+
+                image[ni][nj] = color;
+                q.offer(new int[]{ni, nj});
             }
         }
 
         return image;
-
     }
 }
