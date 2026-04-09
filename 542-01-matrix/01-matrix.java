20487bcbb7fa71812108ab8c711c60fccabc23f6
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-09 19:51:09 +0530

    Time: 16 ms (28.73%) | Memory: 49.2 MB (52.23%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -2,39 +2,44 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] visited = new boolean[n][m];
-        int[][] dirs = {{-1 , 0} , {1 , 0} , {0 , -1} , {0 , 1}};
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if( mat[i][j] == 1 )
-                    mat[i][j] = Integer.MAX_VALUE;
-                else{
-                    q.offer( new int[]{i , j});
-                    visited[i][j] = true;
-                }
+        int[][] ans = new int[n][m];
+        ArrayDeque< int[] > q = new ArrayDeque<>();
+
+        for( int i = 0 ; i < n ; i++ ){
+            for( int j = 0 ; j < m ; j++ ){
+                if( mat[i][j] == 0  ) q.offer( new int[]{ i , j } );
+                else ans[i][j] = Integer.MAX_VALUE;
             }
         }
-        int time = 0;
+
+        int[][] dirs = { {0 , 1} , {1 , 0} , {-1 , 0} ,{0 , -1}};
+        int dist = 1;
+
         while( !q.isEmpty() ){
             int size = q.size();
+            
+            while( size-- > 0 ){
+                int curr[] = q.poll();
+                int i = curr[0];
+                int j = curr[1];
 
-            for(int i = 0 ; i < size ; i++){
-                int[] curr = q.poll();
-                mat[curr[0]][curr[1]] = time;
+                for( int[] d : dirs ){
+                    int newI = i + d[0];
+                    int newJ = j + d[1];
 
-                for( int d[] : dirs){
-                    int newI = curr[0] + d[0];
-                    int newJ = curr[1] + d[1];
-                    if( newI >= 0 && newI < n && newJ >= 0 && newJ < m && !visited[newI][newJ] && mat[newI][newJ] == Integer.MAX_VALUE ){
-                        visited[newI][newJ] = true;
-                        q.offer( new int[]{ newI , newJ });
-                    }
+                    if( newI < 0 || newJ < 0 || newI >= n || newJ >= m )continue;
+                    if( ans[newI][newJ] <= dist )continue;
+
+                    ans[newI][newJ] = dist;
+                    q.offer( new int[]{ newI , newJ } );
                 }
             }
-            time++;
+            
+            dist++;
         }
-        return mat;
+
+        return ans;
+
     }
 }
