097feebbc1f95c95a53504ffc1ddf95169d60511
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-09 19:51:57 +0530

    Time: 15 ms (44.34%) | Memory: 49 MB (67.92%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -4,42 +4,39 @@ class Solution {
         int m = mat[0].length;
 
         int[][] ans = new int[n][m];
-        ArrayDeque< int[] > q = new ArrayDeque<>();
+        ArrayDeque<int[]> q = new ArrayDeque<>();
 
-        for( int i = 0 ; i < n ; i++ ){
-            for( int j = 0 ; j < m ; j++ ){
-                if( mat[i][j] == 0  ) q.offer( new int[]{ i , j } );
-                else ans[i][j] = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    q.offer(new int[]{i, j});
+                    ans[i][j] = 0;
+                } else {
+                    ans[i][j] = Integer.MAX_VALUE;
+                }
             }
         }
 
-        int[][] dirs = { {0 , 1} , {1 , 0} , {-1 , 0} ,{0 , -1}};
-        int dist = 1;
+        int[][] dirs = {{0,1},{1,0},{-1,0},{0,-1}};
 
-        while( !q.isEmpty() ){
-            int size = q.size();
-            
-            while( size-- > 0 ){
-                int curr[] = q.poll();
-                int i = curr[0];
-                int j = curr[1];
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int i = curr[0];
+            int j = curr[1];
 
-                for( int[] d : dirs ){
-                    int newI = i + d[0];
-                    int newJ = j + d[1];
+            for (int[] d : dirs) {
+                int ni = i + d[0];
+                int nj = j + d[1];
 
-                    if( newI < 0 || newJ < 0 || newI >= n || newJ >= m )continue;
-                    if( ans[newI][newJ] <= dist )continue;
+                if (ni < 0 || nj < 0 || ni >= n || nj >= m) continue;
 
-                    ans[newI][newJ] = dist;
-                    q.offer( new int[]{ newI , newJ } );
+                if (ans[ni][nj] > ans[i][j] + 1) {
+                    ans[ni][nj] = ans[i][j] + 1;
+                    q.offer(new int[]{ni, nj});
                 }
             }
-            
-            dist++;
         }
 
         return ans;
-
     }
 }
