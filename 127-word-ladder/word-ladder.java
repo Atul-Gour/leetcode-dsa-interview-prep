712c017966146d49e67a57a7e4e09718c4d4e284
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-10 16:23:07 +0530

    Time: 78 ms (52.08%) | Memory: 47.3 MB (92.99%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,37 +1,49 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
         HashSet<String> set = new HashSet<>();
-        set.addAll(wordList);
-        if( !set.contains(endWord) )return 0;
-        Queue<String> q = new ArrayDeque<>();
+        ArrayDeque<String> q = new ArrayDeque<>();
 
-        q.offer(beginWord);
+        for( String word : wordList ) set.add(word);
+        set.remove( beginWord );
 
-        int steps = 1;
+        if( !set.contains(endWord) ) return 0;
+
+        q.offer( beginWord );
+        int step = 1;
 
         while( !q.isEmpty() ){
             int size = q.size();
 
             while( size-- > 0 ){
-                char[] curr = q.poll().toCharArray();
+                char curr[] = q.poll().toCharArray();
 
                 for( int i = 0 ; i < curr.length ; i++ ){
-                    char ch = curr[i];
-                    for( char c = 'a' ; c <= 'z' ; c++ ){
-                        if( c == ch )continue;
-                        curr[i] = c;
-                        String newString = new String(curr);
-                        if( newString.equals(endWord) )return steps + 1;
+                    char originalChar = curr[i];
+
+                    for( char ch = 'a' ; ch <= 'z' ; ch++ ){
+
+                        if( ch == originalChar ) continue;
+
+                        curr[i] = ch;
+                        String newString  = new String(curr);
+
+                        if( newString.equals( endWord ) ) return step + 1;
+
                         if( set.contains( newString ) ){
                             q.offer( newString );
                             set.remove( newString );
                         }
                     }
-                    curr[i] = ch;
+
+                    curr[i] = originalChar;
                 }
             }
-            steps++;
+
+            step++;
         }
+
         return 0;
+
     }
 }
