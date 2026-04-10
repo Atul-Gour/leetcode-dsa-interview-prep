9ac2b2206e7427ba736c542391a39638e40fbbce
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-10 14:05:06 +0530

    Time: 2 ms (78.84%) | Memory: 47.8 MB (62.99%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,40 +1,40 @@
 class Solution {
-    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
 
-    public void solve(char[][] board) {
-        int n = board.length, m = board[0].length;
+    private void dfs(int i, int j, char[][] board ) {
+        int n = board.length;
+        int m = board[0].length;
 
-        for (int i = 0; i < n; i++) {
-            if (board[i][0] == 'O') bfs(i, 0, board);
-            if (board[i][m-1] == 'O') bfs(i, m-1, board);
+        board[i][j] = '#';
+
+        int[][] dirs = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
+        for (int[] dir : dirs) {
+
+            int newI = i + dir[0];
+            int newJ = j + dir[1];
+
+            if (newI < 0 || newI > n - 1 || newJ < 0 || newJ > m - 1 || board[newI][newJ] == 'X' || board[newI][newJ] == '#' ) continue;
+
+            dfs(newI, newJ, board ) ;
         }
 
-        for (int j = 0; j < m; j++) {
-            if (board[0][j] == 'O') bfs(0, j, board);
-            if (board[n-1][j] == 'O') bfs(n-1, j, board);
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 'O') board[i][j] = 'X';
-                else if (board[i][j] == '#') board[i][j] = 'O';
-            }
-        }
     }
 
-    private void bfs(int x, int y, char[][] board) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x, y});
-        board[x][y] = '#';
+    public void solve(char[][] board) {
+        int n = board.length;
+        int m = board[0].length;
 
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            for (int[] d : dirs) {
-                int ni = cur[0] + d[0], nj = cur[1] + d[1];
-                if (ni >= 0 && nj >= 0 && ni < board.length && nj < board[0].length && board[ni][nj] == 'O') {
-                    board[ni][nj] = '#';
-                    q.add(new int[]{ni, nj});
-                }
+        boolean[][] pathVisited = new boolean[n][m];
+
+        for( int i = 0 ; i < n ; i++ ) if( board[i][0] == 'O' ) dfs( i , 0 , board );
+        for( int i = 0 ; i < n ; i++ ) if( board[i][m-1] == 'O' ) dfs( i , m-1 , board );
+        for( int j = 0 ; j < m ; j++ ) if( board[0][j] == 'O' ) dfs( 0 , j , board );
+        for( int j = 0 ; j < m ; j++ ) if( board[n-1][j] == 'O' ) dfs( n-1 , j , board );
+
+        for( int i = 0 ; i < n ; i++ ){
+            for( int j = 0 ; j < m ; j++ ){
+                if( board[i][j] == '#' ) board[i][j] = 'O';
+                else if( board[i][j] == 'O' ) board[i][j] = 'X';
             }
         }
     }
