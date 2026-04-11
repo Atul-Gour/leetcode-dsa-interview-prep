81dcb3ebab0004e31fabef6c9c6519a858fa3156
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-11 10:59:49 +0530

    Time: 5 ms (99.30%) | Memory: 49.2 MB (85.81%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -2,18 +2,21 @@ class Solution {
 
     private int count( int[] nums, int k ){
 
-        HashMap<Integer , Integer> map = new HashMap<>();
+        int n = nums.length;
+        int[] freq = new int[n + 1];
+        int unique = 0;
 
         int l = 0;
         int total = 0;
 
         for( int r = 0 ; r < nums.length ; r++ ){
 
-            map.put( nums[r] , map.getOrDefault( nums[r] , 0 ) + 1 );
+            freq[nums[r]]++;
+            if( freq[nums[r]] == 1 ) unique++;  
 
-            while( map.size() > k ){
-                map.put( nums[l], map.get(nums[l]) - 1 );
-                if( map.get(nums[l]) == 0 ) map.remove( nums[l] );
+            while( unique > k ){
+                freq[nums[l]]--;
+                if( freq[nums[l]] == 0 ) unique-- ;
                 l++;
             }
 
