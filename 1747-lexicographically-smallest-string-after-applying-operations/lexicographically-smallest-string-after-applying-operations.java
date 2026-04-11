773386a9a429543a760f5ee88131a11633732284
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-11 18:17:54 +0530

    Time: 148 ms (5.13%) | Memory: 55.5 MB (5.13%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.java
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.java
@@ -1,48 +1,69 @@
 class Solution {
-    public String findLexSmallestString(String s, int a, int b) {
-        Queue<String> q = new LinkedList<>();
-        HashSet<String> visited = new HashSet<>();
 
-        String smallest = s;
+    static String smallest ;
 
-        q.offer(s);
-        visited.add(s);
+    private String optionA( String s , int a ){
 
-        while (!q.isEmpty()) {
-            String curr = q.poll();
+        StringBuilder sb = new StringBuilder();
 
-            if (curr.compareTo(smallest) < 0) {
-                smallest = curr;
-            }
-
-            String opA = addOperation(curr, a);
-            if (!visited.contains(opA)) {
-                visited.add(opA);
-                q.offer(opA);
-            }
-
-            String opB = rotateOperation(curr, b);
-            if (!visited.contains(opB)) {
-                visited.add(opB);
-                q.offer(opB);
+        for( int i = 0 ; i < s.length() ; i++ ){
+            if( i % 2 == 0 ) sb.append( s.charAt(i) );
+            else{
+                char ch = s.charAt(i);
+                sb.append( (char)(((ch - '0' + a) % 10) + '0') );
             }
         }
 
-        return smallest;
+        return sb.toString();
+        
     }
 
-    private String addOperation(String s, int a) {
-        StringBuilder sb = new StringBuilder(s);
-        for (int i = 1; i < s.length(); i += 2) {
-            int digit = (sb.charAt(i) - '0' + a) % 10;
-            sb.setCharAt(i, (char)(digit + '0'));
+    private String optionB( String s , int b ){
+
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+
+        for( int i = b ; i < n ; i++ ){
+            sb.append( s.charAt(i) );
         }
+        for( int i = 0 ; i < b && i < n ; i++ ){
+            sb.append( s.charAt(i) );
+        }
+
         return sb.toString();
     }
 
-    private String rotateOperation(String s, int b) {
-        int n = s.length();
-        b %= n;
-        return s.substring(n - b) + s.substring(0, n - b);
+    private boolean compare(String a, String b) {
+        int n = Math.min(a.length(), b.length());
+
+        for (int i = 0; i < n; i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                return a.charAt(i) < b.charAt(i);
+            }
+        }
+
+        return a.length() < b.length();
+    }
+
+    private void solve( String s , int a , int b , HashSet<String> set  ){
+
+        if( compare( s , smallest ) ) smallest = s;
+        set.add(s);
+
+        String stringA = optionA( s , a );
+        String stringB = optionB( s , b );
+
+        if( !set.contains( stringA ) ) solve( stringA , a , b , set );
+        if( !set.contains( stringB ) ) solve( stringB , a , b , set );
+
+    }
+
+    public String findLexSmallestString(String s, int a, int b) {
+        
+        this.smallest = s;
+        solve( s , a , b , new HashSet<>() );
+
+        return smallest;
+
     }
 }
