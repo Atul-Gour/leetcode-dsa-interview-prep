ca9a99fa8b5ffcf1560661d7d0c0c22787da84f1
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-12 22:43:53 +0530

    Time: 1 ms (64.00%) | Memory: 47.3 MB (24.98%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,29 +1,35 @@
+import java.util.*;
+
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        int color[] = new int[n];
-        Arrays.fill( color , -1 );
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
 
-        ArrayDeque<Integer> q = new ArrayDeque<>();
-
-        for( int i = 0 ; i < n ; i++ ){
-            if( color[i] != -1 ) continue;
-
-            q.offer( i );
-            color[i] = 1;
-
-            while( !q.isEmpty() ){
-            int curr = q.poll();
-
-            for( int neigh : graph[curr] ){
-                if( color[neigh] == -1 ){
-                    color[neigh]  = 1 - color[curr];
-                    q.offer(neigh);
-                }
-                else if( color[neigh] == color[curr] ) return false;
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                if (!bfs(graph, i, color)) return false;
             }
         }
-        
+        return true;
+    }
+
+    private boolean bfs(int[][] graph, int start, int[] color) {
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(start);
+        color[start] = 0;
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+
+            for (int neigh : graph[curr]) {
+                if (color[neigh] == -1) {
+                    color[neigh] = 1 - color[curr];
+                    q.offer(neigh);
+                } else if (color[neigh] == color[curr]) {
+                    return false;
+                }
+            }
         }
         return true;
     }
