2b2489032f3e49b2e24a5ec38aa178d5ecc5b6c6
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-16 14:26:58 +0530

    Time: 14 ms (66.77%) | Memory: 47.8 MB (48.24%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,38 +1,40 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-
         int n = grid.length;
-        if (grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
 
         int[][] dist = new int[n][n];
-        for (int[] row : dist) Arrays.fill(row, -1);
+        ArrayDeque<int[]> q = new ArrayDeque<>();
 
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{0, 0});
+        for( int[] d : dist ) Arrays.fill(d , Integer.MAX_VALUE);
+
+        if( grid[0][0] != 0 ) return -1;
+
         dist[0][0] = 1;
+        q.offer( new int[]{0 , 0} );
 
-        int[] dx = {-1,-1,-1,0,0,1,1,1};
-        int[] dy = {-1,0,1,-1,1,-1,0,1};
+        while( !q.isEmpty() ){
 
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int x = cur[0], y = cur[1];
+            int curr[] = q.poll();
+            int i = curr[0];
+            int j = curr[1];
 
-            if (x == n-1 && y == n-1) return dist[x][y];
+            for( int x = -1 ; x <= 1 ; x++ ){
+                for( int y = -1 ; y <= 1 ; y++ ){
 
-            for (int d = 0; d < 8; d++) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
+                    int newI = i + x;
+                    int newJ = j + y;
+                    
+                    if( newI < 0 || newI >= n || newJ < 0 || newJ >= n || grid[newI][newJ] != 0 ) continue;
 
-                if (nx >= 0 && ny >= 0 && nx < n && ny < n
-                        && grid[nx][ny] == 0
-                        && dist[nx][ny] == -1) {
+                    if( dist[newI][newJ] > dist[i][j] + 1 ){
+                        dist[newI][newJ] = dist[i][j] + 1;
+                        q.offer( new int[]{ newI , newJ } );
+                    }
 
-                    dist[nx][ny] = dist[x][y] + 1;
-                    q.offer(new int[]{nx, ny});
                 }
             }
         }
-        return -1;
+
+        return dist[n - 1][n - 1] == Integer.MAX_VALUE ? -1 : dist[n - 1][n - 1];
     }
 }
