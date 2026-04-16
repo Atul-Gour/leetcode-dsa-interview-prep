7d0155cc66d81aee00647085aba709839ba91107
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-16 18:18:42 +0530

    Time: 5 ms (98.83%) | Memory: 46.7 MB (26.72%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,40 +1,23 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        ArrayList< ArrayList<int[]> > graph = new ArrayList<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>( (a,b) -> Integer.compare(a[1] , b[1]));
-        int[][] dist = new int[n][k + 2];
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
 
+        for(int i = 0; i <= k; i++){
+            int[] temp = dist.clone();  // IMPORTANT
 
-        for( int i = 0 ; i < n ; i++ ) graph.add( new ArrayList<>() );
-        for( int flight[] : flights ) graph.get(flight[0]).add( new int[]{ flight[1] , flight[2] } );
+            for(int[] flight : flights){
+                int u = flight[0], v = flight[1], w = flight[2];
 
-        for( int[] d : dist ) Arrays.fill( d , Integer.MAX_VALUE );
-
-        dist[src][0] = 0;
-        pq.offer( new int[]{ src , 0 , 0 } );
-
-        while( !pq.isEmpty() ){
-            int curr[] = pq.poll();
-            int node = curr[0];
-            int cost = curr[1];
-            int stops = curr[2];
-
-            // System.out.println( node + " " + cost + " " + stops );
-
-            if( node == dst ) return cost;
-            if( stops > k || dist[node][stops] < cost ) continue;
-
-            for( int[] neigh : graph.get(node) ){
-                int v = neigh[0];
-                int newFare = cost + neigh[1];
-
-                if( dist[v][stops + 1] > newFare ){
-                    dist[v][stops + 1] = newFare;
-                    pq.offer( new int[]{ v , newFare , stops + 1 } );
+                if(dist[u] != Integer.MAX_VALUE && dist[u] + w < temp[v]){
+                    temp[v] = dist[u] + w;
                 }
             }
+
+            dist = temp;
         }
 
-        return -1;
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
     }
 }
