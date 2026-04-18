4a195fd3df195db7996bf6390ad63cb9246051ad
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-18 15:25:15 +0530

    Time: 27 ms (24.52%) | Memory: 47.3 MB (7.65%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,51 +1,64 @@
 class Solution {
-    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int dist[][] = new int[n][n];
-        for (int d[] : dist) {
-            Arrays.fill(d, 100000);
+
+    private int find( int src , int distanceThreshold ,  ArrayList<ArrayList<int[]>> graph , int[] time , boolean[] visited ){
+        Arrays.fill( time , -1 );
+        int ans = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>( (a,b) -> Integer.compare( b[1] , a[1] ) );
+        pq.offer( new int[]{src , distanceThreshold} );
+
+        while( !pq.isEmpty() ){
+            int[] curr = pq.poll();
+            int node = curr[0];
+            int currThreshold = curr[1];
+
+            if( visited[node] )continue;
+
+            visited[node] = true;
+            if( node != src ) ans++;
+
+            for( int[] neigh : graph.get(node) ){
+                int neighNode = neigh[0];
+                int weight = neigh[1];
+
+                if( !visited[neighNode] &&  weight <= currThreshold && currThreshold - weight > time[neighNode] ){
+                    time[neighNode] = currThreshold - weight;
+                    pq.offer( new int[]{neighNode , currThreshold - weight} );
+                }
+            }
         }
 
-        for (int[] edge : edges) {
+        return ans;
+    }
+
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+
+        ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
+
+        for( int i = 0 ; i < n ; i++ ) graph.add( new ArrayList<>() );
+
+        for( int[] edge : edges ){
             int u = edge[0];
             int v = edge[1];
             int w = edge[2];
 
-            dist[u][v] = w;
-            dist[v][u] = w;
-        }
-        for (int i = 0; i < n; i++) {
-            dist[i][i] = 0;
+            graph.get(u).add( new int[]{v , w} );
+            graph.get(v).add( new int[]{u , w} );
         }
 
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (dist[i][k] != 100000 && dist[k][j] != 100000) {
-                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
-                    }
-                }
-            }
-        }
+        int ans = 0;
+        int minNodesByAns = Integer.MAX_VALUE;
 
-        int ans = n-1;
-        int minReach = Integer.MAX_VALUE;
+        for( int i = 0 ; i < n ; i++ ){
+            int nodes = find( i , distanceThreshold , graph , new int[n] , new boolean[n] );
 
-        for (int i = 0; i < n; i++) {
-            int reach = 0;
-            for (int j = 0; j < n; j++) {
-                if ( i != j && dist[i][j] <= distanceThreshold ) {
-                    reach++;
-                }
-            }
-
-            if( reach < minReach ){
-                minReach = reach;
+            if( nodes < minNodesByAns ){
                 ans = i;
-            }else if( reach == minReach ){
-                ans = Math.max( ans , i );
+                minNodesByAns = nodes;
             }
-
+            else if( nodes == minNodesByAns ) ans = Math.max( ans , i );
         }
+
         return ans;
     }
 }
