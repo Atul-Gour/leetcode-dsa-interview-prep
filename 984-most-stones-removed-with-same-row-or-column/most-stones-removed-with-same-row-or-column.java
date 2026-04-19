f5bd16cbb605f3282fe036e6785a45220609a569
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-19 16:20:34 +0530

    Time: 15 ms (68.53%) | Memory: 47.4 MB (6.25%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,83 +1,37 @@
 class Solution {
+    static class DSU {
+        Map<Integer, Integer> parent = new HashMap<>();
 
-    static class DSU{
-        int[] parent;
-        int[] rank;
-
-        DSU(int n){
-            this.parent = new int[n];
-            this.rank = new int[n];
-            for( int i = 0 ; i < n ; i++ ) parent[i] = i;
-        }
-        
-        int find( int node ){
-            if( parent[node] == node ) return node;
-            return parent[node] = find( parent[node] );
+        int find(int x) {
+            if (!parent.containsKey(x)) parent.put(x, x);
+            if (parent.get(x) != x)
+                parent.put(x, find(parent.get(x)));
+            return parent.get(x);
         }
 
-        boolean union( int a , int b ){
-            int pa = find( a );
-            int pb = find( b );
-
-            if( pa == pb ) return false;
-
-            int ra = rank[pa];
-            int rb = rank[pb];
-
-            if( ra < rb ){
-                parent[pa] = pb;
-            }
-            else if( rb < ra ){
-                parent[pb] = pa;
-            }
-            else{
-                parent[pa] = pb;
-                rank[pb]++;
-            }
-
-            return true;
+        void union(int a, int b) {
+            int pa = find(a);
+            int pb = find(b);
+            if (pa != pb) parent.put(pa, pb);
         }
     }
 
     public int removeStones(int[][] stones) {
-        Map<Integer , List<Integer> > xCoordinateMap = new HashMap<>();
-        Map<Integer , List<Integer> > yCoordinateMap = new HashMap<>();
-        Map<Integer , Integer> freq = new HashMap<>();
+        DSU dsu = new DSU();
+        int OFFSET = 10001;
 
-        int n = stones.length;
-        DSU dsu = new DSU(n);
-
-        for( int i = 0 ; i < n ; i++ ){
-            int x = stones[i][0];
-            int y = stones[i][1];
-
-            xCoordinateMap.computeIfAbsent( x , f -> new ArrayList<>() ).add(i);
-            yCoordinateMap.computeIfAbsent( y , f -> new ArrayList<>() ).add(i);
+        for (int[] stone : stones) {
+            int x = stone[0];
+            int y = stone[1] + OFFSET;
+            dsu.union(x, y);
         }
 
-        for( List<Integer> list : xCoordinateMap.values() ){
-            for( int i = 1 ; i < list.size() ; i++ ){
-                dsu.union( list.get( i - 1 ) , list.get(i) );
-            }
-        }
-        for( List<Integer> list : yCoordinateMap.values() ){
-            for( int i = 1 ; i < list.size() ; i++ ){
-                dsu.union( list.get( i - 1 ) , list.get(i) );
-            }
+        Set<Integer> uniqueParents = new HashSet<>();
+
+        for (int[] stone : stones) {
+            uniqueParents.add(dsu.find(stone[0]));
         }
 
-        System.out.println(dsu.parent);
-
-        for( int i = 0 ; i < stones.length ; i++ ){
-            int parent = dsu.find(i);
-            freq.put( parent , freq.getOrDefault(parent , 0) + 1 );
-        }
-
-        int ans = 0;
-        for( int count : freq.values() ){
-            ans += count - 1;
-        }
-
-        return ans;
+        return stones.length - uniqueParents.size();
     }
 }
