655c79a6591499fc427a39588e24e2882026dedf
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-19 15:18:06 +0530

    Time: 7 ms (53.27%) | Memory: 66.3 MB (54.76%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -1,32 +1,35 @@
 class Solution {
 
     static class DSU{
-
         int[] parent;
         int[] rank;
 
         DSU(int n){
-            parent = new int[n];
-            rank = new int[n];
+            this.parent = new int[n];
+            this.rank = new int[n];
             for( int i = 0 ; i < n ; i++ ) parent[i] = i;
         }
-
+        
         int find( int node ){
-            if( parent[node] == node )return node;
+            if( parent[node] == node ) return node;
             return parent[node] = find( parent[node] );
         }
 
         boolean union( int a , int b ){
-            int pa = find(a);
-            int pb = find(b);
+            int pa = find( a );
+            int pb = find( b );
 
-            if( pa == pb )return false;
+            if( pa == pb ) return false;
 
             int ra = rank[pa];
             int rb = rank[pb];
 
-            if( ra < rb )parent[pa] = pb;
-            else if( rb < ra )parent[pb] = pa;
+            if( ra < rb ){
+                parent[pa] = pb;
+            }
+            else if( rb < ra ){
+                parent[pb] = pa;
+            }
             else{
                 parent[pa] = pb;
                 rank[pb]++;
@@ -34,32 +37,24 @@ class Solution {
 
             return true;
         }
-
-        int provinces(){
-            int count = 0;
-            for( int i = 0 ; i < parent.length ; i++ ){
-                int pCurr = find(parent[i]);
-                if( pCurr == i )count++;
-            }
-
-            return count;
-        }
-
     }
 
     public int makeConnected(int n, int[][] connections) {
+        int edgesSize = connections.length;
         DSU dsu = new DSU(n);
 
-        int redundantCablesCount = 0;
-        for( int edge[] : connections ){
-            if( !dsu.union( edge[0] , edge[1] ) ){
-                redundantCablesCount++;
-            }
+        if( edgesSize < n - 1 ) return -1;
+        int edgesWaste = 0;
+
+        for( int[] edge : connections ){
+            if( !dsu.union( edge[0] , edge[1] ) ) edgesWaste++;
         }
 
-        int provinces = dsu.provinces();
+        int edgesNeed = 0;
+        for( int i = 0 ; i < n ; i++ ){
+            if( dsu.union( 0 , i ) ) edgesNeed++;
+        }
 
-        if( redundantCablesCount < provinces - 1 )return -1;
-        else return provinces - 1;
+        return edgesNeed;        
     }
 }
