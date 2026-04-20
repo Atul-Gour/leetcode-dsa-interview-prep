ca9c2aaaaa4c6317bbcac5931b8cc7cadc755eb7
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-20 12:36:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (80.02%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,45 +1,26 @@
 class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0; 
+        int mid = 0;
+        int high = nums.length - 1;
 
-    private void swap( int i , int j , int[] nums ){
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
-    }
-
-    public void sortColors(int[] nums) {
-        int n = nums.length;
-        int i = 0;
-        int j = n-1;
-
-        while( j >= 0 && nums[j] == 2 ) j--;
-        while( i < n && nums[i] == 0 ) i++;
-
-        for( int k = i ; k < n && k <= j ; ){
-
-            // System.out.println( i + " " + k + " " + j + " arr down " );
-            // for( int ele : nums ) System.out.print( ele  + " " );
-            // System.out.println();
-
-            if( nums[k] == 2 ){
-                swap( k , j , nums );
-                j--;
-                if( nums[k] == 0 ){
-                    swap( i , k , nums );
-                    i++;
-                }
-            }
-            else if( nums[k] == 0 ){
-                swap( k , i , nums );
-                i++;
-                if( nums[k] == 2 ){
-                    swap( j , k , nums );
-                    j--;
-                }
-            }
-
-            if( nums[k] == 1 ) k++;
-            else if( k <= i ) k++;
-
-        }
     }
 }
