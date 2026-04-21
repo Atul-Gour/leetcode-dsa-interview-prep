b0d0ef3a0849a6beabf6be14a6e94f82181f528b
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-21 19:08:18 +0530

    Time: 752 ms (5.06%) | Memory: 263.4 MB (12.75%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.java
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.java
@@ -1,56 +1,59 @@
 class Solution {
 
-    List<List<Integer>> ans = new ArrayList<>();
-    List<Integer>[] adj;
-    int[] tin, low;
-    boolean[] visited;
-    int timer = 0;
 
-    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+    static int[] low;
+    static int[] time;
+    static int t = 0;
 
-        adj = new ArrayList[n];
-        for (int i = 0; i < n; i++)
-            adj[i] = new ArrayList<>();
+    void dfs( int node , int parent , List<List<Integer>> ans , List<List<Integer>> graph ){
 
-        for (List<Integer> e : connections) {
-            int u = e.get(0);
-            int v = e.get(1);
-            adj[u].add(v);
-            adj[v].add(u);
+        time[node] = t;
+        low[node] = t;
+        t++;
+
+        for( int neigh : graph.get(node) ){
+            if( time[neigh] == -1 ){
+                dfs( neigh , node , ans , graph );
+                low[node] = Math.min( low[node] , low[neigh] );
+            }
+            else if( neigh != parent ){
+                low[node] = Math.min( low[node] , time[neigh] );
+            }
+
+            if( low[neigh] > time[node] )  ans.add( new ArrayList<>(List.of(node , neigh)) );
+            
         }
-
-        tin = new int[n];
-        low = new int[n];
-        visited = new boolean[n];
-
-        for(int i=0;i<n;i++){
-            if(!visited[i]) dfs(i, -1);
-        }
-
-        return ans;
     }
 
-    private void dfs(int u, int parent) {
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        this.low = new int[n];
+        this.time = new int[n];
+        t = 0;
 
-        visited[u] = true;
-        tin[u] = low[u] = timer++;
+        List<List<Integer>> graph = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
 
-        for (int v : adj[u]) {
+        for( int i = 0 ; i < n ; i++ ) graph.add( new ArrayList<>() );
+        Arrays.fill( time , -1 );
 
-            if (v == parent) continue;
+        for( List<Integer> connection : connections ){
+            int u = connection.get(0);
+            int v = connection.get(1);
 
-            if (!visited[v]) {
-                dfs(v, u);
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
 
-                low[u] = Math.min(low[u], low[v]);
-
-                if (low[v] > tin[u]) {
-                    ans.add(Arrays.asList(u, v));
-                }
-
-            } else {
-                low[u] = Math.min(low[u], tin[v]);
+        for( int i = 0 ; i < n ; i++ ){
+            if( time[i] == -1 ){
+                dfs( i , -1 , ans , graph );
             }
         }
+
+        for( int ele : time )System.out.print(ele + " ");
+        System.out.println();
+        for( int ele : low )System.out.print(ele + " ");
+
+        return ans;
     }
 }
