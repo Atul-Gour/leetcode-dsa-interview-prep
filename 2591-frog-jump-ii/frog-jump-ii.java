a8b0d4270f1b2d8eb1480611f886f81fb0805ed7
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-21 18:20:08 +0530

    Time: 10 ms (6.80%) | Memory: 83.1 MB (85.53%) - LeetSync

--- 2591-frog-jump-ii/frog-jump-ii.java
+++ 2591-frog-jump-ii/frog-jump-ii.java
@@ -1,12 +1,35 @@
 class Solution {
-    public int maxJump(int[] stones) {
-
+    boolean possible(int[] stones, int jump) {
         int n = stones.length;
-        if(n == 2)return stones[1];
-        int ans = 0;
 
-        for(int i = 2; i < n; i++){
-            ans = Math.max(ans, stones[i] - stones[i-2]);
+        for (int i = 2; i < n; i++) {
+            if (stones[i] - stones[i - 2] > jump) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int maxJump(int[] stones) {
+        int n = stones.length;
+        if( n == 2 ) return stones[1];
+        
+        int ans = stones[n-1];
+        int low = 0;
+        int high = stones[n-1];
+
+        while( low <= high ){
+            
+            int mid = low + (( high - low ) / 2);
+            
+            
+            if( possible(stones , mid ) ){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
         }
 
         return ans;
