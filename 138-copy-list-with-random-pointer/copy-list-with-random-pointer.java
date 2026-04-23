a7a9e0e104a91a5bcb293349d2d04bc0eba8fad2
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-23 18:51:39 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (16.14%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -1,31 +1,39 @@
 class Solution {
     public Node copyRandomList(Node head) {
-        HashMap<Node , Node> map = new HashMap<>();
+
+        if (head == null) return null;
 
         Node temp = head;
-        Node dummy = new Node(-1);
 
-        while( temp != null ){
-            Node newNode = new Node( temp.val );
-            map.put( temp , newNode );
-            dummy.next = newNode;
-            dummy = dummy.next;
-            temp = temp.next;
+        while (temp != null) {
+            Node copy = new Node(temp.val);
+            copy.next = temp.next;
+            temp.next = copy;
+            temp = copy.next;
         }
 
         temp = head;
-
-        while( temp != null ){
-            
-            if( temp.random != null ){
-                Node correspondingNode = map.get( temp );
-                correspondingNode.random = map.get( temp.random );
+        while (temp != null) {
+            if (temp.random != null) {
+                temp.next.random = temp.random.next;
             }
-
-            temp = temp.next;
-            
+            temp = temp.next.next;
         }
 
-        return map.get(head);
+        temp = head;
+        Node dummy = new Node(-1);
+        Node copyTail = dummy;
+
+        while (temp != null) {
+            Node copy = temp.next;
+
+            copyTail.next = copy;
+            copyTail = copy;
+
+            temp.next = copy.next;
+            temp = temp.next;
+        }
+
+        return dummy.next;
     }
 }
