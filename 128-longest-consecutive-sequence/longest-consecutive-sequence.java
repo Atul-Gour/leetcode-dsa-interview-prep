460cd3ecb3dc278d33cc4e1552139d3b1df6423e
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-28 19:00:28 +0530

    Time: 31 ms (64.53%) | Memory: 95.5 MB (57.54%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,63 +1,28 @@
 class Solution {
 
-    static class DSU{
-        HashMap<Integer , Integer> parent;
-        HashMap<Integer , Integer> size;
-        int maxSize ;
-
-        DSU(){
-            parent = new HashMap<>();
-            size = new HashMap<>();
-            maxSize = 1;
-        }
-
-        int find( int node ){
-            if( parent.getOrDefault( node , node ) == node ) return node;
-            parent.put( node , find( parent.get(node) ) );
-            return parent.get(node);
-        }
-
-        boolean union( int a , int b ){
-            int pa = find(a);
-            int pb = find(b);
-
-            if( pa == pb ) return false;
-
-            int sa = size.getOrDefault( pa , 1 );
-            int sb = size.getOrDefault( pb , 1 );
-
-            if( sa <= sb ){
-                parent.put( pa , pb );
-                size.put( pb , sb + sa );
-            }
-            else{
-                parent.put( pb , pa );
-                size.put( pa , sb + sa );
-            }
-
-            maxSize = Math.max( maxSize , sb + sa );
-            return true;
-        }
-    }
-
     public int longestConsecutive(int[] nums) {
-        int n = nums.length;
-        if( n == 0 ) return 0;
-
         HashSet<Integer> set = new HashSet<>();
-        DSU dsu = new DSU();
 
-        for( int ele : nums ){
-            if( set.contains(ele) ) continue;
-            
-            if( set.contains(ele - 1) ) dsu.union(ele , ele - 1);
-            if( set.contains(ele + 1) ) dsu.union(ele , ele + 1);
-
-            set.add( ele );
+        for (int num : nums) {
+            set.add(num);
         }
 
-        
+        int longest = 0;
 
-        return dsu.maxSize;
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int current = num;
+                int streak = 1;
+
+                while (set.contains(current + 1)) {
+                    current++;
+                    streak++;
+                }
+
+                longest = Math.max(longest, streak);
+            }
+        }
+
+        return longest;
     }
 }
