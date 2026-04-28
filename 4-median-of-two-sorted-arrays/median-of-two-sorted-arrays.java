ed0761d55083f7f7726798af100ab22a4d7742fb
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-04-28 18:06:18 +0530

    Time: 1 ms (100.00%) | Memory: 49 MB (16.30%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,29 +1,40 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-        int m = nums1.length, n = nums2.length;
-        int total = m + n;
-        int mid = total / 2;
+        if( nums1.length > nums2.length )  return findMedianSortedArrays( nums2 , nums1 );
 
-        int i = 0, j = 0;
-        int count = 0;
+        int n = nums1.length, m = nums2.length;
+        int total = m + n ;
+        
 
-        int prev = 0, curr = 0;
+        int low = 0;
+        int high = n;
+        int left = (n + m + 1)/2;
+        int l1 = 0 , l2 = 0 , r1 = 0 , r2 = 0;
 
-        while (count <= mid) {
-            prev = curr;
+        while( low <= high ){
+            int mid1 = low + (high - low)/2;
+            l1 = mid1 - 1 < 0 ? Integer.MIN_VALUE : nums1[mid1 - 1];
+            r1 = mid1 >= n ? Integer.MAX_VALUE : nums1[mid1];
 
-            if (i < m && (j >= n || nums1[i] <= nums2[j])) {
-                curr = nums1[i++];
-            } else {
-                curr = nums2[j++];
-            }
-            count++;
+            int mid2 = left - mid1;
+            l2 = mid2 - 1 < 0 ? Integer.MIN_VALUE : nums2[mid2 - 1];
+            r2 = mid2 >= m ? Integer.MAX_VALUE : nums2[mid2];
+
+
+            if( l1 <= r2 && l2 <= r1 ) break;
+            else if( l1 > r2 ) high = mid1 - 1;
+            else low = mid1 + 1;
         }
+        
+        // System.out.println( l1 + " " + l2 + " " + r1 + " " + r2 );
+        double ans ;
 
-        if (total % 2 == 0) {
-            return (prev + curr) / 2.0;
-        }
-        return curr;
+        if( total % 2 == 0 )
+            ans = (Math.max(l1 , l2) + Math.min( r1 , r2 )) / 2.0;
+        else
+            ans = Math.max( l1 , l2 );
+
+        return ans;
     }
 }
