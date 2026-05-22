b6af57b60e98d74184ebd759d9a64866c69f3430
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-05-22 20:34:29 +0530

    Time: 3 ms (88.47%) | Memory: 48.3 MB (24.91%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -3,49 +3,13 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        int l = 0;
-        int r = Math.min(n, m) - 1;
+        int row = 0;
+        int col = m - 1;
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-
-            if (matrix[mid][mid] < target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-
-        int boundary = l;
-
-        for (int i = 0; i < boundary; i++) {
-            if (binarySearch(matrix[i], boundary, m - 1, target)) {
-                return true;
-            }
-        }
-
-        for (int i = boundary; i < n; i++) {
-            if (binarySearch(matrix[i], 0, m - 1, target)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private boolean binarySearch(int[] arr, int l, int r, int target) {
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-
-            if (arr[mid] == target) {
-                return true;
-            }
-
-            if (arr[mid] < target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
+        while (row < n && col >= 0) {
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] > target) col--;
+            else row++;
         }
 
         return false;
