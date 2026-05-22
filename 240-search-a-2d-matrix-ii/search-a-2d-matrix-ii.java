cabe8cdbd9b09a43b0bbffc2931ec1519eda824e
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-05-22 19:55:26 +0530

    Time: 4 ms (22.91%) | Memory: 48.1 MB (81.68%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,30 +1,30 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return false;
-        }
-
         int n = matrix.length;
         int m = matrix[0].length;
 
-        int row = 0;
-        int col = 0;
+        int l = 0;
+        int r = Math.min(n, m) - 1;
 
-        // Move diagonally while current element is smaller than target
-        while (row < n && col < m && matrix[row][col] < target) {
-            row++;
-            col++;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (matrix[mid][mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         }
 
-        // Search upper rows from current col to last col
-        for (int i = 0; i < Math.min(row, n); i++) {
-            if (binarySearch(matrix[i], col, m - 1, target)) {
+        int boundary = l;
+
+        for (int i = 0; i < boundary; i++) {
+            if (binarySearch(matrix[i], boundary, m - 1, target)) {
                 return true;
             }
         }
 
-        // Search remaining rows from 0 to last col
-        for (int i = row; i < n; i++) {
+        for (int i = boundary; i < n; i++) {
             if (binarySearch(matrix[i], 0, m - 1, target)) {
                 return true;
             }
@@ -33,15 +33,19 @@ class Solution {
         return false;
     }
 
-    private boolean binarySearch(int[] arr, int left, int right, int target) {
-        if (left > right) return false;
+    private boolean binarySearch(int[] arr, int l, int r, int target) {
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return true;
+            }
 
-            if (arr[mid] == target) return true;
-            else if (arr[mid] < target) left = mid + 1;
-            else right = mid - 1;
+            if (arr[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         }
 
         return false;
