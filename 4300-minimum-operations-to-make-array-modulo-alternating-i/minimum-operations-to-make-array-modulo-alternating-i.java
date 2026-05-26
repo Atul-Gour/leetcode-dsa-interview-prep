8b8c5bf3a2b5ac81be6b868a5429c4b120366f1c
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-05-26 14:19:38 +0530

    Time: 164 ms (30.26%) | Memory: 45 MB (67.52%) - LeetSync

--- 4300-minimum-operations-to-make-array-modulo-alternating-i/minimum-operations-to-make-array-modulo-alternating-i.java
+++ 4300-minimum-operations-to-make-array-modulo-alternating-i/minimum-operations-to-make-array-modulo-alternating-i.java
@@ -1,30 +1,24 @@
 class Solution {
-
-    private int find(int x, int y, int[] nums, int k) {
-        int ops = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            int a = nums[i];
-            int rem = a % k;
-            int target = (i % 2 == 0) ? x : y;
-
-            int downCost = (rem - target + k) % k;
-            int upCost = (target - rem + k) % k;
-
-            ops += Math.min(downCost, upCost);
-        }
-
-        return ops;
-    } 
-
     public int minOperations(int[] nums, int k) {
-        int ans = Integer.MAX_VALUE;
 
-        for( int i = 0 ; i < k ; i++ ){
-            for( int j = 0 ; j < k ; j++ ){
-                if( i == j ) continue;
-                int currStep = find( i , j , nums , k );
-                ans = Math.min( ans , currStep );
+        int n = nums.length;
+        int ans = Integer.MAX_VALUE;
+        for (int x = 0; x < k; x++) {
+            for (int y = 0; y < k; y++) {
+
+                if (x == y) continue;
+
+                int opr = 0;
+
+                for (int i = 0; i < n; i++) {
+
+                    int rem = nums[i] % k;
+                    int target = (i % 2 == 0) ? x : y;
+
+                    int diff = Math.abs(rem - target);
+                    opr += Math.min(diff, k - diff);
+                }
+                ans = Math.min(ans, opr);
             }
         }
 
