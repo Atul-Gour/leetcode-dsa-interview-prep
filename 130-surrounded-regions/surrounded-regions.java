8fbc71265ecb0db59289c3fa5110d77a30c025be
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-05-28 18:39:15 +0530

    Time: 4 ms (18.20%) | Memory: 47.9 MB (37.51%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,37 +1,41 @@
 class Solution {
-
-    private void dfs(int i, int j, char[][] board ) {
-        int n = board.length;
-        int m = board[0].length;
-
-        board[i][j] = '#';
-
-        int[][] dirs = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-
-        for (int[] dir : dirs) {
-
-            int newI = i + dir[0];
-            int newJ = j + dir[1];
-
-            if (newI < 0 || newI > n - 1 || newJ < 0 || newJ > m - 1 || board[newI][newJ] == 'X' || board[newI][newJ] == '#' ) continue;
-
-            dfs(newI, newJ, board ) ;
-        }
-
-    }
-
     public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
 
-        for( int i = 0 ; i < n ; i++ ) if( board[i][0] == 'O' ) dfs( i , 0 , board );
-        for( int i = 0 ; i < n ; i++ ) if( board[i][m-1] == 'O' ) dfs( i , m-1 , board );
-        for( int j = 0 ; j < m ; j++ ) if( board[0][j] == 'O' ) dfs( 0 , j , board );
-        for( int j = 0 ; j < m ; j++ ) if( board[n-1][j] == 'O' ) dfs( n-1 , j , board );
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        int[][] dirs = {{0,1} ,{1,0} , {0,-1}, {-1,0}};
+
+        for( int i = 0 ; i < n ; i++ ){
+            if( board[i][0] == 'O' ) q.offer( new int[]{ i , 0 } );
+            if( board[i][m - 1] == 'O' ) q.offer( new int[]{ i , m - 1 } );
+        }
+
+        for( int j = 0 ; j < m ; j++ ){
+            if( board[0][j] == 'O' ) q.offer( new int[]{ 0 , j } );
+            if( board[n - 1][j] == 'O' ) q.offer( new int[]{ n - 1 , j } );
+        }
+
+        while( !q.isEmpty() ){
+            int[] curr = q.poll();
+            int x = curr[0];
+            int y = curr[1];
+            if( board[x][y] == 'Y' || board[x][y] == 'X' ) continue;
+
+            board[x][y] = 'Y';
+
+            for( int[] d : dirs ){
+                int newX = x + d[0];
+                int newY = y + d[1];
+
+                if( newX < 0 || newX >= n || newY < 0 || newY >= m || board[newX][newY] == 'X' || board[newX][newY] == 'Y' ) continue;
+                q.offer( new int[]{ newX , newY } );
+            }
+        }
 
         for( int i = 0 ; i < n ; i++ ){
             for( int j = 0 ; j < m ; j++ ){
-                if( board[i][j] == '#' ) board[i][j] = 'O';
+                if( board[i][j] == 'Y' ) board[i][j] = 'O';
                 else if( board[i][j] == 'O' ) board[i][j] = 'X';
             }
         }
