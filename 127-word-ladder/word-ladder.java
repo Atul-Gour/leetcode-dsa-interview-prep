689e3c86b98ab481038cb55926d5c4f86b4bd8d6
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-05-29 16:03:55 +0530

    Time: 85 ms (41.52%) | Memory: 47.8 MB (71.93%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,49 +1,46 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-
         HashSet<String> set = new HashSet<>();
-        ArrayDeque<String> q = new ArrayDeque<>();
-
         for( String word : wordList ) set.add(word);
-        set.remove( beginWord );
-
         if( !set.contains(endWord) ) return 0;
 
+        ArrayDeque<String> q = new ArrayDeque<>();
         q.offer( beginWord );
-        int step = 1;
+
+        int steps = 0;
 
         while( !q.isEmpty() ){
             int size = q.size();
+            steps++;
 
             while( size-- > 0 ){
-                char curr[] = q.poll().toCharArray();
+                char[] curr = q.poll().toCharArray();
 
                 for( int i = 0 ; i < curr.length ; i++ ){
-                    char originalChar = curr[i];
+
+                    char original = curr[i];
 
                     for( char ch = 'a' ; ch <= 'z' ; ch++ ){
 
-                        if( ch == originalChar ) continue;
+                        if( ch == original )continue;
 
                         curr[i] = ch;
-                        String newString  = new String(curr);
+                        String newString = new String(curr);
 
-                        if( newString.equals( endWord ) ) return step + 1;
+                        if( newString.equals(endWord) ) return steps + 1;
 
                         if( set.contains( newString ) ){
-                            q.offer( newString );
-                            set.remove( newString );
+                            q.offer(newString);
+                            set.remove(newString);
                         }
                     }
 
-                    curr[i] = originalChar;
+                    curr[i] = original;
                 }
+                
             }
-
-            step++;
         }
 
         return 0;
-
     }
 }
