f8e2363182a2290a0b625704369a1f7d18a8a13d
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-05-29 15:07:50 +0530

    Time: 22 ms (54.93%) | Memory: 47.9 MB (6.05%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,19 +1,46 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
         int n = nums.length;
-        long totalSum = 0; 
+        long ans = 0;
+        Stack<Integer> st = new Stack<>();
 
-        for (int i = 0; i < n; i++) {
-            int minVal = nums[i];
-            int maxVal = nums[i];
-            for (int j = i; j < n; j++) {
-                minVal = Math.min(minVal, nums[j]);
-                maxVal = Math.max(maxVal, nums[j]);
-                
-              
-                totalSum += (maxVal - minVal);
-            }
+        int[] leftGreater = new int[n];
+        for( int i = 0 ; i < n ; i++ ){
+            while( !st.isEmpty() && nums[st.peek()] <= nums[i] ) st.pop();
+            leftGreater[i] = st.empty() ? -1 : st.peek();
+            st.push(i);
         }
-        return totalSum;
+
+        st.clear();
+        int[] rightGreater = new int[n];
+        for( int i = n-1 ; i >= 0 ; i-- ){
+            while( !st.isEmpty() && nums[st.peek()] < nums[i] ) st.pop();
+            rightGreater[i] = st.empty() ? n : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+        int[] leftSmaller = new int[n];
+        for( int i = 0 ; i < n ; i++ ){
+            while( !st.isEmpty() && nums[st.peek()] >= nums[i] ) st.pop();
+            leftSmaller[i] = st.empty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+        int[] rightSmaller = new int[n];
+        for( int i = n-1 ; i >= 0 ; i-- ){
+            while( !st.isEmpty() && nums[st.peek()] > nums[i] ) st.pop();
+            rightSmaller[i] = st.empty() ? n : st.peek();
+            st.push(i);
+        }
+
+        for( int i = 0 ; i < n ; i++ ){
+            long smallerContri = (long)( i - leftSmaller[i] )*( rightSmaller[i] - i );
+            long greaterContri = (long)( i - leftGreater[i] )*( rightGreater[i] - i );
+            ans += ( nums[i] * greaterContri ) - ( nums[i] * smallerContri );
+        }
+
+        return ans;
     }
 }
