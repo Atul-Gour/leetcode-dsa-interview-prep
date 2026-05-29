c04e60d540f953dc8d9d903e98864d808c94fbac
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-05-29 18:00:07 +0530

    Time: 128 ms (6.02%) | Memory: 47.4 MB (37.22%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,22 +1,38 @@
 class Solution {
+
+    private int solve( int index , int[] dp , int[] nums ){
+        int n = nums.length;
+
+        if( index == n-1 ) return 0;
+        if( dp[index] != -1 ) return dp[index];
+
+        int ans = Integer.MAX_VALUE;
+
+        for( int i = 1 ; i <= nums[index] ; i++ ){
+            int j = i + index;
+            if( j >= n ) break;
+
+            int curr = solve(j , dp , nums);
+            if( curr != Integer.MAX_VALUE ) curr++;
+
+            ans = Math.min( ans , curr );
+        }
+
+        return dp[index] = ans;
+    }
+
     public int jump(int[] nums) {
         int n = nums.length;
-        if(n == 1)return 0;
+        int maxJump = 0;
+
+        for( int ele : nums ) maxJump = Math.max( ele , maxJump );
 
         int[] dp = new int[n];
 
-        Arrays.fill( dp ,Integer.MAX_VALUE );
+        Arrays.fill(dp , -1);
 
-        dp[0] = 0;
+        int ans = solve( 0 , dp , nums );
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i + 1 ; j < n ; j++){
-                if( dp[i] != Integer.MAX_VALUE && nums[i] >= j - i )
-                    dp[j] = Math.min( dp[j] , dp[i] + 1 );
-                else
-                   break;
-            }
-        }
-        return dp[n-1];
+        return ans;
     }
 }
