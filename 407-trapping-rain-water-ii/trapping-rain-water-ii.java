6896d842ad4fa7017f3a57dbab4ca0f6df0cf3d6
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-01 21:42:35 +0530

    Time: 19 ms (89.86%) | Memory: 47.8 MB (79.11%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.java
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.java
@@ -1,38 +1,58 @@
 class Solution {
+
     public int trapRainWater(int[][] heightMap) {
         int n = heightMap.length;
         int m = heightMap[0].length;
 
-        if( n < 3 || m < 3 ) return 0;
+        if(n <= 2 || m <= 2) return 0;
 
         boolean[][] visited = new boolean[n][m];
-        PriorityQueue<int[]> pq = new PriorityQueue<>( (int[] a , int[] b) -> Integer.compare( heightMap[a[0]][a[1]] , heightMap[b[0]][b[1]] ) );
-        int[][] dirs = { { 0 , 1 } , {0 , -1} , {-1 , 0} , {1 , 0} };
- 
+
+        PriorityQueue<int[]> pq =
+            new PriorityQueue<>((a,b) -> a[2] - b[2]);
+
+        for(int i=0;i<n;i++){
+            pq.offer(new int[]{i,0,heightMap[i][0]});
+            pq.offer(new int[]{i,m-1,heightMap[i][m-1]});
+            visited[i][0] = true;
+            visited[i][m-1] = true;
+        }
+
+        for(int j=1;j<m-1;j++){
+            pq.offer(new int[]{0,j,heightMap[0][j]});
+            pq.offer(new int[]{n-1,j,heightMap[n-1][j]});
+            visited[0][j] = true;
+            visited[n-1][j] = true;
+        }
+
         int water = 0;
-        int minHeight = 0;
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
 
-        for( int j = 0 ; j < m ; j++ ){ pq.offer( new int[]{0 , j} ); visited[0][j] = true; }
-        for( int j = 0 ; j < m ; j++ ){ pq.offer( new int[]{n-1 , j} ); visited[n-1][j] = true; }
-        for( int i = 1 ; i < n-1 ; i++ ){ pq.offer( new int[]{i , 0} ); visited[i][0] = true; }
-        for( int i = 1 ; i < n-1 ; i++ ){ pq.offer( new int[]{i , m-1} ); visited[i][m-1] = true; }
-
-        while( !pq.isEmpty() ){
+        while(!pq.isEmpty()){
             int[] curr = pq.poll();
-            int x = curr[0];
-            int y = curr[1];
 
-            minHeight = Math.max( minHeight , heightMap[x][y] );
-            water += Math.max( 0 , minHeight - heightMap[x][y] );
+            int r = curr[0];
+            int c = curr[1];
+            int h = curr[2];
 
-            for( int[] d : dirs ){
-                int newX = x + d[0];
-                int newY = y + d[1];
+            for(int[] d : dir){
+                int nr = r + d[0];
+                int nc = c + d[1];
 
-                if( newX < 0 || newY < 0 || newX >= n || newY >= m || visited[newX][newY] ) continue;
+                if(nr<0 || nc<0 || nr>=n || nc>=m || visited[nr][nc])
+                    continue;
 
-                visited[newX][newY] = true;
-                pq.offer( new int[]{newX , newY} );
+                visited[nr][nc] = true;
+
+                if(heightMap[nr][nc] < h){
+                    water += h - heightMap[nr][nc];
+                }
+
+                pq.offer(new int[]{
+                    nr,
+                    nc,
+                    Math.max(h , heightMap[nr][nc])
+                });
             }
         }
 
