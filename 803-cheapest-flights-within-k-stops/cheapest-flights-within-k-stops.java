6ef0b97a30cd78772606e526dd15bcac0587b96d
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-03 14:16:27 +0530

    Time: 6 ms (84.98%) | Memory: 46.4 MB (80.88%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,23 +1,57 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] dist = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[src] = 0;
+        
+        ArrayList< ArrayList< int[] >> edges = new ArrayList<>();
 
-        for(int i = 0; i <= k; i++){
-            int[] temp = dist.clone();  // IMPORTANT
+        ArrayDeque<int[]> pq = new ArrayDeque<>();
 
-            for(int[] flight : flights){
-                int u = flight[0], v = flight[1], w = flight[2];
+        int[] cost = new int[n];
 
-                if(dist[u] != Integer.MAX_VALUE && dist[u] + w < temp[v]){
-                    temp[v] = dist[u] + w;
+        for( int i = 0 ; i < n ; i++ ) edges.add( new ArrayList<>() );
+
+        for( int[] flight: flights ){
+            int from = flight[0];
+            int to = flight[1];
+            int price = flight[2];
+
+            edges.get( from ).add( new int[]{ to , price } );
+        }
+
+        Arrays.fill( cost , Integer.MAX_VALUE );
+        
+        pq.offer( new int[]{src , 0} );
+        cost[src] = 0;
+
+        while( !pq.isEmpty() && k >= 0 ){
+            int size = pq.size();
+            // System.out.println();
+
+            while( size-- > 0 ){
+                int curr[] = pq.poll();
+                int currCity = curr[0];
+                int currCost = curr[1];
+
+                // if( currCost > cost[currCity] ) continue;
+
+                // System.out.println(currCity + " curr cost =  " + currCost);
+
+                for( int[] neigh : edges.get(currCity) ){
+
+                    int newCity = neigh[0];
+                    int newCost = neigh[1] + currCost;
+
+                    if( newCost < cost[newCity] ){
+                        // System.out.println(newCity + " cost =  " + newCost);
+                        cost[newCity] = newCost;
+                        pq.offer( new int[]{ newCity , newCost } );
+                    }
+
                 }
             }
 
-            dist = temp;
+            k--;
         }
 
-        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+        return cost[dst] != Integer.MAX_VALUE ? cost[dst] : -1;
     }
 }
