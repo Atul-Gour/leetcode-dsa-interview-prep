fa9bef1c406950107f70a88646ebedc881cca494
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-03 10:06:29 +0530

    Time: 14 ms (65.58%) | Memory: 47.5 MB (70.81%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,41 +1,46 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
-
-        if(grid[0][0] != 0 || grid[n-1][n-1] != 0) return -1;
+        int m = grid[0].length;
 
         ArrayDeque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{0, 0});
-        grid[0][0] = 1; // mark visited
+        boolean[][] visited = new boolean[n][m];
 
-        int path = 1;
+        if( grid[0][0] == 1 || grid[n-1][m-1] == 1 ) return -1;
+        if( n == 1 && m == 1 ) return 1;
 
-        int[][] dir = {
-            {-1,-1},{-1,0},{-1,1},
-            {0,-1},{0,1},
-            {1,-1},{1,0},{1,1}
-        };
+        int dist = 1;
 
-        while(!q.isEmpty()){
+        q.offer( new int[]{n-1 , m - 1} );
+        visited[n-1][m-1] = true;
+
+        while( !q.isEmpty() ){
+
             int size = q.size();
+            dist++;
 
-            for(int s = 0; s < size; s++){
-                int[] curr = q.poll();
-                int i = curr[0], j = curr[1];
+            while( size-- > 0 ){
+                int curr[] = q.poll();
+                int x = curr[0];
+                int y = curr[1];
 
-                if(i == n-1 && j == n-1) return path;
+                for( int i = -1 ; i <= 1 ; i++ ){
+                    for( int j = -1 ; j <= 1 ; j++ ){
+                        if( i == 0 && j == 0 ) continue;
 
-                for(int[] d : dir){
-                    int ni = i + d[0];
-                    int nj = j + d[1];
+                        int newX = x + i;
+                        int newY = y + j;
 
-                    if(ni>=0 && nj>=0 && ni<n && nj<n && grid[ni][nj]==0){
-                        grid[ni][nj] = 1;
-                        q.offer(new int[]{ni, nj});
+                        if( newX < 0 || newY < 0 || newX >= n || newY >= m || grid[newX][newY] == 1 || visited[newX][newY] ) continue;
+
+                        if( newX == 0 && newY == 0 ) return dist;
+                        
+                        visited[newX][newY] = true;
+                        q.offer( new int[]{newX , newY} );
+
                     }
                 }
             }
-            path++;
         }
 
         return -1;
