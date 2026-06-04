1867df636d3d978f0b7e2b24b8773c0deb501162
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-05 01:08:33 +0530

    Time: 1119 ms (5.26%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,5 +1,13 @@
-select p.project_id , Round(avg( e.experience_years ) :: numeric , 2 ) as average_years
-from project p
-left join employee e
-on p.employee_id = e.employee_id 
-group by p.project_id
+SELECT DISTINCT
+    p1.project_id,
+    (
+        SELECT ROUND(
+            AVG(e.experience_years)::numeric,
+            2
+        )
+        FROM Project p2
+        JOIN Employee e
+            ON p2.employee_id = e.employee_id
+        WHERE p2.project_id = p1.project_id
+    ) AS average_years
+FROM Project p1;
