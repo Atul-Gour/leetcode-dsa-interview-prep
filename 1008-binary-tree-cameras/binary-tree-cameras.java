70b0431e5dd8789e27a3a15349749501f7a42149
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-05 11:57:03 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (60.10%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.java
+++ 1008-binary-tree-cameras/binary-tree-cameras.java
@@ -1,90 +1,32 @@
 class Solution {
-    static int solve(TreeNode curr){
-        if(( curr.left == null && curr.right == null ) )return 0;
+    int ans = 0;
 
-        int left = 0;
-        if(curr.left != null) left = solve(curr.left);
+    private int solve( TreeNode curr ){
+        if( curr.left == null && curr.right == null ) return 1;
 
-        int right = 0;
-        if(curr.right != null) right = solve(curr.right);
+        int left = -1;
+        int right = -1;
 
-        if(curr.right == null){
-
-            if(curr.left.val == 2){
-                curr.val = 1;
-                return left ;
-            }
-            else if(curr.left.val == 1){
-                curr.val = 0;
-                return left ;
-            }
-            else {
-                curr.val = 2;
-                return 1 + left ;
-            }
+        if( curr.left != null ) left = solve( curr.left );
+        if( curr.right != null ) right = solve( curr.right );
+        
+        if( left == 1 || right == 1 ){
+            ans++;
+            return 2;
         }
-        else if(curr.left == null){
+        
+        if( left == 2 || right == 2 ) return 0;
+        if( left == 0 || right == 0 )return 1;
 
-            if(curr.right.val == 2){
-                curr.val = 1;
-                return right ;
-            }
-            else if(curr.right.val == 1){
-                curr.val = 0;
-                return right ;
-            }
-            else {
-                curr.val = 2;
-                return 1 + right ;
-            }
-        }
-        else{
-            if(curr.left.val == 0 || curr.right.val == 0 ){
-                curr.val = 2;
-                return 1 + left + right;
-            }
-            else if(curr.left.val == 2 || curr.right.val == 2){
-                curr.val = 1;
-                return left + right;
-            }
-            else if(curr.left.val == 1 || curr.right.val == 1){
-                curr.val = 0;
-                return left + right;
-            }
-            else {
-                curr.val = 1;
-                return left + right;
-            }
-        }
+        return 1;
     }
-
+    
     public int minCameraCover(TreeNode root) {
-        if(root.left == null && root.right == null)return 1;
-        int ans =  solve( root );
+        ans = 0;
 
-        if( root.val == 0 ){
-            if(root.left != null && root.left.val == 2){
-                return ans;
-            }
-            if(root.right != null && root.right.val == 2){
-                return ans;
-            }
-            
+        int curr = solve(root);
+        if( curr == 1 ) ans++;
 
-            boolean left = false;
-            if(root.left != null && root.left.val == 1){
-                left = true;
-            }
-            if( root.right == null && left )ans++;
-
-            boolean right = false;
-            if(root.right != null && root.right.val == 1){
-                right = true;
-            }
-            if( root.left == null && right )ans++;
-
-            if( left && right ) ans++ ;
-        }
         return ans;
     }
 }
