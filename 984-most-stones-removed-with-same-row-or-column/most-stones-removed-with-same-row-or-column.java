aeec0667fcdb69c108112e523d395963287f2793
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-05 22:26:51 +0530

    Time: 15 ms (63.91%) | Memory: 47.1 MB (19.45%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,37 +1,89 @@
 class Solution {
-    static class DSU {
-        Map<Integer, Integer> parent = new HashMap<>();
+    class DSU{
+        
+        HashMap<Integer , Integer> rowParent;
+        HashMap<Integer , Integer> colParent;
+        HashMap<Integer , Integer> parent;
+        HashMap<Integer , Integer> size;
+        
+        DSU(){
+            this.rowParent = new HashMap<>();
+            this.colParent = new HashMap<>();
+            this.parent = new HashMap<>();
+            this.size = new HashMap<>();
+        }
+        
+        int find( int ele ){
+            if( parent.get(ele) == ele ) return ele;
 
-        int find(int x) {
-            if (!parent.containsKey(x)) parent.put(x, x);
-            if (parent.get(x) != x)
-                parent.put(x, find(parent.get(x)));
-            return parent.get(x);
+            int middle = find( parent.get(ele) );
+            parent.put( ele , middle );
+            return middle;
         }
 
-        void union(int a, int b) {
+        void add( int x , int y ){
+            int key = x*10001 + y;
+
+            parent.put( key , key );
+            size.put(key , 1);
+
+            if( rowParent.containsKey(x) ){
+                union( key , rowParent.get(x) );
+            }
+            else{
+                rowParent.put(x , key);
+            }
+
+            if( colParent.containsKey(y) ){
+                union( key , colParent.get(y) );
+            }
+            else{
+                colParent.put(y , key);
+            }
+        }
+        
+        void union( int a , int b ){
+            
             int pa = find(a);
             int pb = find(b);
-            if (pa != pb) parent.put(pa, pb);
+            
+            if( pa == pb ) return;
+            
+            int sizePa = size.get(pa);
+            int sizePb = size.get(pb);
+            
+            if( sizePa < sizePb ){
+                parent.put(pa , pb);
+                size.put( pb , sizePa + sizePb );
+            }
+            else{
+                parent.put(pb , pa);
+                size.put( pa , sizePa + sizePb );
+            }            
         }
     }
 
     public int removeStones(int[][] stones) {
+        
         DSU dsu = new DSU();
-        int OFFSET = 10001;
 
-        for (int[] stone : stones) {
-            int x = stone[0];
-            int y = stone[1] + OFFSET;
-            dsu.union(x, y);
+        for( int[] stone : stones ){
+            dsu.add( stone[0] , stone[1] );
         }
 
-        Set<Integer> uniqueParents = new HashSet<>();
+        HashSet<Integer> allKeys = new HashSet<>();
 
-        for (int[] stone : stones) {
-            uniqueParents.add(dsu.find(stone[0]));
+        for( int key : dsu.parent.values() ){
+            int keyParent = dsu.find(key);
+            allKeys.add(keyParent);
         }
 
-        return stones.length - uniqueParents.size();
+        int ans = 0;
+
+        for( int key : allKeys ){
+            ans += dsu.size.get(key) - 1;
+        }
+
+        return ans;
     }
 }
