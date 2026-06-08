8dd6a0688f04d391b87d421b34cdd78cd5822671
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-08 11:59:19 +0530

    Time: 10 ms (77.35%) | Memory: 46.5 MB (43.86%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,62 +1,39 @@
 class Solution {
-    class DSU {
-        int[] parent;
-
-        DSU(int n) {
-            parent = new int[n];
-            for (int i = 0; i < n; i++) parent[i] = i;
-        }
-
-        int find(int x) {
-            if (parent[x] != x)
-                parent[x] = find(parent[x]);
-            return parent[x];
-        }
-
-        void union(int a, int b) {
-            int pa = find(a), pb = find(b);
-            if (pa != pb) parent[pa] = pb;
-        }
-    }
-
     public int swimInWater(int[][] grid) {
         int n = grid.length;
-        int total = n * n;
+        int[][] visited = new int[n][n];
 
-        List<int[]> cells = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cells.add(new int[]{grid[i][j], i, j});
+        PriorityQueue<int[]> pq = new PriorityQueue<>( (a,b) -> Integer.compare( a[2] , b[2] ) );
+        int[][] dirs = {{0,1},{0,-1},{-1,0},{1,0}};
+
+        for( int[] vis : visited ) Arrays.fill( vis , Integer.MAX_VALUE );
+        pq.offer( new int[]{0,0 , grid[0][0] } );
+        visited[0][0] = grid[0][0];
+
+        while( !pq.isEmpty() ){
+            int curr[] = pq.poll();
+            int x = curr[0];
+            int y = curr[1];
+            int currMax = curr[2];
+
+            if( x == n - 1 && y == n-1 ) return currMax;
+            if( visited[x][y] < currMax  ) continue;
+
+            for( int dir[] : dirs ){
+                int newX = x + dir[0];
+                int newY = y + dir[1];
+
+                if( newX < 0 || newX >= n || newY < 0 || newY >= n ) continue;
+
+                int newMax = Math.max( currMax , grid[newX][newY] );
+
+                if( visited[newX][newY] > newMax ){
+                    visited[newX][newY] = newMax;
+                    pq.offer( new int[]{ newX , newY , newMax } );
+                }
             }
         }
 
-        Collections.sort(cells, (a, b) -> a[0] - b[0]);
-
-        DSU dsu = new DSU(total);
-        boolean[][] vis = new boolean[n][n];
-
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-
-        for (int[] cell : cells) {
-            int h = cell[0], i = cell[1], j = cell[2];
-            vis[i][j] = true;
-
-            int id1 = i * n + j;
-
-            for (int[] d : dirs) {
-                int ni = i + d[0], nj = j + d[1];
-
-                if (ni<0 || nj<0 || ni>=n || nj>=n) continue;
-                if (!vis[ni][nj]) continue;
-
-                int id2 = ni * n + nj;
-                dsu.union(id1, id2);
-            }
-
-            if (dsu.find(0) == dsu.find(n*n - 1)) {
-                return h;
-            }
-        }
         return -1;
     }
 }
