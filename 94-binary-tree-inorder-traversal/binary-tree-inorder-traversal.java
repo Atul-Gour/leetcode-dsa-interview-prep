74735b2e5aaa19916cea017cd7bd3864b70626a6
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-11 18:53:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (83.40%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -1,28 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
+    public List<Integer> inorderTraversal(TreeNode head) {
         List<Integer> inorder = new ArrayList<>();
-        
-        while( root != null ){
-            if( root.left == null ){
-                inorder.add( root.val );
-                root = root.right;
-            }else{
-                TreeNode temp = root.left;
 
-                while( temp.right != null && temp.right != root ){
+        while( head != null ){
+            if( head.left == null ){
+                inorder.add( head.val );
+                head = head.right;
+            }
+            else{
+                TreeNode temp = head.left;
+
+                while( temp.right != null && temp.right != head ){
                     temp = temp.right;
                 }
 
                 if( temp.right == null ){
-                    temp.right = root;
-                    root = root.left;
-                }else{
-                    inorder.add(root.val);
+                    temp.right = head;
+                    head = head.left;
+                }
+                else{
                     temp.right = null;
-                    root = root.right;
+                    inorder.add( head.val );
+                    head = head.right;
                 }
             }
         }
+
         return inorder;
     }
 }
