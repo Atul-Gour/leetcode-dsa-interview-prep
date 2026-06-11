9c9f05d38f0b74a516ab3fd3824d726c249b328b
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-11 15:53:32 +0530

    Time: 637 ms (15.44%) | Memory: 0B (100.00%) - LeetSync

--- 1877-find-followers-count/find-followers-count.sql
+++ 1877-find-followers-count/find-followers-count.sql
@@ -1,5 +1,4 @@
--- Write your PostgreSQL query statement below
-select f.user_id , count(*) as followers_count
-from Followers f 
-group by f.user_id
-order by f.user_id
+# Write your MySQL query statement below
+select user_id , count( distinct follower_id ) as followers_count
+from followers 
+group by user_id
