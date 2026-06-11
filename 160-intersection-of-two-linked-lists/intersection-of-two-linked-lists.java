521b7440cd21b6be95d758c81f1580bd58c35b48
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-11 10:17:58 +0530

    Time: 2 ms (51.24%) | Memory: 52.8 MB (61.17%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -1,41 +1,27 @@
-class Solution {
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
 
-        if (headA == null || headB == null) return null;
+        while( temp1 != temp2 ){
+            if( temp1 == null ) temp1 = headB;
+            else temp1 = temp1.next;
 
-        ListNode a = headA;
-        ListNode b = headB;
-
-        while (a != null && b != null) {
-            if (a == b) return a;
-            a = a.next;
-            b = b.next;
+            if( temp2 == null ) temp2 = headA;
+            else temp2 = temp2.next;            
         }
 
-        if (a == null && b == null) return null;
-
-        if (a == null) a = headB;
-        if (b == null) b = headA;
-        
-        while (a != null && b != null) {
-            if (a == b) return a;
-            a = a.next;
-            b = b.next;
-        }
-
-        if (a == null && b == null) return null;
-
-        if (a == null) a = headB;
-        if (b == null) b = headA;
-
-        while (a != b) {
-            if (a == null) a = headB;
-            else a = a.next;
-
-            if (b == null) b = headA;
-            else b = b.next;
-        }
-
-        return a;
+        return temp1;
     }
 }
