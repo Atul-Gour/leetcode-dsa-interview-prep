9a6f876fd97bfd5dff1d031e3dfa3dc12894942f
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-11 14:54:39 +0530

    Time: 620 ms (5.09%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,17 +1,12 @@
-SELECT round((
-    COUNT(*) FILTER (WHERE order_date = customer_pref_delivery_date) * 100.0 /
-    COUNT(*)
-),2) AS immediate_percentage
-FROM (
-    SELECT 
-        d.customer_id,
-        MIN(d.order_date) AS order_date,
-        (
-            SELECT dd.customer_pref_delivery_date
-            FROM Delivery dd
-            WHERE dd.customer_id = d.customer_id 
-            AND dd.order_date = MIN(d.order_date)
-        ) AS customer_pref_delivery_date
-    FROM Delivery d
-    GROUP BY d.customer_id
-) t;
+-- Write your PostgreSQL query statement below
+select Round(((count(*) filter (where x.customer_pref_delivery_date = x.order_date ) * 100.0) / count(*)) :: numeric , 2) as immediate_percentage 
+from(
+    select  d1.*
+    from Delivery d1 
+    where d1.order_date = (
+        select min(d2.order_date)
+        from Delivery d2
+        where d2.customer_id = d1.customer_id
+        group by d2.customer_id
+    )
+) as x
