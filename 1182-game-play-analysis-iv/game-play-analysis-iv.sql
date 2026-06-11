c1343c35eb132089ca333e83a8307a2ee71647cf
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-11 15:28:15 +0530

    Time: 625 ms (12.84%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,12 +1,9 @@
 select (
-    Round(count( * ) filter ( where a3.player_id is not null ) * 1.0 / count(a1.player_id) , 2 )
+    Round(count( distinct a1.player_id ) filter ( where a1.event_date = a1.first_day + interval '1 day' ) * 1.0 / count(distinct a1.player_id) , 2 )
 ) as fraction
 from (
     select 
             a2.*,
-            row_number() over (partition by player_id order by event_date ) as rn 
+            min(event_date) over (partition by player_id ) as first_day
     from activity a2
 ) a1
-left join activity a3
-on a1.player_id = a3.player_id and a3.event_date = a1.event_date + interval '1 day'
-where a1.rn = 1
