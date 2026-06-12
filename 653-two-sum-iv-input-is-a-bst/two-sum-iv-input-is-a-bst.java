97c62929db1380d5d7e85a3412c447e6c44b2e31
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-12 14:24:39 +0530

    Time: 3 ms (77.06%) | Memory: 47.1 MB (83.02%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -1,97 +1,52 @@
+class BSTIterator {
+    private Stack<TreeNode> st = new Stack<>();
+    private boolean reverse;
+
+    public BSTIterator(TreeNode root, boolean reverse) {
+        this.reverse = reverse;
+        pushAll(root);
+    }
+
+    private void pushAll(TreeNode node) {
+        while (node != null) {
+            st.push(node);
+            node = reverse ? node.right : node.left;
+        }
+    }
+
+    public int next() {
+        TreeNode node = st.pop();
+
+        if (reverse) {
+            pushAll(node.left);
+        } else {
+            pushAll(node.right);
+        }
+
+        return node.val;
+    }
+}
+
 class Solution {
-
-    private TreeNode getNext(TreeNode curr) {
-        while (curr != null) {
-            if (curr.left == null) {
-                TreeNode res = curr;
-                curr = curr.right;
-                return res;
-            }
-
-            TreeNode pred = curr.left;
-
-            while (pred.right != null && pred.right != curr) {
-                pred = pred.right;
-            }
-
-            if (pred.right == null) {
-                pred.right = curr;
-                curr = curr.left;
-            } else {
-                pred.right = null;
-                TreeNode res = curr;
-                curr = curr.right;
-                return res;
-            }
-        }
-
-        return null;
-    }
-
-    private TreeNode getPrev(TreeNode curr) {
-        while (curr != null) {
-            if (curr.right == null) {
-                TreeNode res = curr;
-                curr = curr.left;
-                return res;
-            }
-
-            TreeNode succ = curr.right;
-
-            while (succ.left != null && succ.left != curr) {
-                succ = succ.left;
-            }
-
-            if (succ.left == null) {
-                succ.left = curr;
-                curr = curr.right;
-            } else {
-                succ.left = null;
-                TreeNode res = curr;
-                curr = curr.left;
-                return res;
-            }
-        }
-
-        return null;
-    }
-
     public boolean findTarget(TreeNode root, int k) {
-        List<Integer> inorder = new ArrayList<>();
 
-        TreeNode curr = root;
+        if (root == null) return false;
 
-        while (curr != null) {
-            if (curr.left == null) {
-                inorder.add(curr.val);
-                curr = curr.right;
-            } else {
-                TreeNode pred = curr.left;
+        BSTIterator left = new BSTIterator(root, false);
+        BSTIterator right = new BSTIterator(root, true);
 
-                while (pred.right != null && pred.right != curr) {
-                    pred = pred.right;
-                }
-
-                if (pred.right == null) {
-                    pred.right = curr;
-                    curr = curr.left;
-                } else {
-                    pred.right = null;
-                    inorder.add(curr.val);
-                    curr = curr.right;
-                }
-            }
-        }
-
-        int i = 0;
-        int j = inorder.size() - 1;
+        int i = left.next();
+        int j = right.next();
 
         while (i < j) {
-            int sum = inorder.get(i) + inorder.get(j);
+            int sum = i + j;
 
             if (sum == k) return true;
-            if (sum < k) i++;
-            else j--;
+            if (sum < k) {
+                i = left.next();
+            } else {
+                j = right.next();
+            }
         }
 
         return false;
