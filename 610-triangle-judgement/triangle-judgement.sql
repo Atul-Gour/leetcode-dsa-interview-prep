40b48d3e45693ac8c0077dd9b34cb2f0856b4684
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-12 17:22:38 +0530

    Time: 174 ms (97.88%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,7 +1,9 @@
 -- Write your PostgreSQL query statement below
-select t.x , t.y ,t.z,
- case 
-    when x+y>z and y+z>x and x+z>y then 'Yes'
-    else 'No'
- end as triangle
-from Triangle t 
+select x , y , z , 
+    (
+        case
+            when ( ((x + y + z) - Greatest( x , y , z )) > Greatest( x , y , z ) ) then 'Yes'
+            else 'No'
+        end
+    ) as triangle
+from Triangle
