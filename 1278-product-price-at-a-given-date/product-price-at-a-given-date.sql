ffb03e33828c125e8f966d777be487abdcb7b286
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-12 18:42:45 +0530

    Time: 605 ms (5.22%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,16 +1,13 @@
--- Write your PostgreSQL query statement below
-
-
-select distinct p3.product_id , coalesce( x.new_price , 10 ) as price
-from (
-    select distinct product_id
-            ,(
-                select max(p2.change_date) 
-                from products p2
-                where p2.product_id = p1.product_id and p2.change_date <= '2019-08-16'
-            ) as newest_date
-    from products p1
-) as p3
-left join products x
-on p3.product_id = x.product_id and p3.newest_date = x.change_date
-where (p3.newest_date is null or p3.newest_date = x.change_date )
+SELECT DISTINCT p1.product_id,
+       COALESCE(
+           (
+               SELECT p2.new_price
+               FROM Products p2
+               WHERE p2.product_id = p1.product_id
+                 AND p2.change_date <= '2019-08-16'
+               ORDER BY p2.change_date DESC
+               LIMIT 1
+           ),
+           10
+       ) AS price
+FROM Products p1;
