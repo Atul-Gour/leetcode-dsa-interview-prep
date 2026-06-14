f0436ad614c114651ec961ccf9b8c511311c72f7
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-14 22:55:06 +0530

    Time: 217 ms (27.89%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,10 +1,7 @@
 SELECT (
-    SELECT salary
-    FROM (
-        SELECT salary,
-               DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
-        FROM Employee
-    ) t
-    WHERE rnk = 2
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    OFFSET 1
     LIMIT 1
 ) AS SecondHighestSalary;
