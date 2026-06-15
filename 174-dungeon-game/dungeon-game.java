c29bff5ff982be6efe82ed6299565c55c4ea23ca
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-15 20:02:23 +0530

    Time: 2 ms (70.16%) | Memory: 45.3 MB (55.01%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -5,7 +5,7 @@ class Solution {
 
         int[][] dirs = { {-1 , 0} , {0 , -1} };
 
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n + 1][m + 1];
         for( int[] d : dp ) Arrays.fill( d , Integer.MAX_VALUE );
 
         dp[n-1][m-1] = Math.max( 1 , 1 - dungeon[n-1][m-1] );
@@ -13,20 +13,10 @@ class Solution {
         for( int x = n-1 ; x >= 0 ; x-- ){
             for( int y = m-1 ; y >= 0 ; y-- ){
                 
-                if( x == 0 && y == 0 ) continue;
+                if( x == n-1 && y == m-1 ) continue;
 
-                for( int dir[] : dirs ){
-                    int newX = x + dir[0];
-                    int newY = y + dir[1];
+                dp[x][y] = Math.max( Math.min( dp[x+1][y] , dp[x][y+1] ) - dungeon[x][y] , 1 );
 
-                    if( newX < 0 || newY < 0 ) continue;
-
-                    int newNeed = Math.max( 1 , dp[x][y] - dungeon[newX][newY] );
-
-                    if( dp[newX][newY] <= newNeed )continue;
-
-                    dp[newX][newY] = newNeed;
-                }
             }
         }
 
