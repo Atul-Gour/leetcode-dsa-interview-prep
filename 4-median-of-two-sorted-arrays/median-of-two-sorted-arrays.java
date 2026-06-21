dd9e38b3d8e14c9acd3136721379b91e12123ef2
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-21 22:49:50 +0530

    Time: 2 ms (46.39%) | Memory: 48.8 MB (70.48%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,35 +1,43 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
         int n = nums1.length;
         int m = nums2.length;
 
-        if( m < n ) return findMedianSortedArrays( nums2 , nums1 );
+        if (n > m) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
-        int left = ( n + m + 1 )/2;
+        int totalLeft = (n + m + 1) / 2;
 
         int low = 0;
         int high = n;
 
-        while( low <= high ){
-            int mid = low + (high - low)/2 ; 
-            
-            int l1 = mid - 1 >= 0 ? nums1[mid - 1] : Integer.MIN_VALUE;
-            int r1 = mid < n ? nums1[mid] : Integer.MAX_VALUE;
+        while (low <= high) {
 
-            int l2 = left - mid - 1 >= 0 ? nums2[left - mid - 1] : Integer.MIN_VALUE;
-            int r2 = left - mid < m ? nums2[left - mid] : Integer.MAX_VALUE;
+            int cut1 = low + (high - low) / 2;
+            int cut2 = totalLeft - cut1;
 
-            if( l1 > r2 ) high = mid - 1;
-            else if( l2 > r1 ) low = mid + 1;
-            else{
-                if( (n + m) % 2 == 0 ){
-                    return ( Math.max( l1 , l2 ) + Math.min( r1 , r2 ) ) / 2.0 ;
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int r1 = (cut1 == n) ? Integer.MAX_VALUE : nums1[cut1];
+
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+            int r2 = (cut2 == m) ? Integer.MAX_VALUE : nums2[cut2];
+
+            if (l1 > r2) {
+                high = cut1 - 1;
+            } else if (l2 > r1) {
+                low = cut1 + 1;
+            } else {
+
+                if ((n + m) % 2 == 1) {
+                    return Math.max(l1, l2);
                 }
-                else return  (double)Math.max( l1 , l2 );
-            }
 
+                return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+            }
         }
 
-        return 1.0;
+        return 0.0;
     }
 }
