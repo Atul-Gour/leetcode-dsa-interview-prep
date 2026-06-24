9f76a80fa6b989e6c5fc19f434de1f7c524a6bc5
Atul Kumar Gour <162041144+Atul-Gour@users.noreply.github.com>
2026-06-24 22:44:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (60.87%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,14 +1,24 @@
 class Solution {
-    public void merge(int[] nums1, int n, int[] nums2, int m) {
-        int i = n - 1;
-        int j = m - 1;
-        int index = n + m -1;
 
-        while( i >= 0 && j >= 0 ){
-            if( nums1[i] >= nums2[j] ) nums1[index--] = nums1[i--];
-            else nums1[index--] = nums2[j--];
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = 0;
+
+        for( k = n+m-1 ; k >= 0 && i >= 0 && j >= 0 ; k-- ){
+            if( nums1[i] >= nums2[j] ){
+                nums1[k] = nums1[i--];
+            }
+            else nums1[k] = nums2[j--];
         }
-        while( i >= 0 ) nums1[index--] = nums1[i--];
-        while( j >= 0 ) nums1[index--] = nums2[j--];
+
+        while( i >= 0 ){
+            nums1[k--] = nums1[i--];
+        }
+
+        while( j >= 0 ){
+            nums1[k--] = nums2[j--];
+        }
+
     }
 }
